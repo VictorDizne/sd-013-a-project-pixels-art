1ecdac64aee3740cdc860703c9f054eaa7c5046a
Bruno Pinho <brunopinho@outlook.com>
2021-06-01 20:07:03 -0300

    realizado requisito 7 (de um jeito horrivel)

--- script.js
+++ script.js
@@ -1,10 +1,44 @@
 const colors = document.querySelectorAll('.color');
+const pixelBoard = document.querySelector('#pixel-board');
 
 window.onload = function() {
   colors[0].classList.add('selected');
 }
 
-const pixelBoard = document.querySelector('#pixel-board');
+colors[0].addEventListener('click', function (event) {
+  event.target.classList.add('selected');
+
+  colors[1].classList.remove('selected')
+  colors[2].classList.remove('selected')
+  colors[3].classList.remove('selected')
+})
+
+
+colors[1].addEventListener('click', function (event) {
+  event.target.classList.add('selected');
+
+  colors[0].classList.remove('selected')
+  colors[2].classList.remove('selected')
+  colors[3].classList.remove('selected')
+})
+
+colors[2].addEventListener('click', function (event) {
+  event.target.classList.add('selected');
+
+  colors[0].classList.remove('selected')
+  colors[1].classList.remove('selected')
+  colors[3].classList.remove('selected')
+})
+
+colors[3].addEventListener('click', function (event) {
+  event.target.classList.add('selected');
+
+  colors[0].classList.remove('selected')
+  colors[1].classList.remove('selected')
+  colors[2].classList.remove('selected')
+})
+
+
 
 function createPixels(number) {
   for (let indexRow = 0; indexRow < number; indexRow += 1) {
