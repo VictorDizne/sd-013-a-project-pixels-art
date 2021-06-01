7f51b50e4d0a546995880a96d5ae8fd74affec02
FelipeNevess <felipe012neves@gmail.com>
2021-06-01 18:00:46 -0300

    Desafio 4 e 5 finalizados

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 // Paleta de cores
-let color = document.querySelectorAll('.color');
+function addColorList () {
+  let colorList = document.querySelectorAll('.color');
 
-for (let c = 0; c < color.length; c += 1) {
-  let paleta = ['black', '#2a9d8f', '#e9c46a', '#f4a261', '#e76f51'];
+  for (let c = 0; c < colorList.length; c += 1) {
+    let paleta = ['black', '#2a9d8f', '#e9c46a', '#f4a261', '#e76f51'];
 
-  color[c].style.background = paleta[c];
+    colorList[c].style.background = paleta[c];
+  }
 }
+addColorList();
+
+// Quadrado de Pixels
