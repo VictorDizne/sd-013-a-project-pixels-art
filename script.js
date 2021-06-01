6ab3621939fbe26a872835d722d3404fec182fca
Wesley de Melo Pedroso <wesleyxd@hotmail.com>
2021-06-01 16:18:44 -0300

    4 - Adicione à página um quadro de pixels, com 25 pixels.

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 function colorsPalletes() {
-  const classColor = document.querySelectorAll(".color");
-  const pallets = ["#000000", "#F10202", "#2200FD", "#257400"];
+  const classColor = document.querySelectorAll('.color');
+  const pallets = ['#000000', '#F10202', '#2200FD', '#257400'];
   for (let index = 0; index < classColor.length; index += 1) {
     classColor[index].style.backgroundColor = pallets[index];
   }
 }
 colorsPalletes();
+
+function createBoard() {
+  const idBoard = document.querySelector('#pixel-board');
+  for (let index = 0; index < 25; index += 1) {
+    const createPixelBoard = document.createElement('div');
+    createPixelBoard.className = 'pixel';
+    idBoard.appendChild(createPixelBoard);
+  }
+}
+createBoard();
