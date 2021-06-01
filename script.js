a0cbe6b49317b9dd34bea1c04be4094be7e7e920
Gabriel Biasoli <bielbiasoli33@icloud.com>
2021-06-01 18:56:06 -0300

    cria evento de selecionar cores

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.onload = function () {
 
-
+  //Cria a linhas já com os pixel que preenchem o board, ou seja, cria o board.
   function boardMaker() {
     let pixelBoard = document.getElementById('pixel-board');
 
@@ -13,6 +13,7 @@ window.onload = function () {
   }
   boardMaker();
 
+  //Cria os pixels do board.
   function pixelMaker(line) {
 
     for (let i = 0; i < 5; i += 1) {
@@ -22,8 +23,27 @@ window.onload = function () {
     }
   }
 
+  //Define a cor preta como selecionada inicialmente.
+  function startBlack() {
+    let colorBlack = document.getElementsByClassName('color')[0];
+    console.log(colorBlack.backgroundColor);
+    colorBlack.classList.add('selected')
+  }
+  startBlack();
 
+  //Cria o evento de seleção de cores.
+  function colorSelect() {
+    let selected = document.getElementsByClassName('selected');
+    document.addEventListener('click', function (event) {
+      if (event.target.classList.contains('color')) {
+        for (let element of selected) {
+          element.classList.remove('selected');
+        }
+        event.target.classList.add('selected');
+      }
+    })
 
+  }
+  colorSelect();
 
-
-}
+  //Cria o evento de colorir os pixels
