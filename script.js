2c5f971daf03571e7f9cfc7b3457a59780db7315
Filipe <filipecandido123@hotmail.com>
2021-06-01 18:32:48 -0300

    adicionado requisito 3, 4 e 5

--- script.js
+++ script.js
@@ -2,23 +2,32 @@ let div1 = document.querySelector(".color")
 let div2 = document.querySelector(".color").nextElementSibling
 let div3 = document.querySelector(".color").nextElementSibling.nextElementSibling
 let div4 = document.querySelector(".color").nextElementSibling.nextElementSibling.nextElementSibling
+
 function corPreta() {
-  div1.style.backgroundColor="black"
+  div1.style.backgroundColor = "black"
 }
 corPreta()
 
 function corVermelha() {
-  div2.style.backgroundColor="red"
+  div2.style.backgroundColor = "red"
 }
 corVermelha()
 
 function corAzul() {
-  div3.style.backgroundColor="blue"
+  div3.style.backgroundColor = "blue"
 }
 corAzul()
 
 function corRosa() {
-  div4.style.backgroundColor="pink"
+  div4.style.backgroundColor = "pink"
 }
 corRosa()
 
+let AddDivs = document.getElementById("pixel-board")
+
+for (let index = 0; index < 25; index+= 1) {
+  let createBox = document.createElement("div")
+  createBox.hidden = AddDivs[index]
+  createBox.className = "pixel"
+  AddDivs.appendChild(createBox);
+}
