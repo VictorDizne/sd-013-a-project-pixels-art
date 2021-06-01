4bce85beeadca1494a8539ef9b0f99224f872d62
Daniel <daniel.melo.oliveira27.com.br>
2021-06-01 20:11:49 -0300

    Requisitos 4 a 6

--- script.js
+++ script.js
@@ -1,17 +1,54 @@
 let currentSelectedColor = 'rgb(0, 0, 0)';
+let pixelBoardNumber = 1;
 
 function clicouCor(evento) {
   const paletteElement = evento.target;
-  const paletteElementBackgroundColor = window.getComputedStyle(paletteElement).backgroundColor;
-  currentSelectedColor = paletteElementBackgroundColor
+  const paletteElementBackgroundColor =
+    window.getComputedStyle(paletteElement).backgroundColor;
+  currentSelectedColor = paletteElementBackgroundColor;
 }
 
-function onLoad() {
-let elementos= document.querySelectorAll('.color')
-console.log(elementos);
-  for (let elemento of elementos){
+function onLoadRefreshColors() {
+  let elementos = document.querySelectorAll('.color');
+  for (let elemento of elementos) {
     elemento.onclick = clicouCor;
+    if (elemento.id !== 'color1') {
+      elemento.style.backgroundColor = generateRgbColor();
+    }
   }
-
 }
-onLoad();
+
+function generateRgbColor() {
+  const R = Math.random() * (255 - 0) + 0;
+  const G = Math.random() * (255 - 0) + 0;
+  const B = Math.random() * (255 - 0) + 0;
+  return `rgb(${R}, ${G}, ${B})`;
+}
+function refreshColors() {
+  onLoadRefreshColors();
+}
+
+function numberPixelBoard() {
+  const divs = [];
+  pixelBoardNumber = Number(document.getElementById('input').value);
+  const pixelBoard = document.getElementById('pixel-board');
+  if (pixelBoardNumber >= 5) {
+    for (let i = 0; i < pixelBoardNumber; i++) {
+      const div = document.createElement('div');
+      div.className = 'line';
+      for (let j = 0; j < pixelBoardNumber; j++) {
+        const pixelDiv = document.createElement('div');
+        pixelDiv.className = 'pixel';
+        div.appendChild(pixelDiv);
+      }
+      divs.push(div);
+    }
+    pixelBoard.innerHTML = '';
+    for (let i = 0; i < divs.length; i++) {
+      pixelBoard.appendChild(divs[i]);
+    }
+  }
+  console.log(divs);
+}
+
+onLoadRefreshColors();
