42e91b8a872112f1fa12ad8ef75ba643d5dee3f4
ArthurMGoulart <budiarthur@gmail.com>
2021-06-01 18:07:31 -0300

    Realizando requisitos até o oitavo

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+function setListenersOfColors(){
+  let colors = document.querySelectorAll(".color");
+  for(let color of colors){
+    color.addEventListener("click", function (event){
+      unselectAllColors();
+      event.target.classList.add("selected");
+    })
+  }
+}
 function setColors(){
   let colors = document.querySelectorAll(".color");
   // Regra em que a primeira deve ser preta.
@@ -9,23 +18,48 @@ function setColors(){
   for (let i = 1; i < colors.length; i += 1) {
     colors[i].style.backgroundColor = colorsUser[i];
   }
+  setListenersOfColors();
+}
+function unselectAllColors(){
+  let colors = document.querySelectorAll(".color");
+  for(let color of colors){
+    color.classList.remove("selected");
+  }
+}
+function setListenersOfPixels(){
+  let pixels = document.querySelectorAll(".pixel");
+  for(let pixel of pixels){
+    pixel.addEventListener("click", function (event){
+      let colorToPaint = currentSelectedColor();
+      pixel.style.backgroundColor = colorToPaint;
+    })
+  }
 }
 function createPixel(){
   let pixel = document.createElement("div");
   pixel.className = "pixel";
+  pixel.style.backgroundColor = "white";
   return pixel;
 }
 function createBoard(){
   let board = document.querySelector("#pixel-board");
   for (let row = 0; row < 5; row += 1) {
-    let divRow = document.createElement("div");
-    divRow.className = "row";
     for(let column = 0; column < 5; column += 1) {
       let pixel = createPixel();
-      divRow.appendChild(pixel);
+      board.appendChild(pixel);
     }
-    board.appendChild(divRow);
   }
+  setListenersOfPixels();
+}
+function currentSelectedColor(){
+  let colors = document.querySelectorAll(".color");
+  for(let color of colors){
+    if(color.classList.contains("selected")){
+      return color.style.backgroundColor;
+    }
+  }
+  alert("erro");
+  return "white";
 }
 setColors();
 createBoard();
