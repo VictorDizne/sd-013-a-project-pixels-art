a0e136e95b47d483460c821dfc773d38ce5a754b
Julia Baptista <baptista.julia@yahoo.com.br>
2021-06-01 18:55:05 -0300

    8. Clicar em um pixel dentro do quadro após selecionar uma cor na paleta faz com que o pixel seja preenchido com a cor selecionada.

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 window.onload = function() {
 
-function classSelected() {
-  document.querySelector('#color-palette .color:nth-child(1)').classList.add('selected');
+  function classSelected() {
+    document.querySelector('#color-palette .color:nth-child(1)').classList.add('selected');
+  }
+  classSelected();
+
+  function changeClassSelected() {
+    let colorPalet = document.querySelector('#color-palette');
+    let colorPaletChildren = document.querySelectorAll('#color-palette .color');
+    
+    colorPalet.addEventListener('click', function(event) {
+      
+      if(event.target.classList.contains('color')) {
+        for (let i = 0; i < colorPaletChildren.length; i += 1) {
+          colorPaletChildren[i].classList.remove('selected');
+        }
+        event.target.classList.add('selected');
+        console.log(colorPaletChildren);
+      }
+    });
+  }
+
+  changeClassSelected();
+
 }
-
-classSelected();
-
-}
-
 
 
 
