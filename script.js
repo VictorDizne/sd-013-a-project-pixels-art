fadeddf7ec22db1558cd879b148e06e3175c5525
Gustavo-Jezini <Gustavojezini@gmail.com>
2021-06-01 16:55:53 -0300

    requisito 7

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-window.onload = selector()
+window.onload = selector();
 
-function selector () {
-  let black = document.getElementsByClassName("color")[0];
-  black.classList.add("selected")
-}
+function selector() {
+  const black = document.getElementsByClassName("color")[0];
+  black.classList.add("selected");
+};
+
+let palletBlocs = document.getElementById("color-palette")
+palletBlocs.addEventListener("click" ,function selectBlock(clickEvent){
+
+  function selectorChange() {
+    let colors = document.getElementsByClassName("color");
+    console.log(colors);
+    for(let i = 0; i < colors.length; i +=1 ){
+      colors[i].classList.remove("selected");
+    }
+  };
+  selectorChange();
+  let newSelected = clickEvent.target
+  newSelected.classList.add("selected")
+  console.log(newSelected);
+  }
+)
