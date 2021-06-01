b4102680d7197133bae3f59d38117770c2983582
V. Dionysio <vini.dionysio@gmail.com>
2021-06-01 16:45:05 -0300

    Solving challenges 6 and 7

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 window.onload = function() {
+  let initBlack = document.getElementsByClassName('color')[0];
+  initBlack.classList.add('selected')
+
   let pixelBoard = document.createElement('div');
   pixelBoard.id = 'pixel-board';
   pixelBoard.style.display = 'table';
-
   document.body.appendChild(pixelBoard);
 
   function fillPixelBoard(n){
-
     for (let i = 0; i < n; i += 1){
       let pixelRow = document.createElement('div');
       pixelRow.className = 'pixel-row'
@@ -23,4 +24,17 @@ window.onload = function() {
   }
 
   fillPixelBoard(5);
+
+  document.addEventListener('click', function (event) {
+    if ( event.target.classList.contains( 'color' ) ) {
+      for (let i = 0; i < 4; i += 1) {
+        let colorArray = document.getElementsByClassName('color');
+        if (event.target !== colorArray[i]) {
+          colorArray[i].classList.remove('selected')
+        } else {
+          colorArray[i].classList.add('selected')
+        }
+      }
+    }
+  }, false);
 }
