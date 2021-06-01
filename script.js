84dc046660d0fc469852066d0e25b888af54cb65
Filipe <filipecandido123@hotmail.com>
2021-06-01 16:11:25 -0300

    adicionado requisito 2 e 3

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 let div1 = document.querySelector(".color")
-
+let div2 = document.querySelector(".color").nextElementSibling
+let div3 = document.querySelector(".color").nextElementSibling.nextElementSibling
+let div4 = document.querySelector(".color").nextElementSibling.nextElementSibling.nextElementSibling
 function corPreta() {
   div1.style.backgroundColor="black"
 }
 corPreta()
+
+function corVermelha() {
+  div2.style.backgroundColor="red"
+}
+corVermelha()
+
+function corAzul() {
+  div3.style.backgroundColor="blue"
+}
+corAzul()
+
+function corRosa() {
+  div4.style.backgroundColor="pink"
+}
+corRosa()
+
