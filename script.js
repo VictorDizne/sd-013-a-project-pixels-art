9115ee2b69f5fd5a4e0bb175b938a69d95ca1968
Rafael Janovicci <rjanovicci@hotmail.com>
2021-06-01 20:55:55 -0300

    7 e correções

--- script.js
+++ script.js
@@ -9,10 +9,20 @@ function criarQuadros(quantidade) {
     for (let i2 = 1; i2 <= quantidadeQuadrados; i2 += 1) {
       const unidadeQuadrado = document.createElement('div');
       unidadeQuadrado.className = 'pixel';
-      unidadeQuadrado.style.backgroundColor = "#ffffff";
+      unidadeQuadrado.style.backgroundColor = '#ffffff';
       linhaQuadrados.appendChild(unidadeQuadrado);
     }
   }
 }
 
 criarQuadros(5);
+
+const trocaSelecionada = document.getElementById('color-palette');
+function corSelecionada(atual) {
+  for (let i = 0; i < trocaSelecionada.children.length; i += 1) {
+    trocaSelecionada.children[i].classList.remove('selected');
+  }
+  atual.target.classList.add('selected');
+  trocaSelecionada.classList.remove('selected');
+}
+trocaSelecionada.addEventListener('click', corSelecionada);
