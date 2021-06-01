23c4a4ccb5e368c011b90d632e8bd8e88920d801
ALINE <hoshinoaline@yahoo.com.br>
2021-06-01 17:48:07 -0300

    requisito 4

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 function createPixels() {
-  let quadroInicio=5
+  let quadroInicio = 5;
   for (let index = 0; index < quadroInicio; index += 1) {
     const quadrado = document.createElement('div');
-    const pixelBoard= document.querySelector('#pixel-board')
+    const pixelBoard = document.querySelector('#pixel-board');
     pixelBoard.appendChild(quadrado);
-    quadrado.className="pixel";
-  
-    for (let index2 = 1; index2 <= quadroInicio; index2 += 1) {
+    quadrado.className = 'linha';
+
+    for (let index2 = 0; index2 < quadroInicio; index2 += 1) {
       const quadrado2 = document.createElement('div');
-      quadrado2.className = "pixel";
+      quadrado2.className = 'pixel';
       quadrado.appendChild(quadrado2);
     }
   }
-};
-
+}
 createPixels();
