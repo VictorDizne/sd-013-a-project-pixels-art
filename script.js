5cc783b746e4142cb339210d6cb7c8b6126ae624
Murilo Rainho <murilorainho01@gmail.com>
2021-06-01 18:50:21 -0300

    Até o requisito 7

--- script.js
+++ script.js
@@ -4,11 +4,21 @@ const pixelBoard = document.querySelector('#pixel-board');
 function createPaletteDivs () {
 	const colors = ['black', 'red', 'gray', 'green'];
 	for (let index = 0; index < colors.length; index++) {
-		let div = document.createElement('div');
-		div.className = 'color';
-		colorPalette.appendChild(div);
-		div = document.querySelector('#color-palette').lastElementChild;
-		div.style.backgroundColor = colors[index];
+		if (index == 0) {
+			let div = document.createElement('div');
+			div.classList.add('color');
+			div.classList.add('selected')
+			colorPalette.appendChild(div);
+			div = document.querySelector('#color-palette').lastElementChild;
+			div.style.backgroundColor = colors[index];
+		} else {
+			let div = document.createElement('div');
+			div.className = 'color';
+			colorPalette.appendChild(div);
+			div = document.querySelector('#color-palette').lastElementChild;
+			div.style.backgroundColor = colors[index];
+		}
+
 	}
 }
 
@@ -19,10 +29,33 @@ function createMainDivs () {
 		let div1 = document.createElement('div');
 		div1.className = 'pixel';
 		pixelBoard.appendChild(div1);
+		for (let index = 0; index < 4; index++) {
+			let div1 = document.createElement('div');
+			div1.className = 'pixel';
+			pixelBoard.appendChild(div1);
+			div1 = document.querySelector('#pixel-board').lastElementChild;
+		}
 		div1 = document.querySelector('#pixel-board').lastElementChild;
-		div1.style.gridColumn = (index + 2)/(index + 3);
-		div1.style.gridRow = 2/3;
+		// div1.style.gridColumn = (index + 2)/(index + 3);
+		// div1.style.gridRow = 2/3;
 	}
 }
 
 createMainDivs ();
+
+for (let index = 0; index < 5; index++){
+	let colorDiv = document.querySelectorAll('.color')[index];
+	colorDiv.addEventListener('click', () => {
+		let selected = document.querySelector('.selected');
+		selected.classList.remove('selected');
+		colorDiv.classList.add('selected');
+	})
+}
+
+for (let index = 0; index < 25; index++){
+	let toPaint = document.querySelectorAll('.pixel')[index];
+	toPaint.addEventListener('click', () => {
+		let selected = document.querySelector('.selected');
+		toPaint.style.backgroundColor = selected.style.backgroundColor;
+	})
+}
