752e579bb21ac3d9e7b385eb13ee494fd3309e44
Pedro Delicoli <pedrodelicoli@hotmail.com>
2021-06-01 15:56:57 -0300

    Requisito 2

---
+++ script.js
@@ -0,0 +1,10 @@
+window.onload = function(){
+let colorPixel1 = document.getElementsByClassName('color')[0];
+colorPixel1.style.backgroundColor = 'black';
+let colorPixel2 = document.getElementsByClassName('color')[1];
+colorPixel2.style.backgroundColor = 'yellow';
+let colorPixel3 = document.getElementsByClassName('color')[2];
+colorPixel3.style.backgroundColor = 'green';
+let colorPixel4 = document.getElementsByClassName('color')[3];
+colorPixel4.style.backgroundColor = 'red';
+}
