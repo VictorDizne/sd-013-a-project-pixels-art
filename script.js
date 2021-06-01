c9abf12a467320f15be567fb826704c268055cab
Luiza Antiques <luiza.antiques@gmail.com>
2021-06-01 17:45:53 -0300

    Adiciona background white nos pixels

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 const pixelBoard = 5;
+window.onload = blackSelected;
+
+function blackSelected() {
+  const black = document.querySelector('.black');
+  black.classList.add('selected');
+}
+
 
 function createPixelBoard() {
   const board = document.getElementById('pixel-board');
@@ -6,9 +13,9 @@ function createPixelBoard() {
   for (let index = 0; index < pixelBoard; index += 1) {
     const divLine = document.createElement('div');
     divLine.classList.add('line');
-  
+
     board.appendChild(divLine);
-    for (let index = 0; index < pixelBoard; index += 1) {
+    for (let i = 0; i < pixelBoard; i += 1) {
       const divCollum = document.createElement('div');
       divCollum.className = 'pixel';
 
