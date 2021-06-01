ef70f0bdd5e5d4016a613efb3b1892a148f6d2e2
Leandro Liduvino <leandro.augusto460@gmail.com>
2021-06-01 16:56:04 -0300

    requisitos 4 e 5

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const paletteLength = 4;
+const boardSize = 5;
+
+createPalette();
+
+createPixels();
 
 function createPalette() {
   const table = document.querySelector('#color-palette');
@@ -13,4 +18,17 @@ function createPalette() {
   }
 }
 
-createPalette();
+function createPixels() {
+  const table = document.querySelector('#pixel-board');
+
+  for (let lineIndex = 0; lineIndex < boardSize; lineIndex++) {
+    const line = document.createElement('tr');
+    table.appendChild(line);
+
+    for (let columnIndex = 0; columnIndex < boardSize; columnIndex++) {
+      let td = document.createElement('td');
+      td.classList.add('pixel');
+      line.appendChild(td);
+    }
+  }
+}
