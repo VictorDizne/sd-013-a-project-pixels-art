851e4594b6c683982c9ee7284300c622e87f1b2f
FelipeNevess <felipe012neves@gmail.com>
2021-06-01 18:59:50 -0300

    Desafio 6 finalizado

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 // Paleta de cores
 function addColorList () {
   let colorList = document.querySelectorAll('.color');
+  let paleta = ['black', '#2a9d8f', '#e9c46a', '#f4a261', '#e76f51'];
 
   for (let c = 0; c < colorList.length; c += 1) {
-    let paleta = ['black', '#2a9d8f', '#e9c46a', '#f4a261', '#e76f51'];
-
     colorList[c].style.background = paleta[c];
   }
 }
+
 addColorList();
 
-// Quadrado de Pixels
+// Pincel
+
+let a = document.querySelector('.color');
+a.addEventListener('click', clicar);
+
+function clicar() {
+  a.style.background = 'red';
+}
