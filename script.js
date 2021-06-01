2eccc96efe134340ed725a1f87b4ad63cfe97101
Jéssica Queiroz <gnr0034@gmail.com>
2021-06-01 19:43:11 -0300

    arrumando lint

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-window.onload = function(){
-  selectedBlack();
-  initialPixelColor('white');
- 
-}
-
-function selectedBlack(){
+function selectedBlack () {
   let initialSelected = document.querySelector('#color-black');
   initialSelected.classList.add('selected');
 }
 
-function initialPixelColor(color){
+function initialPixelColor (color) {
   let initialPixel = document.getElementsByClassName('pixel');
   for(i=0; i < initialPixel.length; i+=1){
-  initialPixel[i].style.background = color;
+    initialPixel[i].style.background = color;
   }
+}
+
+window.onload = function () {
+  selectedBlack();
+  initialPixelColor('white');
+ 
 }
