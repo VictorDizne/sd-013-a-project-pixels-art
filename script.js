457110e67b7db2cb735661d6db9472d2ef084f10
ALINE <hoshinoaline@yahoo.com.br>
2021-06-01 18:46:48 -0300

    requisito 7

--- script.js
+++ script.js
@@ -14,9 +14,28 @@ function createPixels() {
   }
 }
 createPixels();
-//quando estou no 0, eu coloco a primeira linha no for 2 eu rodo 5 vezes criando 5 quadrados
-//quando estou no 1, eu coloco a segunda linha do for, no 2 for  eu rodo 5 vezes criando 5 quadrados
-//quando estou no 2, eu coloco a terceira linha do for, no 3 for eu rodo 5 vezes cirando 5 quadrados.
-//quando estou no 3 , eu coloco a quarta linha do for, no 4 for eu rodo 5 vezes criando 5 quadrados.
-//quando estou no 4, eu colco a 5 linha do for, no 4 for eu rodo 5 vezes criando 5 quadrados.
-//aprendi no plantao que nao posso por a classe pixel no primeiro for porque o primeiro for seriam as linhas criadas.
+/*quando estou no 0, eu coloco a primeira linha no for 2 eu rodo 5 vezes criando 5 quadrados
+quando estou no 1, eu coloco a segunda linha do for, no 2 for  eu rodo 5 vezes criando 5 quadrados
+quando estou no 2, eu coloco a terceira linha do for, no 3 for eu rodo 5 vezes cirando 5 quadrados.
+quando estou no 3 , eu coloco a quarta linha do for, no 4 for eu rodo 5 vezes criando 5 quadrados.
+quando estou no 4, eu colco a 5 linha do for, no 4 for eu rodo 5 vezes criando 5 quadrados.
+aprendi no plantao que nao posso por a classe pixel no primeiro for porque o primeiro for seriam as linhas criadas.
+requisito baseado no exercício da trybe bloco 4.3 exercício 1, lá tem 25 asteriscos também, fiz a troca pela div.
+joguei como filha para ficar embaixo */
+
+function addClassSelect(event){
+   let corSelecionada= document.querySelector('.selected');
+   corSelecionada.classList.remove('selected');
+   event.target.classList.add('selected');
+}
+
+const corPreta= document.querySelector('.black')
+const corVerde= document.querySelector('.green')
+const corCinza= document.querySelector('.gray')
+const corVermelha= document.querySelector('.red')
+
+corPreta.addEventListener('click', addClassSelect);
+corVerde.addEventListener('click', addClassSelect);
+corCinza.addEventListener('click', addClassSelect);
+corVermelha.addEventListener('click', addClassSelect); 
+/* addClassSelect foi baseado no bloco 5.3 do conteúdo parte 2, toda cor recebeu um evento, que ao clicar remove a classe de quem está e add a classe para o que é escolhido*/
