ad42ae772fd21e6c250972fe15fadff386405e0a
Rafael Janovicci <rjanovicci@hotmail.com>
2021-06-01 19:12:07 -0300

    corrigindo

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 function criarQuadros(quantidade) {
-  let quantidadeQuadrados = quantidade;
-  let pixelBoard = document.querySelector('#pixel-board');
+  const quantidadeQuadrados = quantidade;
+  const pixelBoard = document.querySelector('#pixel-board');
   for (let i = 0; i < quantidadeQuadrados; i += 1) {
-    let linhaQuadrados = document.createElement('div');
+    const linhaQuadrados = document.createElement('div');
     pixelBoard.appendChild(linhaQuadrados);
     linhaQuadrados.className = 'linha';
 
     for (let i2 = 1; i2 <= quantidadeQuadrados; i2 += 1) {
-      let unidadeQuadrado = document.createElement('div');
-      unidadeQuadrado.className='pixel';
+      const unidadeQuadrado = document.createElement('div');
+      unidadeQuadrado.className = 'pixel';
+      unidadeQuadrado.style.backgroundColor = "#ffffff";
       linhaQuadrados.appendChild(unidadeQuadrado);
-    };
-  };
-};
+    }
+  }
+}
 
 criarQuadros(5);
