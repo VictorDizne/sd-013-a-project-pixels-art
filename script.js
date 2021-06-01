c072d2ad0a98e755004bae39afbffb803541a62a
V. Dionysio <vini.dionysio@gmail.com>
2021-06-01 19:14:21 -0300

    Solving basic eslint js errors

--- script.js
+++ script.js
@@ -1,34 +1,35 @@
-window.onload = function() {
-  let initBlack = document.getElementsByClassName('color')[0];
-  initBlack.classList.add('selected')
+window.onload = function () {
+  const initBlack = document.getElementsByClassName('color')[0];
+  initBlack.classList.add('selected');
 
   for (let i = 1; i < 4; i += 1) {
-    let r = Math.floor(Math.random() * 256);
-    let g = Math.floor(Math.random() * 256);
-    let b = Math.floor(Math.random() * 256);
-    let colorUnit = document.getElementsByClassName('color');
-    let strColor = `rgb(${r},${g},${b})`;
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    const colorUnit = document.getElementsByClassName('color');
+    const strColor = `rgb(${r},${g},${b})`;
     colorUnit[i].style.backgroundColor = strColor;
   }
 
-  let pixelBoard = document.createElement('div');
-  pixelBoard.id = 'pixel-board';
+  const pixelBoardID = 'pixel-board';
+  const pixelBoard = document.createElement('div');
+  pixelBoard.id = pixelBoardID;
   pixelBoard.style.display = 'table';
   document.body.appendChild(pixelBoard);
 
-  let n = 5;
+  const n = 5;
 
-  function fillPixelBoard(n){
-    for (let i = 0; i < n; i += 1){
-      let pixelRow = document.createElement('div');
-      pixelRow.className = 'pixel-row'
-      pixelRow.style.display = 'table-row'
-      document.getElementById('pixel-board').appendChild(pixelRow);
-      for (let j = 0; j < n; j += 1) {
-        let pixel = document.createElement('div');
+  function fillPixelBoard(num) {
+    for (let i = 0; i < num; i += 1) {
+      const pixelRow = document.createElement('div');
+      pixelRow.className = 'pixel-row';
+      pixelRow.style.display = 'table-row';
+      document.getElementById(pixelBoardID).appendChild(pixelRow);
+      for (let j = 0; j < num; j += 1) {
+        const pixel = document.createElement('div');
         pixel.className = 'pixel';
-        pixel.style.display = 'table-cell'
-        pixel.style.backgroundColor = 'white'
+        pixel.style.display = 'table-cell';
+        pixel.style.backgroundColor = 'white';
         document.getElementsByClassName('pixel-row')[i].appendChild(pixel);
       }
     }
@@ -37,60 +38,48 @@ window.onload = function() {
   fillPixelBoard(n);
 
   document.addEventListener('click', function (event) {
-    if ( event.target.classList.contains( 'color' ) ) {
+    if (event.target.classList.contains('color')) {
       for (let i = 0; i < 4; i += 1) {
-        let colorArray = document.getElementsByClassName('color');
+        const colorArray = document.getElementsByClassName('color');
         if (event.target !== colorArray[i]) {
-          colorArray[i].classList.remove('selected')
+          colorArray[i].classList.remove('selected');
         } else {
-          colorArray[i].classList.add('selected')
+          colorArray[i].classList.add('selected');
         }
       }
     }
   }, false);
 
   document.addEventListener('click', function (event) {
-    if ( event.target.classList.contains('pixel') ) {
-      let selected = document.getElementsByClassName('selected')[0];
-      let selectedColor = window.getComputedStyle(selected, null).getPropertyValue('background-color')
-      event.target.style.backgroundColor = selectedColor;
+    if (event.target.classList.contains('pixel')) {
+      const sel = document.getElementsByClassName('selected')[0];
+      const selColor = window.getComputedStyle(sel, null).getPropertyValue('background-color');
+      event.target.style.backgroundColor = selColor;
     }
   }, false);
 
   document.getElementById('clear-board').addEventListener('click', function () {
-    let pixelCell = document.getElementsByClassName('pixel')
+    const pixelCell = document.getElementsByClassName('pixel');
     for (let i = 0; i < pixelCell.length; i += 1) {
       pixelCell[i].style.backgroundColor = 'white';
     }
   });
-  /*
-  for (let i = 5; i <= 50; i+= 1) {
-    let option = document.createElement('option')
-    option.value = i.toString();
-    if (i % 5 == 0) {
-      option.label = i.toString();
-    }
-    document.getElementById('tick').appendChild(option)
-  }
-  */
+
   document.getElementById('generate-board').addEventListener('click', function () {
     let size = document.getElementById('board-size').value;
-    if (size == '') {
+    if (size === '') {
       window.alert('Board inválido!');
-    } 
-    else {
-      if (size > 50) {
-        size = 50
-        document.getElementById('pixel-board').innerHTML = '';
-        fillPixelBoard(size);
-      } else if (size < 5) {
-        size = 5
-        document.getElementById('pixel-board').innerHTML = '';
-        fillPixelBoard(size);
-      } else {
-        document.getElementById('pixel-board').innerHTML = '';
-        fillPixelBoard(size);
-      }
+    } else if (size > 50) {
+      size = 50;
+      document.getElementById(pixelBoardID).innerHTML = '';
+      fillPixelBoard(size);
+    } else if (size < 5) {
+      size = 5;
+      document.getElementById(pixelBoardID).innerHTML = '';
+      fillPixelBoard(size);
+    } else {
+      document.getElementById(pixelBoardID).innerHTML = '';
+      fillPixelBoard(size);
     }
   });
-}
+};
