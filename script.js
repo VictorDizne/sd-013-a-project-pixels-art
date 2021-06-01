73a51673007d051bb2e6b69cdda33d4f348acc06
Cássio Pereira <cassiorodp@gmail.com>
2021-06-01 17:00:26 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 window.onload = function () {
   let paletteBlack = document.querySelector('div .black');
   paletteBlack.classList.add('selected');
-  console.log(paletteBlack);
+
+  let palleteColor = document.querySelectorAll('.color');
+  for (let index = 0; index < palleteColor.length; index += 1) {
+    palleteColor[index].addEventListener('click', function (event) {
+      for (let index2 = 0; index2 < palleteColor.length; index2 += 1) {
+        palleteColor[index2].classList.remove('selected');
+      }
+      event.target.classList.add('selected');
+    })
+  }
 }
