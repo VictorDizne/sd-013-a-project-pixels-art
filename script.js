02f0943e4460cbf2a964a90850a75bea873f8c69
gustavo.caruso <gustavoalmeida26@hotmail.com>
2021-06-01 16:49:49 -0300

    Adiciona requisito 4 ao projeto PA

---
+++ script.js
@@ -0,0 +1,8 @@
+let pixelBoard = document.getElementById('pixel-board');
+
+for (let i = 0; i < 25; i += 1) {
+  let pixel = document.createElement('div');
+  pixel.className = 'pixel';
+  pixelBoard.appendChild(pixel);
+  pixel.style.backgroundColor = 'rgb(255, 255, 255)';
+}
