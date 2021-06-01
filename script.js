2b3d7ca97507e33360baa3b714cf544de0c354a4
Wesley de Melo Pedroso <wesleyxd@hotmail.com>
2021-06-01 17:31:32 -0300

    6 - Defina a cor preta como cor inicial. Ao carregar a página a cor preta já deve estar selecionada para pintar os pixels

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-function colorsPalletes() {
-  const classColor = document.querySelectorAll('.color');
+const classColor = document.querySelectorAll('.color');
+
+function colorsPalletes(classColor) {
   const pallets = ['#000000', '#F10202', '#2200FD', '#257400'];
   for (let index = 0; index < classColor.length; index += 1) {
     classColor[index].style.backgroundColor = pallets[index];
   }
 }
-colorsPalletes();
+colorsPalletes(classColor);
 
 function createBoard() {
   const idBoard = document.querySelector('#pixel-board');
@@ -16,3 +17,38 @@ function createBoard() {
   }
 }
 createBoard();
+
+function initBlack() {
+  for (let index = 0; index < classColor.length; index += 1) {
+    let styleColor = classColor[index].getAttribute("style");
+    if (classColor[0].classList[1] === undefined) {
+      classColor[0].className += " selected";
+      colorPixelBoard(styleColor);
+    }
+  }
+}
+initBlack()
+
+function getColor() {
+  for (let index = 0; index < classColor.length; index += 1) {
+    let styleColor = classColor[index].getAttribute("style");
+    classColor[index].addEventListener("click", function(){
+      if (classColor[index].classList[1] === "selected") {
+        classColor[index].className = "color";
+      }else {
+        classColor[index].className = "color selected";
+        colorPixelBoard(styleColor);
+      }
+    });
+  }
+}
+getColor();
+
+function colorPixelBoard(styleValue) {
+  const pixel = document.querySelectorAll('.pixel');
+  for (let index = 0; index < pixel.length; index += 1) {
+    pixel[index].addEventListener("click",function(){
+      pixel[index].style.cssText = styleValue;
+    });
+  }
+}
