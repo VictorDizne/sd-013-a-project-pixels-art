7ce5ec0caf2707b6d8a26ea38d94de36935f73ce
thomasravache <thomasravache31@gmail.com>
2021-06-01 17:46:56 -0300

    Requisito 05 - Faça com que cada elemento do quadro de pixels possua 40 pixels de largura, 40 pixels de altura e seja delimitado por uma borda preta de 1 pixel. --> Finalizado.

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
-window.onload = function () {
+function createLines() {
+  const pixelBoard = document.querySelector('#pixel-board');
 
-  let pixelBoard = document.querySelector('#pixel-board');
-  
   for (let numLine = 0; numLine < 5; numLine += 1) {
-    let line = document.createElement('div');
+    const line = document.createElement('div');
     line.className = 'tr';
     pixelBoard.appendChild(line);
   }
+}
 
-  let tr = document.querySelectorAll('.tr');
+function createPixels() {
+  const tr = document.querySelectorAll('.tr');
 
   for (let line = 0; line < tr.length; line += 1) {
-    for (column = 0; column < 5; column += 1) {
-      let pixel = document.createElement('div');
+    for (let column = 0; column < 5; column += 1) {
+      const pixel = document.createElement('div');
       pixel.className = 'pixel';
       tr[line].appendChild(pixel);
     }
   }
-
 }
+
+window.onload = function inicio() {
+  createLines();
+  createPixels();
+};
