80e560a36a98daa700a41fc141972559a361008f
gustavo.caruso <gustavoalmeida26@hotmail.com>
2021-06-01 17:45:21 -0300

    Adiciona requisito 7 ao projeto PxArt

--- script.js
+++ script.js
@@ -1,13 +1,45 @@
-let pixelBoard = document.getElementById('pixel-board');
+const pixelBoard = document.getElementById('pixel-board');
 
 for (let i = 0; i < 25; i += 1) {
-  let pixel = document.createElement('div');
+  const pixel = document.createElement('div');
   pixel.className = 'pixel';
   pixelBoard.appendChild(pixel);
   pixel.style.backgroundColor = 'rgb(255, 255, 255)';
 }
 
-window.onload = function () {
-  let black = document.querySelector('.one');
-  black.className = 'color one selected';
-}
+let colorOne = document.querySelector('.one');
+let colorTwo = document.querySelector('.two');
+let colorThree = document.querySelector('.three');
+let colorFour = document.querySelector('.four');
+
+window.onload = function blackFirst () {
+  colorOne.className = 'color one selected';
+};
+
+colorOne.addEventListener('click', function () {
+  colorOne.className = 'color one selected';
+  colorTwo.className = 'color two';
+  colorThree.className = 'color three';
+  colorFour.className = 'color four';
+});
+
+colorTwo.addEventListener('click', function () {
+  colorOne.className = 'color one';
+  colorTwo.className = 'color two selected';
+  colorThree.className = 'color three';
+  colorFour.className = 'color four';
+});
+
+colorThree.addEventListener('click', function () {
+  colorOne.className = 'color one';
+  colorTwo.className = 'color two';
+  colorThree.className = 'color three selected';
+  colorFour.className = 'color four';
+});
+
+colorFour.addEventListener('click', function () {
+  colorOne.className = 'color one';
+  colorTwo.className = 'color two';
+  colorThree.className = 'color three';
+  colorFour.className = 'color four selected';
+});
