f22bc41d464920afc4471695c22f7119bb5bb2d0
Wesley Maia <maia.weol@gmail.com>
2021-06-01 18:56:59 -0300

    Requisito 5- Faça com que cada elemento do quadro de pixels possua 40x40 pixels

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 window.onload = function () {
-  let elementPixel = 5;
+  const elementPixel = 5;
   let elementPixelBoard = document.querySelector('#pixel-board');
-
-  fillFrame(elementPixel);
-
   function fillFrame(value) {
-    //Varre a linha
+    // Varre a linha
     for (let line = 0; line < value; line += 1) {
       let boxLine = document.createElement("div");
       boxLine.className = 'linha';
       elementPixelBoard.appendChild(boxLine);
 
-      //Varre a coluna
+      // Varre a coluna
       for (let column = 0; column < value; column += 1) {
         let boxColumn = document.createElement("div");
         boxColumn.className = 'pixel';
@@ -19,5 +16,5 @@ window.onload = function () {
       }
     }
   }
-  console.log(document.body)
+  fillFrame(elementPixel);
 }
