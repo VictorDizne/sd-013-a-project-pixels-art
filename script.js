3ac0872919a15c773caae3219e3a08f37a7d8af9
Icaro Ferreira <icaro.iky@gmail.com>
2021-06-01 17:24:37 -0300

    Req 3 e 4

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
-let colors = ['red', 'blue', 'green', 'yellow'];
+let colors = ['black', 'blue', 'green', 'red'];
 
-for ( i = 0; i < 4; i += 1){
-  let div = document.createElement('div');
-  div.className = 'color';
-  let colorPallete = document.querySelector('#color-palette');
-  colorPallete.appendChild(div);
+function createBoxPalette(){
+  for ( let i = 0; i < 4; i += 1){
+    let div = document.createElement('div');
+    div.className = 'color';
+    let colorPalette = document.querySelector('#color-palette');
+    colorPalette.appendChild(div);
+  };
 };
+createBoxPalette();
 
 function setColor(){
   let colorsAvaiable = document.querySelectorAll('#color-palette div');
   for (i = 0; i < colorsAvaiable.length; i += 1){
     colorsAvaiable[i].style.backgroundColor = colors[i];
-  }
-}
+  };
+};
 setColor();
+
+function createPixelBoard(){
+  for (let i = 0; i < 5; i += 1){
+    let pixelBoard = document.querySelector('#pixel-board');
+    let divLine = document.createElement('div');
+    pixelBoard.appendChild(divLine);
+    for( let index = 0; index < 5; index += 1){
+      let div = document.createElement('div');
+      div.className = 'pixel';
+      divLine.appendChild(div);
+    };
+  };
+};
+createPixelBoard();
