5ba65364e2b5d1616f3ba0d3d2f8c920dda755d4
Cássio Pereira <cassiorodp@gmail.com>
2021-06-01 17:49:31 -0300

    Requisito 8

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 window.onload = function () {
-  let paletteBlack = document.querySelector('div .black');
+  // Define a cor preta como a selecionada primeiramente
+  const paletteBlack = document.querySelector('div .black');
   paletteBlack.classList.add('selected');
 
-  let palleteColor = document.querySelectorAll('.color');
+  // Adiciona função de selecionar a cor da paleta
+  const palleteColor = document.querySelectorAll('.color');
   for (let index = 0; index < palleteColor.length; index += 1) {
     palleteColor[index].addEventListener('click', function (event) {
       for (let index2 = 0; index2 < palleteColor.length; index2 += 1) {
         palleteColor[index2].classList.remove('selected');
       }
       event.target.classList.add('selected');
-    })
+    });
   }
+};
+
+
+// Adiciona função de pintar o pixel
+let pixel = document.querySelectorAll('.pixel');
+for (let index = 0; index < pixel.length; index += 1) {
+  pixel[index].addEventListener('click', function (event) {
+    let selected = document.querySelector('.selected');
+    let backgroundColor = window.getComputedStyle(selected).getPropertyValue('background-color');
+    event.target.style.backgroundColor = backgroundColor;
+  })
 }
