82f6b2b330102c506e0b9a35b30b559f36afeea4
Emídio Rebouças <emidioreb@gmail.com>
2021-06-01 18:46:41 -0300

    realização do requisito 6

---
+++ script.js
@@ -0,0 +1,4 @@
+//adicionar classe selected
+const getBlack = document. querySelector(".color");
+if (getBlack.classList) getBlack.classList. add("selected");
+else getBlack. className += "selected";
