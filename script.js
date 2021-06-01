915b83837ee6d5fb130de591df52bb646c5a1515
Thiago <thiagoc.carboneri@gmail.com>
2021-06-01 17:11:04 -0300

    resolução requisitos 4 e 5

--- script.js
+++ script.js
@@ -14,5 +14,22 @@ function createPalette(colors) {
     getColorPalette.style.textAlign = 'center';
     }    
 }
-createPalette(['green', 'blue', 'orange', 'gray'])
+createPalette(['black', 'blue', 'orange', 'gray']);
 
+function createPixelSquares (number) {
+    for (let i = 0; i < number; i++) {
+        let getPixelBoard = document.querySelector('#pixel-board');
+        let linhaPixel = document.createElement('div');
+        linhaPixel.className = 'pixel';
+        linhaPixel.style.width = '40px';
+        linhaPixel.style.height = '40px';
+        linhaPixel.style.display = 'inline-table';
+        linhaPixel.style.border = '1px solid black'    
+        linhaPixel.style.margin = "20px"
+        getPixelBoard.appendChild(linhaPixel);
+    }
+    for (let j = 0; j < number[j];j+=5){
+        linhaPixel[j].style.display = 'block';
+    }
+}
+createPixelSquares(25);
