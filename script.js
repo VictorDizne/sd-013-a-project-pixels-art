092442e240391eafe382575dabbf6068f51d0c56
Luiza Antiques <luiza.antiques@gmail.com>
2021-06-01 17:18:20 -0300

    Arruma requisito 4

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-let pixelBoard = 5
+const pixelBoard = 5;
 
 function createPixelBoard() {
-  let board = document.getElementById('pixel-board');
+  const board = document.getElementById('pixel-board');
 
   for (let index = 0; index < pixelBoard; index += 1) {
-    let divLine = document.createElement('div');
-    divLine.classList.add('line', 'pixel');
+    const divLine = document.createElement('div');
+    divLine.classList.add('line');
   
     board.appendChild(divLine);
     for (let index = 0; index < pixelBoard; index += 1) {
-      let divCollum = document.createElement('div');
+      const divCollum = document.createElement('div');
       divCollum.className = 'pixel';
 
       divLine.appendChild(divCollum);
     }
   }
 }
-
 createPixelBoard();
