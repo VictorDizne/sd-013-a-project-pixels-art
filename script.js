9a32bbcd0af5f64650c44fe90d2ea81beecc20e5
Mateus Montagnini <mateus.r.montagnini@gmail.com>
2021-06-01 19:31:29 -0300

    Resolve requisito 4 e 5

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-// function changeColor() {
-//   let colorRed = document.getElementsByClassName('color');
-//   colorRed.style.backgroundColor = 'red';
-//   changeColor();
-// }
+// function pixelBoard () {
+// let board = document.createElement('div');
+// board.id = 'pixel-board';
+// body.appendChild(board);
 
-// window.onload = changeColor();
+// for (i = 0; i < 5; i += 1) {
+//   let linha = document.createElement('div');
+//   linha.className = 'pixel';
+//   board.appendChild(linha);
+//   for (let j = 0; j < 5; j += 1) {
+//     let coluna = 
+//   }
+// }
+// }
