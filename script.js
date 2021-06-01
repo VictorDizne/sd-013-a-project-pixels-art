e1d6674a78084237f00a3adcb7a01230f1223378
Reinaldo Paixao <reipaixao.b@gmail.com>
2021-06-01 19:12:14 -0300

    Adiciona o requisito 4 - Tabela de pixels

---
+++ script.js
@@ -0,0 +1,27 @@
+// Fonte: https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model/Traversing_an_HTML_table_with_JavaScript_and_DOM_Interfaces
+
+function createBoard(){
+let b = document.getElementsByTagName("body")[0];
+let t = document.createElement("table");
+t.className = "pixel-board";
+let tb = document.createElement("tbody");
+for (let i = 0; i < 5; i += 1) {
+  let line = document.createElement("tr");
+
+  for (let i2 = 0; i2 < 5; i2 += 1) {
+    let pixel = document.createElement("td");
+    pixel.className = "pixel";
+    line.appendChild(pixel);
+  }
+  tb.appendChild(line);
+}
+t.appendChild(tb);
+b.appendChild(t);
+}
+createBoard();
+
+
+
+
+
+
