043374818da70b8f31913d16acecf5b4bb8ba9f1
Lanai Conceição <conceicao.lanai@gmail.com>
2021-06-01 18:32:26 -0300

    Requisito 4 e 5

--- script.js
+++
@@ -1,5 +0,0 @@
-//onload = openPixelWhite(){
-  //let selectClass = document.querySelector('.pixel');
-  //selectClass.style.backgroundColor = white;
-  //selectClass.appendChild(openPixelWhite);
-//}
