87ee0b583bd436599308d857a7cbaf3eec7872d8
sonkai <mfilho86@gmail.com>
2021-06-01 18:37:57 -0300

    finalizei os pixels req4 e sem querer fiz o req5

--- script.js
+++ script.js
@@ -9,3 +9,26 @@ cor3.style.background = "blue"
 
 let cor4 = document.getElementsByClassName("color")[3]
 cor4.style.background = "green"
+
+/* Pixel Board
+?
+*/
+
+const tabelaPixels = document.getElementById('pixel-board');
+
+function gerartabelaPixels(Tamanho) {
+  for (let i = 0; i < Tamanho; i += 1 ) {
+    const linha = document.createElement('tr');
+    tabelaPixels.appendChild(linha);
+    gerarPixel(Tamanho, linha);
+  }
+}
+
+function gerarPixel(Tamanho, linha) {
+  for (let i = 0; i < Tamanho; i += 1 ) {
+    const pixel = document.createElement('td');
+    linha.appendChild(pixel);
+    pixel.classList = 'pixel';
+  }
+}
+gerartabelaPixels(5);
