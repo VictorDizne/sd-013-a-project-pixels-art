37b7bf9283d9cc7c932ac3cc14e403ad2c016d3b
sonkai <mfilho86@gmail.com>
2021-06-01 17:19:56 -0300

    coloquei paleta req2 e sem querer fiz o req3

---
+++ script.js
@@ -0,0 +1,11 @@
+let cor1 = document.getElementsByClassName("color")[0]
+cor1.style.background = "black"
+
+let cor2 = document.getElementsByClassName("color")[1]
+cor2.style.background = "red"
+
+let cor3 = document.getElementsByClassName("color")[2]
+cor3.style.background = "blue"
+
+let cor4 = document.getElementsByClassName("color")[3]
+cor4.style.background = "green"
