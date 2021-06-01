4ee1d31e35f23d45a29545f7779111338a33e026
thomasravache <thomasravache31@gmail.com>
2021-06-01 19:40:54 -0300

    Requisitos 06 e 08 --> Finalizados.

--- script.js
+++ script.js
@@ -23,4 +23,38 @@ function createPixels() {
 window.onload = function inicio() {
   createLines();
   createPixels();
+
+
+  let colorsOfPalette = document.querySelectorAll('.color');
+  let pixels = document.querySelectorAll('.pixel');
+  let colorSelected = 'black'
+
+  for (let index = 0; index < colorsOfPalette.length; index += 1) {
+    colorsOfPalette[index].addEventListener('click', function() {
+      console.log('clicado');
+      switch(index) {
+        case 1:
+          colorSelected = 'red';
+          break;
+        case 2:
+          colorSelected = 'purple';
+          break;
+        case 3:
+          colorSelected = 'orange';
+          break;
+        default:
+          colorSelected = 'black';
+      }
+    
+    })
+  }
+
+  for (let index = 0; index < pixels.length; index += 1) {
+
+    pixels[index].addEventListener('click', function () {
+      pixels[index].style.backgroundColor = colorSelected;
+      console.log(`pixel ${index} foi clicado`);
+    })
+  }
 };
+
