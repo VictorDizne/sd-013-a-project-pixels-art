4b5ef35fca267f09436ec87907de2a946dd0b138
Pedro Verano <pedroverano2009@yahoo.com.br>
2021-06-01 20:06:28 -0300

    Adiciona tabela gerada pelo JavaScript

--- script.js
+++ script.js
@@ -3,9 +3,24 @@
 //   elementocriado.className = classeElemento;
 //   elementoPai.appendChild(elementocriado);
 // }
-
+let numeroLinhas = 5;
+function createBoard() {
+  for (let i = 0; i < numeroLinhas; i += 1) {
+    const div = document.createElement('div');
+    div.className = 'tr';
+    document.querySelector('#pixel-board').appendChild(div);
+    for (let j = 0; j < numeroLinhas; j += 1) {
+      const pixel = document.createElement('div');
+      pixel.className = 'pixel td';
+      div.appendChild(pixel);
+    }
+  }
+}
+createBoard();
 const paletaCores = document.querySelectorAll('.color');
 const pixels = document.querySelectorAll('.pixel');
+
+
 
 function selectedActive() {
   for (let i = 0; i < paletaCores.length; i += 1) {
@@ -32,11 +47,13 @@ function addCollor(event) {
   event.target.style.backgroundColor = cor;
 }
 function clearPixels() {
-  for(let i = 0; i < pixels.length; i += 1) {
+  for (let i = 0; i < pixels.length; i += 1) {
     pixels[i].style.backgroundColor = 'white';
   }
 }
+
 window.document.onload = selectedActive();
 addEvents(pixels, 'click', addCollor);
 addEvents(paletaCores, 'click', sectorColor);
 document.getElementById('clear-board').addEventListener('click', clearPixels);
+
