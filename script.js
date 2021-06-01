74b5647eaf8c53799d6471b70e83cc10a8f5118f
ALINE <hoshinoaline@yahoo.com.br>
2021-06-01 18:10:26 -0300

    requisito 6

--- script.js
+++ script.js
@@ -14,3 +14,9 @@ function createPixels() {
   }
 }
 createPixels();
+//quando estou no 0, eu coloco a primeira linha no for 2 eu rodo 5 vezes criando 5 quadrados
+//quando estou no 1, eu coloco a segunda linha do for, no 2 for  eu rodo 5 vezes criando 5 quadrados
+//quando estou no 2, eu coloco a terceira linha do for, no 3 for eu rodo 5 vezes cirando 5 quadrados.
+//quando estou no 3 , eu coloco a quarta linha do for, no 4 for eu rodo 5 vezes criando 5 quadrados.
+//quando estou no 4, eu colco a 5 linha do for, no 4 for eu rodo 5 vezes criando 5 quadrados.
+//aprendi no plantao que nao posso por a classe pixel no primeiro for porque o primeiro for seriam as linhas criadas.
