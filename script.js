8369cec9e2932b73ab3710c5c1ef8fa14458dd26
Filipe <filipecandido123@hotmail.com>
2021-06-01 19:38:40 -0300

    algumas coisinhas

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const div1 = document.querySelector(".color")
 const div2 = document.querySelector(".color").nextElementSibling
 const div3 = document.querySelector(".color").nextElementSibling.nextElementSibling
 const div4 = document.querySelector(".color").nextElementSibling.nextElementSibling.nextElementSibling
-
+//cores fixas com um selecionador
 function corPreta() {
   div1.style.backgroundColor = "black"
   div1.classList.add("selected")
@@ -23,7 +23,7 @@ function corRosa() {
   div4.style.backgroundColor = "pink"
 }
 corRosa()
-
+//criação dos blocoDIv
 let AddDivs = document.getElementById("pixel-board")
 
 for (let index = 0; index < 25; index+= 1) {
@@ -32,3 +32,21 @@ for (let index = 0; index < 25; index+= 1) {
   createBox.className = "pixel"
   AddDivs.appendChild(createBox);
 }
+//evento que seleciona as paletas de cores
+function mudaDiv(event) {
+  const elementoTech = document.querySelector('.selected');
+  elementoTech.classList.remove('selected');
+  event.target.classList.add('selected');
+}
+
+div1.addEventListener("click", mudaDiv)
+div2.addEventListener("click", mudaDiv)
+div3.addEventListener("click", mudaDiv)
+div4.addEventListener("click", mudaDiv)
+
+let boxsDePinturas = document.querySelectorAll(".pixel")
+
+// boxsDePinturas.forEach(function(cor) {
+//   let buscaPalletas = document.querySelectorAll(".cores");
+//   buscaPalletas
+// }
