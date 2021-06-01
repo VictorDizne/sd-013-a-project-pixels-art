e04aff9be8a86a9e04cbc33cf568a8f9eb945efd
Gustavo-Jezini <Gustavojezini@gmail.com>
2021-06-01 19:15:25 -0300

    ponitnho

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.onload = function selector() {
   const black = document.getElementsByClassName('color')[0];
   black.classList.add('selected');
-}
+};
 
 const palletBlocs = document.getElementById('color-palette');
 
@@ -14,34 +14,22 @@ palletBlocs.addEventListener('click', function selectedBlock(clickEvent) {
   }
   const newSelected = clickEvent.target;
   newSelected.classList.add('selected');
-  if (newSelected === document.getElementsByClassName('color')[0]){
+  if (newSelected === document.getElementsByClassName('color')[0]) {
     inputColor = '#000';
-    }
+  }
   else if (newSelected === document.getElementsByClassName('color')[1]) {
     inputColor = '#0000ff';
-    }
+  }
   else if (newSelected === document.getElementsByClassName('color')[2]) {
     inputColor = '#ff0000';
-    }
+  }
   else if (newSelected === document.getElementsByClassName('color')[3]) {
     inputColor = '#ffff00';
   }
-}
-
-);
-
-
-// document.getElementsByClassName('color')[0].style.background = 'black'
-// document.getElementsByClassName('color')[1].style.background = 'blue'
-// document.getElementsByClassName('color')[2].style.background = 'red'
-// document.getElementsByClassName('color')[3].style.background = 'yellow'
-
+},);
 const painting = document.getElementById('pixel-board');
-
 painting.addEventListener('click' ,function paintingBlocks(secondClick) {
   const paintingBlock = secondClick.target;
-  // let colorChoose = document.getElementsByClassName('color')[0];
-  // console.log(colorChoose);
   paintingBlock.style.backgroundColor = inputColor;
 
 });
