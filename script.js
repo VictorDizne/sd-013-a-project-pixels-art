9f9593f3fee8d208434c74d07b8f53cb5797b844
VictorDizne <victoralbertocostadiniz@gmail.com>
2021-06-01 18:06:23 -0300

    Completa o oitavo requisito do projeto

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let palletSelectedColor = 'black';
+
 function selectColor() {
   const pallet = document.getElementsByClassName('color');
   for (let i = 0; i < pallet.length; i += 1) {
@@ -6,7 +8,25 @@ function selectColor() {
       selectedColor.classList.remove('selected');
       const clickedColor = event.target;
       clickedColor.classList.add('selected');
+      if (pallet[i] === pallet[0]) {
+        palletSelectedColor = 'black';
+      } else if (pallet[i] === pallet[1]) {
+        palletSelectedColor = 'rgb(235, 7, 7)';
+      } else if (pallet[i] === pallet[2]) {
+        palletSelectedColor = 'rgb(160, 34, 34)';
+      } else if (pallet[i] === pallet[3]) {
+        palletSelectedColor = 'rgb(136, 207, 252)';
+      }
     });
   }
 }
 selectColor();
+function paintPixel() {
+  const pixels = document.getElementsByClassName('pixel');
+  for (let i = 0; i < pixels.length; i += 1) {
+    pixels[i].addEventListener('click', function () {
+      pixels[i].style.backgroundColor = palletSelectedColor;
+    });
+  }
+}
+paintPixel();
