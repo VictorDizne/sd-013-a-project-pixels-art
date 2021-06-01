80fa0a0d8b2ad45f56b6f6547203d3b9a7b0f3a3
Jean Marcel <jeanvanzella91@gmail.com>
2021-06-01 16:47:03 -0300

    paleta feita

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementsByClassName("color")[0].style.backgroundColor = "red";
+document.getElementsByClassName("color")[1].style.backgroundColor = "orange";
+document.getElementsByClassName("color")[2].style.backgroundColor = "yellow";
+document.getElementsByClassName("color")[3].style.backgroundColor = "green";
+document.getElementsByClassName("color")[4].style.backgroundColor = "blue";
+document.getElementsByClassName("color")[5].style.backgroundColor = "indigo";
+document.getElementsByClassName("color")[6].style.backgroundColor = "purple";
