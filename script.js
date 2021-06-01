777c89d05913c223fe89b141679881809e48d6d5
gustavo.caruso <gustavoalmeida26@hotmail.com>
2021-06-01 17:14:11 -0300

    Adiciona requisito 6 ao projeto PxArt

--- script.js
+++ script.js
@@ -6,3 +6,8 @@ for (let i = 0; i < 25; i += 1) {
   pixelBoard.appendChild(pixel);
   pixel.style.backgroundColor = 'rgb(255, 255, 255)';
 }
+
+window.onload = function () {
+  let black = document.querySelector('.one');
+  black.className = 'color one selected';
+}
