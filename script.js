7964a82019dd81c46cfb7db1380042b077d7c873
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2021-06-01 20:58:06 -0300

    Requesito 07 - ok

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const paletaColor = document.getElementById('color-palette')
+
 // função retorna uma valor de 0 ao numero
 function randomNumber(numero) {
   return Math.floor(Math.random() * numero);
@@ -13,17 +15,18 @@ function randomColorDiv() {
   for (let el of listaCorDiv) {
     if (el.style.backgroundColor !== 'black') {
       // Usa a cor e deleta ela da lista.
-      el.style.backgroundColor = listCor.splice(randomNumber(listCor.length), 1); 
+      el.style.backgroundColor = listCor.splice(randomNumber(listCor.length), 1);
     }
   }
 }
 
-function createPixelBord(){
+// Criar o quadro de pixels
+function createPixelBord() {
   const pixelBoard = document.getElementById('pixel-board')
 
-  for(let linha = 0; linha < 5; linha++){
+  for (let linha = 0; linha < 5; linha++) {
     let divLinha = document.createElement('div')
-    for(let coluna = 0; coluna < 5; coluna++){
+    for (let coluna = 0; coluna < 5; coluna++) {
       let divCelular = document.createElement('div')
       divCelular.className = 'pixel'
       divLinha.appendChild(divCelular)
@@ -32,7 +35,23 @@ function createPixelBord(){
   }
 }
 
+// Função que seleciona a cor
+function selectCorClick(elemet) {
+  const listPaleta = document.getElementsByClassName('color')
+  for (let select of listPaleta) {
+    if(elemet.target.className !== ''){
+      select.className = 'color'
+    }
+  }
+
+  if(elemet.target.className === 'color'){
+    elemet.target.className = 'color selected '
+  }
+}
+
+
+paletaColor.addEventListener('click', selectCorClick)
 window.onload = () => {
   randomColorDiv();
-  createPixelBord()
+  createPixelBord();
 };
