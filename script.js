510e031da91f5f40ba957673caa140bc04bf8197
turtore <brunosap@gmail.com>
2021-06-01 19:28:33 -0300

    reparado tamanho do quadro de pixels

---
+++ script.js
@@ -0,0 +1,29 @@
+
+// onload = function criaQuadro(){
+//     for (let i = 0; i > 5 ; i++) {
+//         let tempPixel = document.createElement('div');
+//         tempPixel.className = 'pixel';
+//         console.log(tempPixel.className)
+//         document.querySelector('pixel-board').appendChild(tempPixel);
+
+    
+//     }
+// }
+
+// onclick = function changebackground(){
+// 	document.getElementsByClassName('pixel')[0].style.backgroundColor ='green';     console.log(document.getElementsByClassName('pixel').style.backgroundColor)
+// }
+
+
+// onclick = function changeColor(){
+//     let isso = document.querySelector('.pixel .pixel');
+//     console.log(isso)
+//     console.log(isso.style.backgroundColor)
+//     return isso;
+
+// }
+
+// se  div id="color-palette , faz seleciona a cor da class color
+
+
+//se div id="pixel-board" , pinta a class "pixel"
