114a1be627d315ffa7f7b72a105e1fa9c00eebd1
Douglas dos Santos <doug.frutuoso@gmail.com>
2021-06-01 18:41:48 -0300

    desafio 4 e 5 completos

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+
+function gradePixel(){
+let board = document.querySelector("#pixel-board");
+let quadrado = 5;
+
+for(index =0; index< quadrado; index+=1){
+
+  let linha = document.createElement("div");
+  linha.className = "linha";
+  board.appendChild(linha);
+
+
+  for(let i=0; i< quadrado ; i+=1){
+
+    let blocos = document.createElement("div");
+    blocos.className = "pixel";
+    linha.appendChild(blocos);
+  }
+}
+
+}
+
+gradePixel();
