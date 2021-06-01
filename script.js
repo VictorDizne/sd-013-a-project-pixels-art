a5567328f2b901b88e900d98f38282d13c9ab958
Wallacy Francis <wallacy_francis@hotmail.com>
2021-06-01 16:50:43 -0300

    Quadro de pixesl criado

---
+++ script.js
@@ -0,0 +1,5 @@
+for (let line = 0 ; line < 25 ; line++) {
+  let pixels = document.createElement('div');
+  pixels.className = 'pixel';
+  document.getElementById('pixel-board').appendChild(pixels);
+}
