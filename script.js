8189e19caeb0f1f470a6336797e29d7a4e386178
Wallacy Francis <wallacy_francis@hotmail.com>
2021-06-01 19:23:17 -0300

    Paleta de cores com seleção de classe realizada, elemento selecio o objeto clicado e adiciona a classe select

--- script.js
+++ script.js
@@ -1,5 +1,99 @@
-for (let line = 0 ; line < 25 ; line++) {
-  let pixels = document.createElement('div');
-  pixels.className = 'pixel';
-  document.getElementById('pixel-board').appendChild(pixels);
+window.onload = function () {
+
+  // Cria quadrado de pixels 5 x 5
+  for (let line = 0 ; line < 5 ; line++) {
+    let pixelLine = document.createElement('tr');
+      for (let colum = 0 ; colum < 5 ; colum++) {
+        let pixelData = document.createElement('td');
+        pixelData.className = 'pixel';
+        pixelLine.appendChild(pixelData);
+      }  
+    document.getElementById('pixel-board').appendChild(pixelLine);
+  }
+
+  // Inicia com cor preta selecionada
+  let black = document.getElementById('black');
+  let red = document.getElementById('red');
+  let green = document.getElementById('green');
+  let blue = document.getElementById('blue');
+
+  black.className += ' selected';
+
+  black.onclick = function() {
+    if (green.className === 'color selected') {
+      green.classList.remove('selected');
+    }
+    if (red.className === 'color selected') {
+      red.classList.remove('selected');
+    }
+    if (blue.className === 'color selected') {
+      blue.classList.remove('selected');
+    }
+    black.className += ' selected';
+  }
+
+  green.onclick = function() {
+    if (black.className === 'color selected') {
+      black.classList.remove('selected');
+    }
+    if (red.className === 'color selected') {
+      red.classList.remove('selected');
+    }
+    if (blue.className === 'color selected') {
+      blue.classList.remove('selected');
+    }
+    green.className += ' selected';
+  }
+
+  red.onclick = function() {
+    if (black.className === 'color selected') {
+      black.classList.remove('selected');
+    }
+    if (green.className === 'color selected') {
+      green.classList.remove('selected');
+    }
+    if (blue.className === 'color selected') {
+      blue.classList.remove('selected');
+    }
+    red.className += ' selected';
+  }
+
+  blue.onclick = function() {
+    if (black.className === 'color selected') {
+      black.classList.remove('selected');
+    }
+    if (red.className === 'color selected') {
+      red.classList.remove('selected');
+    }
+    if (green.className === 'color selected') {
+      green.classList.remove('selected');
+    }
+    blue.className += ' selected';
+  }
+
+
+ 
+  /* let color;
+  black.onclick = function () {
+    color = 'black';
+  }
+  red.onclick = function () {
+    color = 'red';
+  }
+  green.onclick = function () {
+    color = 'green';
+  }
+  blue.onclick = function () {
+    color = 'blue';
+  } */
+
+  
+  
+    
+
+
+  
 }
+
+
+
