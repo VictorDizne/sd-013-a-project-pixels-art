7696e30d4cc0ad77e851780f491ce3d6964a9710
Jean Marcel <jeanvanzella91@gmail.com>
2021-06-01 17:11:32 -0300

    arrumando a 2

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 document.getElementsByClassName("color")[0].style.backgroundColor = "red";
-document.getElementsByClassName("color")[1].style.backgroundColor = "orange";
-document.getElementsByClassName("color")[2].style.backgroundColor = "yellow";
-document.getElementsByClassName("color")[3].style.backgroundColor = "green";
-document.getElementsByClassName("color")[4].style.backgroundColor = "blue";
-document.getElementsByClassName("color")[5].style.backgroundColor = "indigo";
-document.getElementsByClassName("color")[6].style.backgroundColor = "purple";
+document.getElementsByClassName("color")[1].style.backgroundColor = "yellow";
+document.getElementsByClassName("color")[2].style.backgroundColor = "green";
+document.getElementsByClassName("color")[3].style.backgroundColor = "blue";
