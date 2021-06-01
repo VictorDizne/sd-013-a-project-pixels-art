1fd43c15985399213fc17a2dc959895660b88b95
Gustavo-Jezini <Gustavojezini@gmail.com>
2021-06-01 19:10:55 -0300

    Arrumando os pontinho2

--- script.js
+++ script.js
@@ -1,34 +1,33 @@
-window.onload = selector;
-
-function selector() {
+window.onload = function selector() {
   const black = document.getElementsByClassName('color')[0];
   black.classList.add('selected');
 }
 
-const palletBlocs = document.getElementById("color-palette");
+const palletBlocs = document.getElementById('color-palette');
 
 let inputColor = '#000';
 
-palletBlocs.addEventListener('click', function sBlock(clickEvent) {
+palletBlocs.addEventListener('click', function selectedBlock(clickEvent) {
   const colors = document.getElementsByClassName('color');
-    for (let i = 0; i < colors.length; i += 1) {
-      colors[i].classList.remove('selected');
-    }
+  for (let i = 0; i < colors.length; i += 1) {
+    colors[i].classList.remove('selected');
+  }
   const newSelected = clickEvent.target;
   newSelected.classList.add('selected');
-    if (newSelected === document.getElementsByClassName('color')[0]){
-    inputColor = '#000'
+  if (newSelected === document.getElementsByClassName('color')[0]){
+    inputColor = '#000';
     }
-    else if (newSelected === document.getElementsByClassName('color')[1]) {
-    inputColor = '#0000ff'
+  else if (newSelected === document.getElementsByClassName('color')[1]) {
+    inputColor = '#0000ff';
     }
-    else if (newSelected === document.getElementsByClassName('color')[2]) {
-    inputColor = '#ff0000'
+  else if (newSelected === document.getElementsByClassName('color')[2]) {
+    inputColor = '#ff0000';
     }
-    else if (newSelected === document.getElementsByClassName('color')[3]) {
-    inputColor = '#ffff00'
+  else if (newSelected === document.getElementsByClassName('color')[3]) {
+    inputColor = '#ffff00';
   }
 }
+
 );
 
 
@@ -39,9 +38,10 @@ palletBlocs.addEventListener('click', function sBlock(clickEvent) {
 
 const painting = document.getElementById('pixel-board');
 
-painting.addEventListener('click' ,function paintingBlocks(secondClick){
+painting.addEventListener('click' ,function paintingBlocks(secondClick) {
   const paintingBlock = secondClick.target;
   // let colorChoose = document.getElementsByClassName('color')[0];
   // console.log(colorChoose);
   paintingBlock.style.backgroundColor = inputColor;
+
 });
