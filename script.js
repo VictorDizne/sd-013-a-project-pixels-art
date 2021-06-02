d7b099a4eb0107d8b5b3cb75b260b7f6e0d4199a
ivandosss <ivanndosssilva9389@gmail.com>
2021-06-02 03:16:41 -0300

    4 - Adicione à página um quadro de pixels)

---
+++ script.js
@@ -0,0 +1,21 @@
+window.onload = function test() {
+  let paletaCores = document.getElementsByClassName('color');
+  let colorArray = ['black', 'red', 'blue', 'green'];
+  for (let i = 0; i < 4; i += 1) {
+    paletaCores[i].style.backgroundColor = colorArray[i];
+  }
+  let pixels = document.getElementById('pixel-board');
+  for (let i = 0; i < 5; i += 1) {
+      let horizontal = criaElemento('tr')
+      pixels.appendChild(horizontal)
+      for (let j = 0; j < 5; j += 1) {
+          let vertical = criaElemento('td')
+          vertical.className = 'pixel'
+          pixels.lastElementChild.appendChild(vertical)
+      }
+  }
+  function criaElemento(nome) {
+      let elemento = document.createElement(nome);
+      return elemento;
+  }
+};
