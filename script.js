60be56b400eb2c1a77cce1e6d0134d6b93e493d4
Japa <wesleyy_@live.com>
2021-06-02 18:07:56 -0300

    Adicionado requisitos 1, 2 e 3 do projeto

---
+++ script.js
@@ -0,0 +1,6 @@
+function corDaPeleta (){
+let corPaleta = document.querySelectorAll('.color')
+let cor = document.getElementById("black").getAttribute("data-value");
+  corPaleta[i].style.backgroundColor = cor;
+
+}
