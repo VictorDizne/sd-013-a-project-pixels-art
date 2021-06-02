9f0269dc38c6f9327715b1dba6fb0d725c3d493c
Olavio Timoteo <olaviotimoteo@hotmail.com>
2021-06-01 22:06:52 -0300

    Adiciona requisitos 7 e 8

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
 function createPixelsBoard() {
   for (let index = 0; index < 5; index += 1) {
-    let pixelBoard = document.querySelector('#pixel-board');
-    let line = document.createElement('div');
+    const pixelBoard = document.querySelector('#pixel-board');
+    const line = document.createElement('div');
     pixelBoard.appendChild(line);
 
-    for (let index = 0; index < 5; index += 1) {
+    for (let i = 0; i < 5; i += 1) {
       let div = document.createElement('div');
       div.className = 'pixel';
       line.appendChild(div);
-    };
-  };
-};
+    }
+  }
+}
 
 createPixelsBoard();
+
+function changeFocus(event) {
+  let actualFocus = document.querySelector('.selected');
+  actualFocus.className = 'color';
+  event.target.className = 'color selected';
+};
+let paletteArray = document.querySelectorAll('#color-palette div');
+for (let i = 0; i < paletteArray.length; i += 1) {
+  paletteArray[i].addEventListener('click', changeFocus);
+};
+
+function changePixels(event) {
+  let actualColor = document.querySelector('.selected').style.backgroundColor;
+  event.target.style.backgroundColor = actualColor;
+}
+let pixelsArray = document.querySelectorAll('.pixel');
+for (let i = 0; i < pixelsArray.length; i += 1) {
+  pixelsArray[i].addEventListener('click', changePixels);
+};
