f0d7adae1f5c8badeeb2c804d224fb1ecaf7a4c8
ivandosss <ivanndosssilva9389@gmail.com>
2021-06-02 11:15:54 -0300

    Ajustes de lint, e do pixel-board

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 window.onload = function color() {
   const paletaCores = document.getElementsByClassName('color');
   const colorArray = ['black', 'red', 'blue', 'green'];
+
   for (let i = 0; i < 4; i += 1) {
     paletaCores[i].style.backgroundColor = colorArray[i];
   }
   function criaElemento(nome) {
-    const elemento = document.createElement(nome);
+    let elemento = document.createElement(nome);
     return elemento;
   }
-  const pixels = document.getElementById('pixel-board');
+  let pixels = document.getElementById('pixel-board');
   for (let i = 0; i < 5; i += 1) {
-    const horizontal = criaElemento('tr');
+
+    let horizontal = criaElemento('tr');
     pixels.appendChild(horizontal);
+    
     for (let j = 0; j < 5; j += 1) {
-      const vertical = criaElemento('td');
+      let vertical = criaElemento('td');
       vertical.className = 'pixel';
       pixels.lastElementChild.appendChild(vertical);
     }
