d3795fdce6d40fc75e7c605169a8f16672d5b45e
Ricardo-Sousa <rwmsousa@gmail.com>
2021-06-02 14:44:35 -0300

    requisitos 7 e 8 + ajustes no styles e linter

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 // Cria a paleta de cores inicial
 function palette() {
-  let paletteGrid = document.getElementById('color-palette');
-  let color1 = document.createElement('div');
+  const paletteGrid = document.getElementById('color-palette');
+
+  const color1 = document.createElement('div');
   color1.className = 'color black selected';
   paletteGrid.appendChild(color1);
-  let color2 = document.createElement('div');
+
+  const color2 = document.createElement('div');
   color2.className = 'color yellow';
   paletteGrid.appendChild(color2);
-  let color3 = document.createElement('div');
+
+  const color3 = document.createElement('div');
   color3.className = 'color blue';
   paletteGrid.appendChild(color3);
-  let color4 = document.createElement('div');
+
+  const color4 = document.createElement('div');
   color4.className = 'color red';
   paletteGrid.appendChild(color4);
 }
@@ -19,15 +23,15 @@ palette();
 // Cria o tamanho do board
 function gradeSize() {
   const pixelBoard = document.getElementById('pixel-board');
-  let bordSize = 5;
+  const bordSize = 5;
 
   for (let index = 0; index < bordSize; index += 1) {
-    let line = document.createElement('div');
+    const line = document.createElement('div');
     line.className = 'lines';
     pixelBoard.appendChild(line);
 
     for (let i = 0; i < bordSize; i += 1) {
-      let blocks = document.createElement('div');
+      const blocks = document.createElement('div');
       blocks.className = 'pixel';
       blocks.style.backgroundColor = 'rgb(255, 255, 255)';
       line.appendChild(blocks);
@@ -36,3 +40,43 @@ function gradeSize() {
 }
 gradeSize();
 
+let colorOk = document.querySelector('.selected');
+// Seleciona a cor na paleta
+function colorSelect() {
+  const allColors = document.getElementsByClassName('color');
+
+  for (let i = 0; i < allColors.length; i += 1) {
+    allColors[i].addEventListener('click', (event) => {
+      colorOk.className = colorOk.className.replace(' selected', '');
+      event.target.className += ' selected';
+      colorOk = document.querySelector('.selected');
+    });
+  }
+}
+colorSelect();
+
+// let allBlocks = document.getElementById('pixel');
+
+// function printBlocks() {
+
+//   for (let i = 0; i < allBlocks.length; i += 1) {
+//   allBlocks[i].addEventListener('click', function (event) {
+//     let colorBlock = window.getComputedStyle(colorOk, null).getPropertyValue('background-color');
+//     event.target.style.backgroundColor = colorBlock;
+//   });
+//   }
+// }
+// printBlocks();
+
+function printBlocks() {
+  const allBlocks = document.getElementById('pixel-board');
+
+  allBlocks.addEventListener('click', (event) => {
+    const colorBlock = window
+      .getComputedStyle(colorOk, null)
+      .getPropertyValue('background-color');
+    event.target.style.backgroundColor = colorBlock;
+    console.log(colorOk.backgroundColor);
+  });
+}
+printBlocks();
