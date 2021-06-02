9add3da9f0cc1cc2f687e6b899a2339c062c6f84
rene-t13-trybe <cleytonacompanha@gmail.com>
2021-06-02 13:50:32 -0300

    Desafio 7 (parcial) Projeto Pixels Arts

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
-window.onload
-
 function selectCor() {
-  var a1 = document.getElementsByClassName('color');
-}
+  const colorir = document.getElementsByClassName('color');
+  for (let index = 0; index < colorir.length; index +=1) {
+    colorir[index].addEventListener('click', function () {
+      const selectedCor = document.querySelector('.selected');
+      selectedCor.classList.remove('selected');
+      const clickCor = event.target; 
+      clickCor.classList.add('selected');
+    });
+    } 
+  }
+  selectCor();
+  
+  function selectedBlack() {
+    const criaClasse = document.getElementById('black');
+    criaClasse.classList.add('selected');
+  }
+
+  function classeSelected () {
+    const corPaleta = document.querySelector('#color-palette');
+    const selectedPaleta = document.querySelector('#color-palette .color');
+    corPaleta.addEventListener('click', function (evento) {
+      if (evento.target.classList.contains('color')) {
+      for (let index = 0; index < selectedPaleta.length; index += 1) {
+        selectedPaleta[index].remove('selected');
+      }
+      evento.target.classList.add('selected');
+      selectedPaleta();
+    }
+    });
+  }
+
+  window.onload = function pixels() {
+    selectedBlack();
+    criaClasse();
+  }
