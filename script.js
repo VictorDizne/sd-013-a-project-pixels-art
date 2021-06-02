7450299bd92196402b39f014ad287b6857e554a4
Julio Fallace <julioscoffee@gmail.com>
2021-06-02 18:00:52 -0300

    resolvendo requisito 7

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
+const colorPalette = document.querySelector('#color-palette');
+const colorBlack = document.querySelector('#black');
+const colorBlue = document.querySelector('#blue');
+const colorRed = document.querySelector('#red');
+const colorGreen = document.querySelector('#green');
+
 const pixelBoard = document.querySelector('#pixel-board');
+const tabela = document.querySelector('#table');
+
 let tabelaTd = '<td class="pixel"></td>';
 for (let i = 0; i < 4; i += 1) {
   tabelaTd += '<td class="pixel"></td>';
@@ -9,4 +17,14 @@ for (let i = 0; i < 2; i += 1) {
   aux += tabelaTd + tabelaTd;
 }
 tabelaTd = aux;
-pixelBoard.innerHTML = `<table> ${tabelaTd} </table>`;
+pixelBoard.innerHTML = `<table id="table"> ${tabelaTd} </table>`;
+
+function seleciona(event) {
+  const classSelected = document.querySelector('.selected');
+  classSelected.classList.remove('selected');
+  event.target.classList.add('selected');
+}
+colorBlack.addEventListener('click', seleciona);
+colorBlue.addEventListener('click', seleciona);
+colorRed.addEventListener('click', seleciona);
+colorGreen.addEventListener('click', seleciona);
