b789e1a6a942a5287a7d2c11f867de34a8f9c86d
Filipe <filipecandido123@hotmail.com>
2021-06-02 17:54:40 -0300

    exercicio 8 feito

--- script.js
+++ script.js
@@ -1,52 +1,145 @@
-const div1 = document.querySelector(".color")
-const div2 = document.querySelector(".color").nextElementSibling
-const div3 = document.querySelector(".color").nextElementSibling.nextElementSibling
-const div4 = document.querySelector(".color").nextElementSibling.nextElementSibling.nextElementSibling
+const div1 = document.querySelector("#black");
+div1.classList.add("selected")
+const div2 = document.querySelector("#red")
+const div3 = document.querySelector("#yellow")
+const div4 = document.querySelector("#green")
+
 //cores fixas com um selecionador
-function corPreta() {
-  div1.style.backgroundColor = "black"
-  div1.classList.add("selected")
-}
-corPreta()
 
-function corVermelha() {
-  div2.style.backgroundColor = "red"
+//criação dos blocoDiv
+function createPixels(n=5) {
+  for (let index = 0; index < n; index += 1) {
+    const quadrado = document.createElement('div');
+    const pixelBoard = document.querySelector('#pixel-board');
+    pixelBoard.appendChild(quadrado);
+    quadrado.classList = 'linha';
+    for (let index2 = 0; index2 < n; index2 += 1) {
+      const quadradinho = document.createElement('div');
+      quadradinho.classList = 'pixel';
+      quadrado.appendChild(quadradinho);
+    }
+  }
 }
-corVermelha()
+createPixels();
+/*quando estou no 0, eu coloco a primeira linha no for 2 eu rodo 5 vezes criando 5 quadrados
+quando estou no 1, eu coloco a segunda linha do for, no 2 for  eu rodo 5 vezes criando 5 quadrados
+quando estou no 2, eu coloco a terceira linha do for, no 3 for eu rodo 5 vezes cirando 5 quadrados.
+quando estou no 3 , eu coloco a quarta linha do for, no 4 for eu rodo 5 vezes criando 5 quadrados.
+quando estou no 4, eu colco a 5 linha do for, no 4 for eu rodo 5 vezes criando 5 quadrados.
+aprendi no plantao que nao posso por a classe pixel no primeiro for porque o primeiro for seriam as linhas criadas.
+requisito baseado no exercício da trybe bloco 4.3 exercício 1, lá tem 25 asteriscos também, fiz a troca pela div.
+joguei como filha para ficar embaixo */
 
-function corAzul() {
-  div3.style.backgroundColor = "blue"
-}
-corAzul()
-
-function corRosa() {
-  div4.style.backgroundColor = "pink"
-}
-corRosa()
-//criação dos blocoDIv
-let AddDivs = document.getElementById("pixel-board")
-
-for (let index = 0; index < 25; index+= 1) {
-  let createBox = document.createElement("div")
-  createBox.hidden = AddDivs[index]
-  createBox.className = "pixel"
-  AddDivs.appendChild(createBox);
-}
+//codigo de aline hoshino
 //evento que seleciona as paletas de cores
+
+
 function mudaDiv(event) {
-  const elementoTech = document.querySelector('.selected');
-  elementoTech.classList.remove('selected');
+  const elemento = document.querySelector('.selected');
+  elemento.classList.remove('selected');
   event.target.classList.add('selected');
 }
-
 div1.addEventListener("click", mudaDiv)
 div2.addEventListener("click", mudaDiv)
 div3.addEventListener("click", mudaDiv)
 div4.addEventListener("click", mudaDiv)
 
-let boxsDePinturas = document.querySelectorAll(".pixel")
+//era pra pintar
+const pixels = document.querySelectorAll(".pixel");
+const pixelBoard = document.querySelector("#pixel-board");
 
-// boxsDePinturas.forEach(function(cor) {
-//   let buscaPalletas = document.querySelectorAll(".cores");
-//   buscaPalletas
+function pincel(event) {
+  const evento = event;
+  if(evento.target.classList.contains("pixel")){
+  const selectedElement = document.querySelector('.selected');
+  const SelectColor = window.getComputedStyle(selectedElement, null). getPropertyValue('background-color');
+  evento.target.style.backgroundColor = SelectColor;
+  }
+}
+
+pixelBoard.addEventListener("click", pincel)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function setBackground(element) {
+//   const pixelElement = element;
+//   const selectElement = document.getElementsByClassName('selected')[0];
+
+//   pixelElement.style.backgroundColor = selectElement.style.backgroundColor;
+
+//   return pixelElement;
+// }
+
+// function drawColor() {
+//   const pixelsListElements = document.getElementsByClassName('pixel');
+
+//   for (let elementIndex = 0; elementIndex < pixelsListElements.length; elementIndex += 1 ) {
+//     const pixelElement = pixelsListElements[elementIndex];
+//     pixelElement.addEventListener('click', () => setBackground(pixelElement));
+//   }
+// }
+
+// function clearButton() {
+//   const buttonElement = document.getElementById('clear-board');
+//   const pixelBoardItems = document.getElementsByClassName('pixel');
+
+//   buttonElement.addEventListener('click', () => {
+//     for (let index = 0; index < pixelBoardItems.length; index += 1) {
+//       pixelBoardItems[index].style.backgroundColor = 'white';
+//     }
+//   });
+// }
+
+// function createPixel() {
+//   const pixelElement = document.createElement('li');
+//   pixelElement.className = 'pixel';
+//   pixelElement.addEventListener('click', () => setBackground(pixelElement));
+
+//   return pixelElement;
 // }
