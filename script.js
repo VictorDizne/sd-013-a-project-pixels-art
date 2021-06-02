42bb4ae48994d3d5b021021f4284e22244f26e3f
ArthurMGoulart <budiarthur@gmail.com>
2021-06-02 15:33:42 -0300

    Realizando requisitos 9 e 10

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ function unselectAllColors(){
 function setListenersOfPixels(){
   let pixels = document.querySelectorAll(".pixel");
   for(let pixel of pixels){
-    pixel.addEventListener("click", function (event){
+    pixel.addEventListener("click", function (event) {
       let colorToPaint = currentSelectedColor();
       pixel.style.backgroundColor = colorToPaint;
     })
@@ -41,12 +41,20 @@ function createPixel(){
   pixel.style.backgroundColor = "white";
   return pixel;
 }
-function createBoard(){
+
+function ajustBoardSize(board, n){
+  let boardWidth = (n * 40) + 50;
+  let boardHeight = (n * 40) + 50; 
+  board.style.width = `${boardWidth}px`;
+  board.style.height = `${boardHeight}px`;
+}
+function createBoard(n){
   let board = document.querySelector("#pixel-board");
-  for (let row = 0; row < 5; row += 1) {
+  ajustBoardSize(board, n);
+  for (let row = 0; row < n; row += 1) {
     let divRow = document.createElement("div");
     divRow.className = "row";
-    for(let column = 0; column < 5; column += 1) {
+    for(let column = 0; column < n; column += 1) {
       let pixel = createPixel();
       divRow.appendChild(pixel);
     }
@@ -64,5 +72,32 @@ function currentSelectedColor(){
   alert("erro");
   return "white";
 }
+function setButtonCleanBoard(){
+  let button = document.querySelector("#clear-board");
+  let board = document.querySelector("#pixel-board");
+  button.addEventListener("click", function (event) {
+    let rows = board.children;
+    for(let row of rows){
+      let pixels = row.children;
+      for(let pixel of pixels){
+        pixel.style.backgroundColor = "white";
+    }
+  }
+  });
+}
+function setButtonCreateBoard(){
+  let board = document.querySelector("#pixel-board");
+  let button = document.querySelector
+  ("#generate-board");
+  let input = document.querySelector("#board-size");
+  button.addEventListener("click", function (event) {
+    // Retira o tabuleiro antigo, artigo de referência https://stackoverflow.com/questions/3955229/remove-all-child-elements-of-a-dom-node-in-javascript.
+    board.innerHTML = '';
+    let n = input.value;
+    createBoard(n);
+  })
+}
+setButtonCreateBoard();
+setButtonCleanBoard();
 setColors();
-createBoard();
+createBoard(5);
