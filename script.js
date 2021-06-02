5df665ec37fb2a877a6d22fd1b9d1397433ecd90
Micael Santos <micaelmaicon@hotmail.com>
2021-06-02 11:17:46 -0300

    Finilize requirement 7

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
-// Iniciando o projeto
+// Iniciando o projeto.
+const colorBlack = document.getElementById('colorBlack');
+colorBlack.style.backgroundColor = 'black';
+const colorRed = document.getElementById('colorRed');
+colorRed.style.backgroundColor = 'Red';
+const colorBlue = document.getElementById('colorBlue');
+colorBlue.style.backgroundColor = 'Blue';
+const colorGreen = document.getElementById('colorGreen');
+colorGreen.style.backgroundColor = 'Green';
+
+//  Requiremet 7.
+// adicionar a classe `classe selected` a cor selecionada e retira a mesma classe da outra cor
+colorBlack.addEventListener('click', colorSelectBlack);
+colorRed.addEventListener('click', colorSelectRed);
+colorBlue.addEventListener('click', colorSelectBlue);
+colorGreen.addEventListener('click', colorSelectGreen);
+
+function colorSelectBlack() {
+  colorRed.classList.remove('selected');
+  colorBlue.classList.remove('selected');
+  colorGreen.classList.remove('selected');
+  colorBlack.classList.add('selected');
+  }
+function colorSelectRed() {
+  colorBlack.classList.remove('selected');
+  colorBlue.classList.remove('selected');
+  colorGreen.classList.remove('selected');
+  colorRed.classList.add('selected');
+  }
+
+function colorSelectBlue() {
+  colorBlack.classList.remove('selected');
+  colorRed.classList.remove('selected');
+  colorGreen.classList.remove('selected');
+  colorBlue.classList.add('selected');
+  }
+
+function colorSelectGreen() {
+  colorBlack.classList.remove('selected');
+  colorRed.classList.remove('selected');
+  colorBlue.classList.remove('selected');
+  colorGreen.classList.add('selected');
+  }
