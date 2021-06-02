88f0343714f1b497ed0625753c4a2f03409465d3
Bruno Pinho <brunopinho@outlook.com>
2021-06-01 21:21:10 -0300

    iniciado requisito 8

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
+// Códigos referente a paleta de cores:
 const colors = document.querySelectorAll('.color');
 const pixelBoard = document.querySelector('#pixel-board');
 
 window.onload = function() {
   colors[0].classList.add('selected');
 }
+
+/* Solução dinâmica para adicionar a classe selected (falta conseguir remover das outras cores):
+for (let index = 0; index < colors.length; index += 1) {
+  colors[index].addEventListener('click', function (event) {
+    event.target.classList.add('selected');
+  })
+}
+*/
 
 colors[0].addEventListener('click', function (event) {
   event.target.classList.add('selected');
@@ -38,7 +47,7 @@ colors[3].addEventListener('click', function (event) {
   colors[2].classList.remove('selected')
 })
 
-
+//Códigos referentes ao quadro de pixels:
 
 function createPixels(number) {
   for (let indexRow = 0; indexRow < number; indexRow += 1) {
@@ -51,3 +60,13 @@ function createPixels(number) {
 }
 
 createPixels(5);
+
+const pixels = document.querySelectorAll('.pixel')
+const colorSelected = document.querySelector('.selected');
+const color1 = document.querySelectorAll('.color')[0];
+
+for (let index = 0; index < pixels.length; index += 1) {
+  pixels[index].addEventListener('click', function (event) {
+    event.target.style.backgroundColor = "black";
+  })
+}
