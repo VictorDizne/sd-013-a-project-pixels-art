0602c0003a699760843ab57cc46b5c5f21f20676
Mario <mjfneto@gmail.com>
2021-06-02 12:44:13 -0300

    Faz o quadro de pixels ter seu tamanho definido pelo usuario

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const palette = ['black', 'red', 'green', 'blue'];
-const boardWidth = 5;
-const boardLength = 5;
+let boardWidth = 5;
+let boardLength = boardWidth;
+let boardSize = pow(boardWidth);
 const defaultPixelColor = '#fff';
 let selectedColor = palette[0];
 
@@ -8,40 +9,54 @@ const container = new dom('.container');
 const colorPalette = create('div', undefined, { id: 'color-palette' });
 const board = create('div', undefined, { id: 'pixel-board' });
 const clearBtn = create('button', 'Limpar', { id: 'clear-board' });
+const boardSizeWrapper = create('div', undefined, {
+  class: 'board-size-wrapper',
+});
+const boardSizeInput = create('input', undefined, {
+  id: 'board-size',
+  type: 'number',
+  min: 1,
+});
+const genBoardBtn = create('button', 'VQV', { id: 'generate-board' });
 
-for (let i = 0; i < palette.length; i += 1) {
-  const colorClassList = i == 0 ? 'color selected' : 'color';
-  const colorAttributes = { class: colorClassList };
+fillColors();
+fillBoard();
 
-  const colorSelection = create('div', undefined, colorAttributes);
-  changeStyle(colorSelection)('backgroundColor', palette[i]);
-  colorPalette.appendChild(colorSelection);
-}
-
-container.append(colorPalette)();
-container.append(clearBtn)();
-
-for (let i = 0; i < boardWidth; i += 1) {
-  const line = create('div', undefined, { class: 'line' });
-  for (let j = 0; j < boardLength; j += 1) {
-    const pixel = create('div', undefined, { class: 'pixel' });
-    changeStyle(pixel)('backgroundColor', defaultPixelColor);
-    line.appendChild(pixel);
-  }
-  board.appendChild(line);
-}
-
-container.append(board)();
+append(boardSizeWrapper)([boardSizeInput, genBoardBtn])();
+container.append([colorPalette, clearBtn, boardSizeWrapper, board])();
 
 sel('#color-palette').on('click', cycleToSelected);
-
 sel('#pixel-board').on('click', function (e) {
   changeStyle(e.target)('backgroundColor', selectedColor);
 });
-
-sel('#clear-board').on('click', clearBoard);
+sel('#clear-board').on('click', cleanBoard);
+sel('#board-size').on('change', handleSizeChange);
+sel('#generate-board').on('click', generateBoard);
 
 // ************************
+
+function fillColors() {
+  for (let i = 0; i < palette.length; i += 1) {
+    const colorClassList = i == 0 ? 'color selected' : 'color';
+    const colorAttributes = { class: colorClassList };
+
+    const colorSelection = create('div', undefined, colorAttributes);
+    changeStyle(colorSelection)('backgroundColor', palette[i]);
+    colorPalette.appendChild(colorSelection);
+  }
+}
+
+function fillBoard() {
+  for (let i = 0; i < boardWidth; i += 1) {
+    const line = create('div', undefined, { class: 'line' });
+    for (let j = 0; j < boardLength; j += 1) {
+      const pixel = create('div', undefined, { class: 'pixel' });
+      changeStyle(pixel)('backgroundColor', defaultPixelColor);
+      line.appendChild(pixel);
+    }
+    append(board)(line)();
+  }
+}
 
 function cycleToSelected(e) {
   const colors = e.currentTarget.children;
@@ -61,7 +76,17 @@ function cycleToSelected(e) {
   }
 }
 
-function clearBoard(e) {
+function generateBoard(e) {
+  if (boardSizeInput.value.trim() == '') {
+    window.alert('Board inválido!');
+    return;
+  }
+
+  clear(board)();
+  fillBoard();
+}
+
+function cleanBoard(e) {
   const board = e.target.nextElementSibling.children;
 
   for (let line of board) {
@@ -70,4 +95,23 @@ function clearBoard(e) {
       selectedColor = palette[0];
     }
   }
+}
+
+function handleSizeChange(e) {
+  if (e.target.value.trim() !== '') {
+    updateDimensions(e.target.value);
+    return;
+  }
+
+  e.preventDefault();
+}
+
+function updateDimensions(n) {
+  boardSize = pow(n, 2);
+  boardWidth = n;
+  boardLength = n;
+}
+
+function pow(base, exponent) {
+  return Math.pow(base, exponent);
 }
