0206c5d2cd16614793f5847850f342e16722305b
Douglas dos Santos <doug.frutuoso@gmail.com>
2021-06-02 13:59:45 -0300

    desafio 7 resolvido

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 
-
-
 function gradePixel(){
 let board = document.querySelector("#pixel-board");
 let quadrado = 5;
@@ -24,3 +22,21 @@ for(index =0; index< quadrado; index+=1){
 
 gradePixel();
 
+function classSelect(event){
+
+let selectedELement = document.querySelector(".selected");
+    selectedELement.classList.remove("selected");
+
+    event.target.classList.add("selected");    
+
+}
+
+let elementColor1 = document.querySelectorAll(".color")[0];
+let elementColor2 = document.querySelectorAll(".color")[1];
+let elementColor3 = document.querySelectorAll(".color")[2];
+let elementColor4 = document.querySelectorAll(".color")[3];
+
+elementColor1.addEventListener('click' , classSelect);
+elementColor2.addEventListener('click' , classSelect);
+elementColor3.addEventListener('click' , classSelect);
+elementColor4.addEventListener('click' , classSelect);
