3159d98a0462723b1936ef7b64a68fa4bcbfa7d5
Debora Teodorico <debora.steodorico@gmail.com>
2021-06-02 10:45:47 -0300

    Adiciona a questão 7

--- script.js
+++ script.js
@@ -2,4 +2,23 @@ function selecionaCorPreta() {
   const criarClasse = document.getElementById('black');
   criarClasse.classList.add('selected');
 }
-window.onload = selecionaCorPreta;
+
+function classeSelecionada() {
+  const corDaPaleta = document.querySelector('#color-palette');
+  const selecionaPaletaFilhos = document.querySelectorAll('#color-palette .color');
+  corDaPaleta.addEventListener('click', function (evento) {
+    if (evento.target.classList.contains('color')) {
+      for (let i = 0; i < selecionaPaletaFilhos.length; i += 1) {
+        selecionaPaletaFilhos[i].remove('selected');
+      }
+      evento.target.classList.add('selected');
+      console.log(selecionaPaletaFilhos);
+    }
+  });
+}
+
+window.onload = function projeto() {
+  selecionaCorPreta();
+  classeSelecionada();
+
+};
