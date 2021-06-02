b2cc9803c60a36423ba722128d74baf4b4f04623
Mario <mjfneto@gmail.com>
2021-06-01 22:45:53 -0300

    Cria um botao de clique para limpar o quadro

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const palette = ['black', 'red', 'green', 'blue'];
 const boardWidth = 5;
 const boardLength = 5;
+const defaultPixelColor = '#fff';
 let selectedColor = palette[0];
 
 const container = new dom('.container');
 const colorPalette = create('div', undefined, { id: 'color-palette' });
 const board = create('div', undefined, { id: 'pixel-board' });
+const clearBtn = create('button', 'Limpar', { id: 'clear-board' });
 
 for (let i = 0; i < palette.length; i += 1) {
   const colorClassList = i == 0 ? 'color selected' : 'color';
@@ -17,12 +19,13 @@ for (let i = 0; i < palette.length; i += 1) {
 }
 
 container.append(colorPalette)();
+container.append(clearBtn)();
 
 for (let i = 0; i < boardWidth; i += 1) {
   const line = create('div', undefined, { class: 'line' });
   for (let j = 0; j < boardLength; j += 1) {
     const pixel = create('div', undefined, { class: 'pixel' });
-    changeStyle(pixel)('backgroundColor', '#fff');
+    changeStyle(pixel)('backgroundColor', defaultPixelColor);
     line.appendChild(pixel);
   }
   board.appendChild(line);
@@ -35,6 +38,8 @@ sel('#color-palette').on('click', cycleToSelected);
 sel('#pixel-board').on('click', function (e) {
   changeStyle(e.target)('backgroundColor', selectedColor);
 });
+
+sel('#clear-board').on('click', clearBoard);
 
 // ************************
 
@@ -52,6 +57,17 @@ function cycleToSelected(e) {
     if (isTarget && !isSelected) {
       colors[i].classList.add('selected');
       selectedColor = palette[i];
+    }
+  }
+}
+
+function clearBoard(e) {
+  const board = e.target.nextElementSibling.children;
+
+  for (let line of board) {
+    for (let pixel of line.children) {
+      changeStyle(pixel)('backgroundColor', defaultPixelColor);
+      selectedColor = palette[0];
     }
   }
 }
