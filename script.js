337e33fd5bf55172fb9c311298174f2cd6f7cb5f
Ramon Vitor <vitorramon@hotmail.com>
2021-06-01 23:49:40 -0300

    Corrigindo indentação do style.css e script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function quadroDePixels() {
-    let quadro = document.createElement('div');
-    quadro.id = 'pixel-board';
-    document.body.appendChild(quadro);
+  const quadro = document.createElement('div');
+  quadro.id = 'pixel-board';
+  document.body.appendChild(quadro);
 
-    for (let i = 0; i < 5; i += 1){
-        let pixelLinha = document.createElement('div');
-        pixelLinha.className = 'pixel-linha';
-        quadro.appendChild(pixelLinha);
-        for (let j = 0; j < 5; j += 1){
-            let pixelColuna = document.createElement('div');
-            pixelColuna.className = 'pixel';
-            pixelLinha.appendChild(pixelColuna);
-        }
+  for (let i = 0; i < 5; i += 1) {
+    const pixelLinha = document.createElement('div');
+    pixelLinha.className = 'pixel-linha';
+    quadro.appendChild(pixelLinha);
+    for (let j = 0; j < 5; j += 1) {
+        const pixelColuna = document.createElement('div');
+        pixelColuna.className = 'pixel';
+        pixelLinha.appendChild(pixelColuna);
     }
+  }
 }
 quadroDePixels();
