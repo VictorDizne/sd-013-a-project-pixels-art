bbd6f9b4847b48b5dfbdfb2b8d5da99598d83fec
Andrey Gois <gois.andrey@hotmail.com>
2021-06-01 22:15:58 -0300

    Adiciona até a questão 8

---
+++ script.js
@@ -0,0 +1,34 @@
+function createPixelsBoard() {
+  for (let index = 0; index < 5; index += 1) {
+    const pixelBoard = document.querySelector('#pixel-board');
+    const line = document.createElement('div');
+    pixelBoard.appendChild(line);
+
+    for (let index = 0; index < 5; index += 1) {
+      let div = document.createElement('div');
+      div.className = 'pixel';
+      line.appendChild(div);
+    }
+  }
+}
+createPixelsBoard();
+
+
+function changeFocus(event) {
+  let actualFocus = document.querySelector('.selected');
+  actualFocus.className = 'color';
+  event.target.className = 'color selected';
+};
+let paletteArray = document.querySelectorAll('#color-palette div');
+for (let i = 0; i < paletteArray.length; i += 1) {
+  paletteArray[i].addEventListener('click', changeFocus);
+};
+
+function changePixels(event) {
+  let actualColor = document.querySelector('.selected').style.backgroundColor;
+  event.target.style.backgroundColor = actualColor;
+}
+let pixelsArray = document.querySelectorAll('.pixel');
+for (let i = 0; i < pixelsArray.length; i += 1) {
+  pixelsArray[i].addEventListener('click', changePixels);
+};
