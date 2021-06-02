8ea51707e65851a1577cd9efb5cb834612510888
Jéssica Queiroz <gnr0034@gmail.com>
2021-06-02 10:17:19 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -5,13 +5,28 @@ function selectedBlack () {
 
 function initialPixelColor (color) {
   let initialPixel = document.getElementsByClassName('pixel');
-  for(i=0; i < initialPixel.length; i+=1){
+  for(i = 0; i < initialPixel.length; i += 1){
     initialPixel[i].style.background = color;
   }
 }
 
+function selectedColor () {
+  let arrayColorPalette = document.querySelector('#color-palette');
+  let arrayColors = document.getElementsByClassName('color');
+  arrayColorPalette.addEventListener('click', function(e) {
+    if(e.target.classList.contains('color')) {
+      for(let i = 0; i < arrayColors.length; i += 1) {
+        arrayColors[i].classList.remove('selected');
+      }
+      e.target.classList.add('selected');
+    }
+  });
+}
+
+selectedColor();
+
 window.onload = function () {
   selectedBlack();
   initialPixelColor('white');
- 
+  selectedColor();
 }
