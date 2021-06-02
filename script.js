c6b2b24edd54b89275cd8fc2621842af134183fc
Daniel <daniel.melo.oliveira27.com.br>
2021-06-01 23:33:49 -0300

    Requisitos 7 ao 12

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
 let currentSelectedColor = 'rgb(0, 0, 0)';
 let pixelBoardNumber = 1;
+let elementos;
 
 function clicouCor(evento) {
+  for (let elemento of elementos) {
+    elemento.onclick = clicouCor;
+    if (elemento.classList.contains('selected')) {
+      elemento.classList.remove('selected');
+    }
+  }
   const paletteElement = evento.target;
+  paletteElement.classList.add('selected');
   const paletteElementBackgroundColor =
     window.getComputedStyle(paletteElement).backgroundColor;
   currentSelectedColor = paletteElementBackgroundColor;
 }
 
 function onLoadRefreshColors() {
-  let elementos = document.querySelectorAll('.color');
+  elementos = document.querySelectorAll('.color');
+  pixels = document.querySelectorAll('.pixel');
   for (let elemento of elementos) {
     elemento.onclick = clicouCor;
     if (elemento.id !== 'color1') {
       elemento.style.backgroundColor = generateRgbColor();
     }
+  }
+  for (let pixel of pixels) {
+    pixel.onclick = applyColor;
   }
 }
 
@@ -30,25 +42,45 @@ function refreshColors() {
 
 function numberPixelBoard() {
   const divs = [];
-  pixelBoardNumber = Number(document.getElementById('input').value);
+  pixelBoardNumber = Number(document.getElementById('board-size').value);
   const pixelBoard = document.getElementById('pixel-board');
-  if (pixelBoardNumber >= 5) {
-    for (let i = 0; i < pixelBoardNumber; i++) {
-      const div = document.createElement('div');
-      div.className = 'line';
-      for (let j = 0; j < pixelBoardNumber; j++) {
-        const pixelDiv = document.createElement('div');
-        pixelDiv.className = 'pixel';
-        div.appendChild(pixelDiv);
-      }
-      divs.push(div);
-    }
-    pixelBoard.innerHTML = '';
-    for (let i = 0; i < divs.length; i++) {
-      pixelBoard.appendChild(divs[i]);
-    }
+  if (pixelBoardNumber == '' || pixelBoardNumber < 0) {
+    alert('Board inválido!');
   }
-  console.log(divs);
+  if (pixelBoardNumber > 50) {
+    pixelBoardNumber = 50;
+  } else if (pixelBoardNumber < 5) {
+    pixelBoardNumber = 5;
+  }
+  for (let i = 0; i < pixelBoardNumber; i++) {
+    const div = document.createElement('div');
+    div.className = 'line';
+    for (let j = 0; j < pixelBoardNumber; j++) {
+      const pixelDiv = document.createElement('div');
+      pixelDiv.className = 'pixel';
+      pixelDiv.onclick = applyColor;
+      div.appendChild(pixelDiv);
+    }
+    divs.push(div);
+  }
+  pixelBoard.innerHTML = '';
+  for (let i = 0; i < divs.length; i++) {
+    pixelBoard.appendChild(divs[i]);
+  }
+}
+
+function applyColor(evento) {
+  const pixelElement = evento.target;
+  pixelElement.style.backgroundColor = currentSelectedColor;
+  console.log(pixelElement);
+  console.log(currentSelectedColor);
+}
+
+function refreshBoard() {
+  const refresh = document.getElementsByClassName('pixel');
+  for (const i of refresh) {
+    i.style.backgroundColor = 'white';
+  }
 }
 
 onLoadRefreshColors();
