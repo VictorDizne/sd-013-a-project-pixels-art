0ea0c1ecd09feabf49ee42f64c827375dd46a091
Mario <mjfneto@gmail.com>
2021-06-01 21:08:30 -0300

    Adiciona a pagina um quadro de pixels, com 25 pixels

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const palette = ['black', 'red', 'green', 'blue'];
+const boardWidth = 5;
+const boardLength = 5;
 
 const container = new dom('.container');
 const colorPalette = create('div', undefined, { id: 'color-palette' });
+const board = create('div', undefined, { id: 'pixel-board' });
 
 for (let color of palette) {
   const colorSelection = create('div', undefined, { class: 'color' });
@@ -10,3 +13,15 @@ for (let color of palette) {
 }
 
 container.append(colorPalette)()
+
+for (let i = 0; i < boardWidth; i += 1) {
+  const line = create('div', undefined, { class: 'line' });
+  for (let j = 0; j < boardLength; j += 1) {
+    const pixel = create('div', undefined, { class: 'pixel' });
+    changeStyle(pixel)('backgroundColor', '#fff');
+    line.appendChild(pixel);
+  }
+  board.appendChild(line);
+}
+
+container.append(board)();
