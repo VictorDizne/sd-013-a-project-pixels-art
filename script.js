0e04b54269e4fd7d9d183956995298e62bb5fcd3
Breno Lopes <brenokos4@gmail.com>
2021-06-02 15:11:49 -0300

    conserta erros

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // função para adicionar linhas e colunas no pixel-board!
 const tbody = document.querySelector('#pixel-board tbody');
-const trElem = document.getElementsByClassName('pixel');
-const tdElem = document.querySelectorAll('.pixel td');
+const trElem = document.getElementsByClassName('coluna');
+const tdElem = document.querySelectorAll('.coluna td');
 
 function addPixels(numCol = 5, numLin = 5) {
   for (let i = 0; i <= numCol - 1; i += 1) {
     const tr = document.createElement('tr');
-    tr.className = 'pixel';
+    tr.className = 'coluna';
     tbody.appendChild(tr);
     for (let o = 0; o <= numLin - 1; o += 1) {
       const td = document.createElement('td');
@@ -15,12 +15,15 @@ function addPixels(numCol = 5, numLin = 5) {
   }
 }
 
-function addClass() {
-  const item = document.querySelectorAll('.pixel td');
+function addClass(Array, classe) {
+  // essa função adiciona classes em itens de array.
+  const item = Array;
   for (i in item) {
-    item[i].className = 'pixel';
+    item[i].className = classe;
   }
 }
+
 addPixels();
-addClass();
+addClass(document.querySelectorAll('.coluna td'), 'pixel');
+
 console.log(tdElem);
