6cb6ab637bec6e107c8abc228b2c552bcbafe219
Felipe Dias <felipeferreiradias1@gmail.com>
2021-06-02 00:05:36 -0300

    adiciona parte II requisito 6

--- script.js
+++ script.js
@@ -1,40 +1,22 @@
 window.onload = function () {
+  document.getElementById('colorOne').className = 'selected';
+};
 
 let colorOne = document.getElementById('colorOne'); 
 let colorTwo = document.getElementById('colorTwo'); 
 let colorThree = document.getElementById('colorThree'); 
 let colorFour = document.getElementById('colorFour'); 
 
-document.querySelector('color').className = 'selected';
-
 colorOne.addEventListener('click', selected);
 colorTwo.addEventListener('click', selected);
 colorThree.addEventListener('click', selected);
 colorFour.addEventListener('click', selected);
 
-  
-  function selected(evento) {
-    
-    if (evento.target == colorOne) {
-      colorOne.className = "selected";
-      colorTwo.className = "color";
-      colorThree.className = "color";
-      colorFour.className = "color";
-    } else if (evento.target == colorTwo) {
-      colorOne.className = "color";
-      colorTwo.className = "selected";
-      colorThree.className = "color";
-      colorFour.className = "color";
-    } else if (evento.target == colorThree) {
-      colorOne.className = "color";
-      colorTwo.className = "color";
-      colorThree.className = "selected";
-      colorFour.className = "color";
-    } else {
-      colorOne.className = "color";
-      colorTwo.className = "color";
-      colorThree.className = "color";
-      colorFour.className = "selected";
-    }
-  }
+function selected(event) {
+  const colorElement = document.querySelector('.color');
+  colorElement.classList.remove('selected');
+  event.target.classList.add('selected');
+  input.value = 'color';
 }
+
+
