4928b687116d8155297080b56636d11a308610fc
Lucas Pinheiro <lucaspinheiroalberdanha@gmail.com>
2021-06-02 20:26:45 -0300

    requisito 8

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-var */
+/* eslint-disable max-len */
 /* eslint-disable complexity */
 /* eslint-disable no-loop-func */
 /* eslint-disable max-lines-per-function */
@@ -6,44 +8,65 @@
 /* eslint-disable func-names */
 /* eslint-disable no-param-reassign */
 /* eslint-disable prefer-arrow-callback */
+var color = 'black';
+
+function generatePalette() {
+  const arrayPalette = ['black', 'red', 'green', 'blue'];
+  for (let index = 0; index < arrayPalette.length; index += 1) {
+    const element = document.getElementById('color-palette');
+    const newElement = document.createElement('li');
+    if (arrayPalette[index] === 'black') {
+      newElement.className = 'color selected';
+      newElement.style.backgroundColor = 'black';
+    } else if (arrayPalette[index] === 'red') {
+      newElement.className = 'color';
+      newElement.style.backgroundColor = 'red';
+    } else if (arrayPalette[index] === 'green') {
+      newElement.className = 'color';
+      newElement.style.backgroundColor = 'green';
+    } else if (arrayPalette[index] === 'blue') {
+      newElement.className = 'color';
+      newElement.style.backgroundColor = 'blue';
+    }
+    element.appendChild(newElement);
+  }
+}
+generatePalette();
+
 function setColorClass() {
   const elementToClick = document.querySelectorAll('.color');
   const selectedClass = document.getElementsByClassName('selected');
-  const color = 'color';
-  const select = 'color selected';
+  const classColor = 'color';
+  const classSelect = 'color selected';
   for (let index = 0; index < elementToClick.length; index += 1) {
     elementToClick[index].addEventListener('click', function(event) {
       if (selectedClass.length === 0) {
-        event.target.className = select;
+        event.target.className = classSelect;
       } else {
-        event.target.className = color;
+        event.target.className = classColor;
       }
     });
   }
 }
-
 setColorClass();
 
-function setPixelColor() {
-  const selectedColor = document.getElementsByTagName('li');
-  const selectedClassColor = document.querySelector('.selected');
+function setColorByClass() {
+  const selectedClass = document.querySelector('.selected');
+  color = selectedClass.style.backgroundColor;
+}
+
+function setPixelColorSelected() {
   const pixelTable = document.querySelectorAll('.pixel');
-  let color = null;
   for (let index = 0; index < pixelTable.length; index += 1) {
     pixelTable[index].addEventListener('click', function(event) {
       const targetColor = event.target.style.backgroundColor;
-      for (let index1 = 0; index < selectedColor.length; index += 1) {
-        if (selectedColor[index1].className === selectedClassColor.className) {
-          color = selectedColor[index1].style.backgroundColor;
-        }
-      }
+      setColorByClass();
       if (targetColor !== color) {
         event.target.style.backgroundColor = color;
-      } else if (targetColor === color) {
-        event.target.style.backgroundColor = 'rgb(119,119,119)';
+      } else {
+        event.target.style.backgroundColor = 'white';
       }
     });
   }
 }
-
-setPixelColor();
+setPixelColorSelected();
