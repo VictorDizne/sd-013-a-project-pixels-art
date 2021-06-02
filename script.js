6d78d96d6b012d1fdcb4b33e2e492b9c78f4da14
Pedro Muniz <pedro.muniz@aluno.uece.br>
2021-06-02 16:11:14 -0300

    att6

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-function addSelect (){
+/* function addSelect (){
   let element = document.getElementsByClassName('color')
 
   for (let i = 0; i < element.length; i++){
     element[i].classList.remove('selected')
   }
 
-  this.classList.add('selected')
-}
+  element.addEventListener('click', addSelect)
+  /* this.classList.add('selected')
+  document.getElementsByClassName('color').addEventListener("click", addSelect)
+} */
 
-document.getElementsByClassName('color').addEventListener("click", addSelect)
+
+
+/* document.getElementsByClassName('color').addEventListener("click", addSelect) */ 
+
+function addSelected (event){
+  let x = event.target;
+  let element = document.getElementsByClassName('color')
+
+  for (let i = 0; i < element.length; i++){
+    element[i].classList.remove('selected')
+  }
+
+  x.classList.add('selected')
+} 
