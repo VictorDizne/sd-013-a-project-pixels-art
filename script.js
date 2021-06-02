5facf137b7fb17b0d742e779fe394d2dc9e09c0d
Wesley Maia <maia.weol@gmail.com>
2021-06-01 21:47:26 -0300

    Requisito 7- Clicar em uma das cores da paleta faz com que ela seja selecionada e utilizada para preencher os pixels no quadro

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 window.onload = function () {
   const elementPixel = 5;
+  const paleta = document.getElementsByClassName('color');
+  const color1 = paleta[0];
+  const color2 = paleta[1];
+  const color3 = paleta[2];
+  const color4 = paleta[3];
+
   let elementPixelBoard = document.querySelector('#pixel-board');
+  let clickB = document.getElementById('clear-board');
+
+  clickB.addEventListener('click', recebeClick);
+  color1.addEventListener('click', changeSelected);
+  color2.addEventListener('click', changeSelected);
+  color3.addEventListener('click', changeSelected);
+  color4.addEventListener('click', changeSelected);
+
+  function recebeClick() {
+    console.log('botão');
+  }
+
+  function changeSelected(evento) {
+    console.log(evento.target);
+    console.log(evento.type);
+    const seleciona = document.querySelector('.selected');
+    seleciona.classList.remove('selected');
+    evento.target.classList.add('selected');
+    console.log(paleta);
+  }
+
   function fillFrame(value) {
     // Varre a linha
     for (let line = 0; line < value; line += 1) {
@@ -18,3 +45,4 @@ window.onload = function () {
   }
   fillFrame(elementPixel);
 };
+console.log(document.body)
