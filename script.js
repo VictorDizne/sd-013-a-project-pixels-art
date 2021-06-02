400029eeea89eea6fb15e6344595efc7a4e0a945
Diego Vilela <vilelaphp@gmail.com>
2021-06-02 09:56:48 -0300

    3 - Adicione a cor preta como a primeira cor da paleta de cores e aplica melhorias informadas pelo lint

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
-window.onload = () => {
-    createPalette();
+/** Returns a random hexadecimal color except #ffffff (white). */
+function getRandomColor() {
+  let color;
+  do {
+    /* Source: https://css-tricks.com/snippets/javascript/random-hex-color/ */
+    color = Math.floor(Math.random() * 16777215).toString(16);
+  } while (color.length !== 6 || color === 'ffffff');
 
-    function createPalette() {
-        const paletteLine = document.querySelector('#color-palette .tr');
-
-        let cell;
-        for (let i = 0; i < 4; i++) {
-            cell = document.createElement('div');
-            cell.className = 'color td';
-            cell.style.backgroundColor = getRandomColor();
-            paletteLine.appendChild(cell);
-        }
-    }
-
-    /** Returns a random hexadecimal color except #ffffff (white). */
-    function getRandomColor() {
-        let color;
-        do {
-            /* Source: https://css-tricks.com/snippets/javascript/random-hex-color/ */
-            color = Math.floor(Math.random() * 16777215).toString(16);
-        } while (color.length !== 6 || color == 'ffffff');
-
-        return `#${color}`;
-    }
+  return `#${color}`;
 }
+
+function createPalette() {
+  const paletteLine = document.querySelector('#color-palette .tr');
+
+  let cell;
+  let color = '#000000';
+  for (let i = 0; i < 4; i += 1) {
+    cell = document.createElement('div');
+    cell.className = 'color td';
+    if (i) color = getRandomColor();
+    cell.style.backgroundColor = color;
+    paletteLine.appendChild(cell);
+  }
+}
+
+window.onload = () => {
+  createPalette();
+};
