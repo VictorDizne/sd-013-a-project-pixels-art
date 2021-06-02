9aadbbd59a40fbf59521fb3fdc0cebf78d598bc1
thomasravache <thomasravache31@gmail.com>
2021-06-01 22:30:12 -0300

    Requisito 08 - Clicar em um pixel dentro do quadro após selecionar uma cor na paleta faz com que o pixel seja preenchido com a cor selecionada. --> Finalizado.

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
-function createLines() {
-  const pixelBoard = document.querySelector('#pixel-board');
+const colorPalette = document.querySelector('#color-palette');
+const divsPalette = document.querySelector('#color-palette').children;
+let pixelBoard = document.querySelector('#pixel-board');
+let tr = document.querySelector('#pixel-board').children;
+let colors = [];
+let colorSelected = 'black';
 
+
+
+function createColorPalette() {
+  
+  for (let index = 0; index < 4; index += 1) {
+    let red = Math.random()*255;
+    let green = Math.random()*255;
+    let blue = Math.random()*255;
+    let color;
+
+    const div = document.createElement('div');
+    div.className = 'color';
+    div.id = index;
+    colorPalette.appendChild(div);
+
+    if (index === 0) {
+      color = div.style.backgroundColor = 'black';
+      colors.push(color);
+      div.className = 'color selected';
+    } else {
+      color = div.style.backgroundColor = `rgb(${red},${green},${blue})`;
+      colors.push(color);
+    }
+  }
+  console.log(colors);
+}
+
+function createLines() {
   for (let numLine = 0; numLine < 5; numLine += 1) {
     const line = document.createElement('div');
     line.className = 'tr';
@@ -9,7 +41,6 @@ function createLines() {
 }
 
 function createPixels() {
-  const tr = document.querySelectorAll('.tr');
 
   for (let line = 0; line < tr.length; line += 1) {
     for (let column = 0; column < 5; column += 1) {
@@ -20,41 +51,35 @@ function createPixels() {
   }
 }
 
+function resetClassSelected() {
+  for (let index in divsPalette) {
+    divsPalette[index].className = 'color';
+  }
+}
+
 window.onload = function inicio() {
+  createColorPalette();
   createLines();
   createPixels();
 
-
-  let colorsOfPalette = document.querySelectorAll('.color');
-  let pixels = document.querySelectorAll('.pixel');
-  let colorSelected = 'black'
-
-  for (let index = 0; index < colorsOfPalette.length; index += 1) {
-    colorsOfPalette[index].addEventListener('click', function() {
-      console.log('clicado');
-      switch(index) {
-        case 1:
-          colorSelected = 'red';
-          break;
-        case 2:
-          colorSelected = 'purple';
-          break;
-        case 3:
-          colorSelected = 'orange';
-          break;
-        default:
-          colorSelected = 'black';
+  document.addEventListener('click', function(event) {
+    if (event.target.classList.contains('color')) {
+      console.log(event.target.id);
+      resetClassSelected();
+      for(let index = 0; index < colors.length; index += 1) {
+        if (parseInt(event.target.id, 10) === index) {
+          event.target.className = 'color selected';
+          colorSelected = colors[index];
+        }
       }
-    
-    })
-  }
-
-  for (let index = 0; index < pixels.length; index += 1) {
-
-    pixels[index].addEventListener('click', function () {
-      pixels[index].style.backgroundColor = colorSelected;
-      console.log(`pixel ${index} foi clicado`);
-    })
-  }
+    }
+  });
+  
+  document.addEventListener('click', function (event) {
+    if (event.target.classList.contains('pixel')) {
+      console.log('pixel clicado')
+      event.target.style.backgroundColor = colorSelected;
+    }
+  })
 };
 
