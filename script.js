cdf8846c202a3615d91429fe373b6a18b9798d8f
Tatiane Chiarello <tatichiarello@gmail.com>
2021-06-02 17:33:18 +0200

    resolve requisitos 4, 5 e 6

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 function colorsPalette() {
   const colorPalette = document.querySelector('#color-palette');
-  for (let i = 0; i < 4; i += 1) {
-    const color = document.createElement('div');
-    color.classList = 'color';
-    colorPalette.appendChild(color);
-  }
+  const colors = ['black', 'yellow', 'red', 'green'];
+
+  colors.forEach((color) => {
+    const colorElement = document.createElement('div');
+    colorElement.className = 'color';
+    colorElement.style.backgroundColor = color;
+
+    if (color === 'black') {
+      colorElement.classList.add('selected');
+    }
+
+    colorPalette.appendChild(colorElement);
+  });
 }
 colorsPalette();
+
+function pixelsBoard() {
+  const pixelBoard = document.querySelector('#pixel-board');
+  for (let i = 0; i < 25; i += 1) {
+    const pixel = document.createElement('div');
+    pixel.className = 'pixel';
+    pixelBoard.appendChild(pixel);
+  }
+}
+pixelsBoard();
