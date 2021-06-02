9ba89aa5a12620f6901d6a174a3bda6706875b39
Cassyel <cassyel1967@gmail.com>
2021-06-02 15:32:07 -0300

    Formatação ESlint, e correção de algumas linhas de código desnecessárias

--- script.js
+++ script.js
@@ -1,18 +1,9 @@
 const quadroPixels = document.getElementById('pixel-board');
-const inputBoard = document.getElementById('board-size')
-const classColor = document.getElementsByClassName('color')
-const classPixel = document.getElementsByClassName('pixel')
+const classColor = document.getElementsByClassName('color');
+const classPixel = document.getElementsByClassName('pixel');
 const clean = document.getElementById('clear-board');
-window.onload = colorBlack();
 
-//Funções para gerar o quadro de pixels
-function geradorDeQuadros(tamanho) {
-  for (let index = 0; index < tamanho; index += 1) {
-    const linha = document.createElement('tr');
-    quadroPixels.appendChild(linha);
-    generate(tamanho, linha);
-  }
-}
+//  Funções para gerar o quadro de pixels
 
 function generate(tamanho, linha) {
   for (let index = 0; index < tamanho; index += 1) {
@@ -22,24 +13,27 @@ function generate(tamanho, linha) {
   }
 }
 
+function geradorDeQuadros(tamanho) {
+  for (let index = 0; index < tamanho; index += 1) {
+    const linha = document.createElement('tr');
+    quadroPixels.appendChild(linha);
+    generate(tamanho, linha);
+  }
+}
+
 geradorDeQuadros(5);
 
-//Função para gerar cores aleatórias
-function generateColors () {
-  const r = Math.ceil(Math.random() * 255)
-  const g = Math.ceil(Math.random() * 255)
-  const b = Math.ceil(Math.random() * 255)
-  if(r === g | r === b){
-     r = Math.ceil(Math.random() * 255)
-     g = Math.ceil(Math.random() * 255)
-     b = Math.ceil(Math.random() * 255)
-  }
+//  Função para gerar cores aleatórias
+function generateColors() {
+  const r = Math.ceil(Math.random() * 255);
+  const g = Math.ceil(Math.random() * 255);
+  const b = Math.ceil(Math.random() * 255);
   return `rgb(${r}, ${g}, ${b})`;
 }
-//Função para adicionar as cores aleatórias na paleta de cores
-function paletteColors () {
-  for (let i = 0; i < classColor.length; i += 1){
-    if ( i === 0) {
+//  Função para adicionar as cores aleatórias na paleta de cores
+function paletteColors() {
+  for (let i = 0; i < classColor.length; i += 1) {
+    if (i === 0) {
       classColor[i].style.backgroundColor = '#000';
     } else {
       classColor[i].style.backgroundColor = generateColors();
@@ -47,25 +41,25 @@ function paletteColors () {
   }
 }
 paletteColors();
-//Função para deixar selecionado a cor preta quando a pagina for carregada (chamada no ONLOAD)
-function colorBlack () {
+//  Função para deixar selecionado a cor preta quando a pagina for carregada
+function colorBlack() {
   classColor[0].classList.add('selected');
 }
-colorBlack(); 
-//Funções para que apenas uma cor tenha a classe 'selected' por vez
-//1 função = remove a classe 'selected' por
-//2 função = muda a classe 'selected' de lugar
-//3 função = adiciona a classe 'selected' na cor selecionada
-function unselected () {
+colorBlack();
+//  Funções para que apenas uma cor tenha a classe 'selected' por vez
+//  1 função = remove a classe 'selected' por
+//  2 função = muda a classe 'selected' de lugar
+//  3 função = adiciona a classe 'selected' na cor selecionada
+function unselected() {
   for (let i = 0; i < classColor.length; i += 1) {
     if (classColor[i].classList.contains('selected')) {
-      classColor[i].classList.remove('selected')
+      classColor[i].classList.remove('selected');
     }
   }
 }
 
 function changeSelected(event) {
-  unselected(); 
+  unselected();
   event.target.classList.add('selected');
 }
 for (let i = 0; i < classColor.length; i += 1) {
@@ -79,7 +73,7 @@ function selectedColor() {
     }
   }
 }
-//Função para permitir que os pixels sejam pintados
+//  Função para permitir que os pixels sejam pintados
 function changePixelColor(event) {
   const ev = event;
   ev.target.style.backgroundColor = selectedColor();
@@ -90,7 +84,7 @@ function changePixelColorListener() {
   }
 }
 changePixelColorListener();
-//Função para limpar os pixels
+//  Função para limpar os pixels
 function clearBoard() {
   for (let i = 0; i < classPixel.length; i += 1) { // percorre item por item do array
     classPixel[i].style.backgroundColor = 'white'; // altera a propriedade backgroundColor para white
