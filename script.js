94576f068d0a29e00a3646714cc8c5e8b857a155
Gustavo Mauricio <gmauricio2072gmail.com>
2021-06-02 14:53:44 -0300

    Adiciona class selected apenas ao item selecionado

--- script.js
+++ script.js
@@ -1,4 +1,48 @@
-window.onload = function(){
-	document.querySelector('.black').className += " selected"
+function startBlack(){
+	document.querySelector('#black').classList.add("selected");
 }
+startBlack();
+
+function removeSelected(){
+let color = document.querySelectorAll('.color');
+for(let index = 0 ; index< color.length; index +=1){
+		if(color[index].classList.contains('selected')){
+		color[index].classList.remove("selected");
+		}	
+	}
+}
+
+
+
+let color = document.querySelectorAll('.color');
+for(let index = 0; index < color.length; index += 1){
+    let cores = color[index];
+    cores.addEventListener('click', mostreCor);
+	
+}
+console.log(color)
+
+function mostreCor(event){ 
+	removeSelected();
+ 	event.target.classList.toggle("selected") ;
+ 	console.log(event.target);
+}
+
+
+
+
+
+
+
+
+function pegaCor(){	
+let colors = document.querySelectorAll('.colors');
+
+for(let index = 0; index < colors.length; index += 1){
+	if(colors[index].classList.contains("selected")){
+		return colors[index].style.backgroundColor;
+	}
+}
+}
+
 
