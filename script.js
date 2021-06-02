8aa37557043dc341e50c94a8f8e207204fea3f2f
Caio-Borin <caioborin@gmail.com>
2021-06-02 10:36:05 -0300

    ver se recupera

--- script.js
+++ script.js
@@ -1,45 +1,43 @@
-function () {
-  const elementPixel = 5;
-  const paleta = document.getElementById('color');
-  const color1 = paleta[0];
-  const color2 = paleta[1];
-  const color3 = paleta[2];
-  const color4 = paleta[3];
+const elementPixel = 5;
+const paleta = document.getElementById('color');
+const color1 = paleta[0];
+const color2 = paleta[1];
+const color3 = paleta[2];
+const color4 = paleta[3];
 
-  let elementPixelBoard = document.querySelector('#pixel-board');
-  let clickB = document.getElementById('clear-board');
+const elementPixelBoard = document.querySelector('#pixel-board');
+const clickB = document.getElementById('clear-board');
 
-  clickB.addEventListener('click', recebeClick);
-  color1.addEventListener('click', changeSelected);
-  color2.addEventListener('click', changeSelected);
-  color3.addEventListener('click', changeSelected);
-  color4.addEventListener('click', changeSelected);
+clickB.addEventListener('click', recebeClick);
+color1.addEventListener('click', changeSelected);
+color2.addEventListener('click', changeSelected);
+color3.addEventListener('click', changeSelected);
+color4.addEventListener('click', changeSelected);
 
-  function recebeClick () {
-    console.log('botão');
-  }
+function recebeClick() {
+  console.log('botão');
+}
 
-  function changeSelected(evento) {
-    console.log(evento.target);
-    console.log(evento.type);
-    const seleciona = document.querySelector('.selected');
-    seleciona.classList.remove('selected');
-    evento.target.classList.add('selected');
-    console.log(paleta);
-  }
+function changeSelected(evento) {
+  console.log(evento.target);
+  console.log(evento.type);
+  const seleciona = document.querySelector('.selected');
+  seleciona.classList.remove('selected');
+  evento.target.classList.add('selected');
+  console.log(paleta);
+}
 
-  function fillFrame(value) {
-    for (let line = 0; line < value; line += 1) {
-      let boxline = document.createElement("div");
-      boxline.className = 'linha';
-      elementPixelBoard.appendChild(boxline);
-    
-    for (let column = 0; column < value; column +=1) {
-      let boxColumn = document.createElement("div");
+function fillFrame(value) {
+  for (let line = 0; line < value; line += 1) {
+    const boxline = document.createElement('div');
+    boxline.className = 'linha';
+    elementPixelBoard.appendChild(boxline);
+
+    for (let column = 0; column < value; column += 1) {
+      const boxColumn = document.createElement('div');
       boxColumn.className = 'pixel';
-      boxline.appendChild(boxColumn)
-     }
+      boxline.appendChild(boxColumn);
     }
   }
-  fillFrame(elementPixel);
 }
+fillFrame(elementPixel);
