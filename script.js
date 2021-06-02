573a9870367e0e4665a059e6c2eba0f95d1b0ee1
Yuri Garrido <y.garrido.yuri@gmail.com>
2021-06-02 17:45:24 -0300

    envia requisito 7

--- script.js
+++ script.js
@@ -21,18 +21,36 @@ function PaletCor () {
   palet.className = 'centraliza';
   
     //aplicar cor preta no primeiro quadrado da paleta
-    let div = document.createElement('div');
-    palet.appendChild(div);
-    div.className = 'selected';
-    div.classList.add('color');
-    div.style.backgroundColor = 'black';
+    let div1 = document.createElement('div');
+    palet.appendChild(div1);
+    div1.className = ' style1';
+    div1.classList.add('color');
+    div1.id ='click';
+    div1.style.backgroundColor = 'black';
+    
+
+    let div2 = document.createElement('div');
+    palet.appendChild(div2);
+    div2.className = 'color';
+    div2.style.backgroundColor = 'yellow';
+    
+
+    let div3 = document.createElement('div');
+    palet.appendChild(div3);
+    div3.className = 'color';
+    div3.style.backgroundColor = 'green'
+
+    let div4 = document.createElement('div');
+    palet.appendChild(div4);
+    div4.className = 'color style';
+    div4.style.backgroundColor = 'red';
   
     //Cria 3 quadrados 
-    let nCor = 3;
+    /* let nCor = 3;
     for (let index =0; index < nCor; index += 1){
       let div = document.createElement('div');
       palet.appendChild(div);
-      div.className = 'color';
+      div.className = 'color style';
       div.style.backgroundColor = CorRandom ();
     }
   
@@ -48,8 +66,30 @@ function PaletCor () {
           cor = '#ff5fff';
         }
       }
-      return cor;
+      return cor; */
       //by: stackOverFlow
     }
-  }
+
   PaletCor ();
+// clica no quadrado e armazena a cor dele
+/* let link = document.getElementsByClassName('click');
+    for(let index = 0;)
+      link.addEventListener('click', function(){
+        link.classList.remove('selected');
+        this.classList.add('selected');
+      })
+
+ */
+/* aqui crio a classe no quadrado 1 e removo dos outros 3 */
+let coresDaPaleta = document.querySelectorAll('.color');
+
+function recebeCliq(event) {
+  for (let index = 0; index < coresDaPaleta.length; index += 1) {
+    coresDaPaleta[index].className = 'color';
+  }
+  event.target.className = 'color selected';
+}
+
+for (let index = 0; index < coresDaPaleta.length; index += 1) {
+  coresDaPaleta[index].addEventListener('click', recebeCliq);
+}
