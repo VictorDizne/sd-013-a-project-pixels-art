0816f613b3d21bf8540717032e077754adac0274
Julio Fallace <julioscoffee@gmail.com>
2021-06-02 15:54:45 -0300

    resolvendo requisito 5

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const pixelBoard = document.querySelector('#pixel-board');
-let tabela = '<tr><td class="pixel"></td><td class="pixel"></td><td class="pixel"></td><td class="pixel"></td><td class="pixel"></td></tr>';
+let tabelaTd = '<td class="pixel"></td>';
 for (let i = 0; i < 4; i += 1) {
-  tabela += '<tr><td class="pixel"></td><td class="pixel"></td><td class="pixel"></td><td class="pixel"></td><td class="pixel"></td></tr>';
+  tabelaTd += '<td class="pixel"></td>';
 }
-pixelBoard.innerHTML = '<table>'+tabela+'</table>';
+tabelaTd = '<tr>'+tabelaTd+'</tr>';
+for (let i = 0; i < 4; i += 1) {
+  tabelaTd += '<tr><td class="pixel"></td><td class="pixel"></td><td class="pixel"></td><td class="pixel"></td><td class="pixel"></td></tr>';
+}
+pixelBoard.innerHTML = '<table>'+tabelaTd+'</table>';
