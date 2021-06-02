bc990ab2487e26aec24008e618413a709d2f9122
Pedro Delicoli <pedrodelicoli@hotmail.com>
2021-06-02 16:33:18 -0300

    Requisito 6

--- script.js
+++ script.js
@@ -1,35 +1,21 @@
-window.onload = function(){
+window.onload = function () {
   let colorPixel1 = document.getElementsByClassName('color')[0];
   colorPixel1.style.backgroundColor = 'black';
-  setPalleteColor();
-  getRandomColor()
-}
-
-function getRandomColor()
- {
-  let color = Math.floor(Math.random() * 16777215).toString(16);
-  let color2 = Math.floor(Math.random() * 16777215).toString(16);
-  let color3 = Math.floor(Math.random() * 16777215).toString(16);
-   
   let colorPixel2 = document.getElementsByClassName('color')[1];
-colorPixel2.style.backgroundColor = '#' + color;
-let colorPixel3 = document.getElementsByClassName('color')[2];
-colorPixel3.style.backgroundColor = '#' + color2;
-let colorPixel4 = document.getElementsByClassName('color')[3];
-colorPixel4.style.backgroundColor = '#' + color3;
+  colorPixel2.style.backgroundColor = 'green';
+  let colorPixel3 = document.getElementsByClassName('color')[2];
+  colorPixel3.style.backgroundColor = 'blue';
+  let colorPixel4 = document.getElementsByClassName('color')[3];
+  colorPixel4.style.backgroundColor = 'yellow';
 }
 
-function setPalleteColor() {
-    let selectColor = document.getElementsByClassName('color selected');
-    let myPalletes = document.querySelector('.color');
-     
-    myPalletes.addEventListener('click', function(event) {
-      if (selectColor.length === 0) {
-        event.target.className = 'color selected';
-      } else {
-        event.target.className = 'color';
-      }
-    });
+function setPaletteColor (event) {
+   let x = event.target;
+   let myPalettes = document.getElementsByClassName('color');
+   for (let index = 0; index < myPalettes.length; index += 1){
+     myPalettes[index].classList.remove("selected");
+   }
+   x.classList.add("selected");    
   }; 
 
 
