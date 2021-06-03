bb3c342e9e0c0842af71a3a3c931e0ae7f26e945
Emídio Rebouças <emidioreb@gmail.com>
2021-06-03 00:28:30 -0300

    realizacao do requisito 7

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-//adicionar classe selected
-const getBlack = document. querySelector(".color");
-if (getBlack.classList) getBlack.classList. add("selected");
-else getBlack. className += "selected";
+
+  const colors = document.querySelectorAll('.color');
+  const palette = document.querySelector('#color-palette');
+
+palette.addEventListener('click', function (event){
+    if(event.target.classList.contains('color')){
+      colors.forEach(function(_, index){
+        colors[index].classList.remove("selected");
+      })
+      event.target.classList.add("selected");
+    }
+  })
+
+
+
