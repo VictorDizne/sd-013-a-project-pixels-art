daf5fa4e5f735eb24d55692572e38271f3c3d988
icaroMelo <exemployt@gmail.com>
2021-06-03 20:50:32 -0300

    realizo os 80%

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+function correction(){
+  let corrigir = document.querySelectorAll('.color');
+  corrigir[0].style.backgroundColor = 'black';
+  corrigir[1].style.backgroundColor = 'blue';
+  corrigir[2].style.backgroundColor = 'red';
+  corrigir[3].style.backgroundColor = 'green';
+}
+
+correction();
+
 function req41() {
   let box1 = document.querySelector('#pixel-board');
  
@@ -8,6 +18,7 @@ function req41() {
     for (let i = 0; i < 5; i += 1) {
       let pixels = document.createElement('div');
       pixels.className = 'pixel';
+      pixels.style.backgroundColor = 'white';
       row.appendChild(pixels);
   
    }
@@ -46,13 +57,11 @@ window.onload = function(){
   blackcolor.classList.add('selected'); 
 };
 
-
-
 function req7() {
   let seleciona = document.querySelector('#color-palette');
   let selected = document.querySelector('.color');
 
-  
+
   seleciona.addEventListener('click', (event) => {
     if (selected.classList.contains('selected') == false && event.target.classList.contains('color-palette') == false) {
       event.target.classList.add('selected');
@@ -67,19 +76,32 @@ function req7() {
 }
 req7();
 
-// function req8() {
-//   let select = document.querySelector('.selected');
-//   let paintbox = document.querySelectorAll('.pixel');
+function req8() {
 
-//   target.addEventListener('click', (event) => {
+document.addEventListener('click', function (event) {
+  if (event.target.classList.contains('pixel') == true) {
 
+    event.target.style.backgroundColor =  document.querySelector('.selected').style.backgroundColor;
+      
+  }
+}, false);
+}
+
+req8();
+
+// function req9(){
+//   let clearall = document.querySelector('#clear-board')
+ 
+//   document.addEventListener('click', (event) => {
+//     if(event.target.classList.contains('limpar') == true)
+//     for (let i = 0; i < 25; i +=1) {
+//     let clearpixels = document.querySelectorAll('.pixel');
+
+//     clearpixels.style.backgroundColor = 'white';
+//     }
     
-
-
-//   })
-
-
+//   }, false);
 // }
 
-// function req9
+// req9();
 
