dc52c7ffebb458d802f3a5bef7dd35f35bf44a4d
Mateus Montagnini <mateus.r.montagnini@gmail.com>
2021-06-03 17:16:03 -0300

    Resolve requisito 7

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
+function selectedBlack() {
+  const blackColor = document.querySelector('#colorBlack');
+  blackColor.classList.add('selected');
+}
+
+window.onload = selectedBlack;
+  
+//Requisito 7 resolvido com a ajuda de Gabriel Moraes
+//Função para remover a classe "selected"
+function removeSelected() {
+  const palette = document.querySelector('#color-palette'); 
+  const colors = document.getElementsByClassName('color');
+
+  function changeSelected (event) {
+    for (let i = 0; i < colors.length; i += 1) { //For para percorrer todo elemento "colors"
+      if (colors[i].classList.contains('selected')) { //Caso contenha a classe "selected", ela é removida
+        colors[i].classList.remove('selected');
+      }
+    }
+      event.target.classList.add('selected'); //Adiciona a classe "selected"
+    }
+
+    palette.addEventListener('click', changeSelected); //Adiciona evento "ao clicar" realiza a função
+  }
+
+  removeSelected();
+
+  
 
 
-let selectedBlack = document.querySelector('.pixel');
-
-selectedBlack.addEventListener('click', function() {
-  selectedBlack.classList.add('selected');
-})
