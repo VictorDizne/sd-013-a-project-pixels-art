0ffdb37fc76ecbfdcfd241a4d0fff5c109d0bb86
icaroMelo <exemployt@gmail.com>
2021-06-03 19:28:20 -0300

    realizo o requisito 7

--- script.js
+++ script.js
@@ -48,4 +48,38 @@ window.onload = function(){
 
 
 
+function req7() {
+  let seleciona = document.querySelector('#color-palette');
+  let selected = document.querySelector('.color');
+
+  
+  seleciona.addEventListener('click', (event) => {
+    if (selected.classList.contains('selected') == false && event.target.classList.contains('color-palette') == false) {
+      event.target.classList.add('selected');
+      selected = document.querySelector('.selected');
+    } else {
+      selected.classList.remove('selected');
+      event.target.classList.add('selected');
+      selected = document.querySelector('.selected');
+    }
+
+  })
+}
+req7();
+
+// function req8() {
+//   let select = document.querySelector('.selected');
+//   let paintbox = document.querySelectorAll('.pixel');
+
+//   target.addEventListener('click', (event) => {
+
+    
+
+
+//   })
+
+
+// }
+
+// function req9
 
