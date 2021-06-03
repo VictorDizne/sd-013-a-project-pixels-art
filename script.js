94dd7f7d70bc526bb281d1e1ddca83db5b997bec
deyvid-dev <deyvidscm@hotmail.com>
2021-06-02 22:44:14 -0300

    add pixel

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-function createPallete(){
-  let linha = document.createElement('div')
-  
+
+let linha = [5];
+function creatPallet() {
+let linha = [5];
+  for(index = 0; index < linha.length; index +=1){
+    const novo = document.createElement("div");
+    const element = document.querySelector("#pixel-board");
+    novo.className = "pixel";
+    element.appendChild(novo);
+  }
 }
+console.log(linha);
+creatPallet();
