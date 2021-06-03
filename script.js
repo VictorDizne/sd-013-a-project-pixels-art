11f9df8de04c1e30a9f2d4426b4016270cf97dee
icaroMelo <exemployt@gmail.com>
2021-06-03 15:39:29 -0300

    requisitos 1 e 2 novamente

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-let cores = document.getElementsByClassName('color');
-
-
-
-
-function cores2() {
-  let r = Math.ceil(Math.random() * 255 );
-  let g = Math.ceil(Math.random() * 255 );
-  let b = Math.ceil(Math.random() * 255 );
-  return rgb(r,g,b);
-}
-
 function req2() {
-  for (let i = 0; i < cores.length; i += 1){
-    if (i === 0) {
-      cores[i].style.backgroundColor = 'black';
-
-    } else {
-      cores[i].style.backgroundColor = cores2;
-    }
-  }
+  
 }
-
-
