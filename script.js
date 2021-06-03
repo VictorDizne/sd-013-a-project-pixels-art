f659c2d1fa291ebf0cd329cca8ae6870c5af16ed
Japa <wesleyy_@live.com>
2021-06-03 16:26:01 -0300

    Adicionado requisitos até o 6

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
-function corDaPeleta (){
-let corPaleta = document.querySelectorAll('.color')
-let cor = document.getElementById("black").getAttribute("data-value");
-  corPaleta[i].style.backgroundColor = cor;
+// Função criar pixels
 
+let pixelBoard = document.getElementById('pixel-board');
+
+function createPixels(tamanho, elementoPai) {
+  for (let index = 0; index < tamanho; index += 1){
+   let pixel = document.createElement('td');
+   pixel.className = 'pixel';
+   elementoPai.appendChild(pixel);
+  }
 }
+
+// Função criar o Pixel Board
+
+function createPixelBoard (tamanho) {
+  for (let index = 0; index < tamanho; index += 1) {
+    let elementoPai = document.createElement('tr');
+    pixelBoard.appendChild(elementoPai);
+    createPixels(tamanho, elementoPai);
+  }
+}
+
+createPixelBoard(5);
+
+// Função definir cor preta como inicial
+
+function initialColor () {
+  let corInicial = document.querySelector('.color');
+  corInicial.classList.add('selected');
+}
+
+initialColor();
+
+
