bac0dceeaa3c3085e330db3d81699f9a9b07a769
Marcos Durval <marc0sdurval20@gmail.com>
2021-06-03 20:12:10 -0300

    questão 9

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-function cores (){
+function Paleta (){
   let nCores = 4
-  let divMain = document.querySelector("#cor")
+  let divMain = document.querySelector("#color-palette")
   for(let i = 0; i < nCores; i +=1 ){
     let cor = document.createElement("div")
     divMain.appendChild(cor)
     cor.className = "color"
-    cor.id = "color-palette"
   }
 }
-cores()
-function test (){
+Paleta()
+
+
+function cores (){
   let listaC = document.querySelectorAll(".color")
   for(let j = 0; j < listaC.length; j += 1){
     if(listaC[j] === listaC[0]){
@@ -23,15 +24,14 @@ function test (){
     }
   }
 }
-test()
+cores()
 
 function tabela(){
   const linhas = 5
-  let divB = document.querySelector("#border")
+  let divB = document.querySelector("#pixel-board")
   for(let i =0; i < linhas; i += 1 ){
     let caixa = document.createElement("div")
     divB.appendChild(caixa)
-    caixa.id = "pixel-board"
     for(let j =0; j < linhas; j += 1){
       let pixel = document.createElement("div")
       caixa.appendChild(pixel)
@@ -42,24 +42,91 @@ function tabela(){
 }
 tabela()
 window.onload = function(){
-  
   const elementoCor = document.querySelector(".preto")
   elementoCor.classList.add("selected")
+  pret()
+  function colorido(){
+    let corMae = document.querySelectorAll(".color")
+    let tabCor = document.querySelector("#color-palette")
+    tabCor.addEventListener("click",(event) =>{
+      for (let i = 0; i < corMae.length; i += 1) {
+        let element = corMae[i];
+        if(element.classList.contains("selected")=== true){
+          if(element.classList.contains("preto")=== true){
+            pret()
+          }else if(element.classList.contains("vermelho")=== true){
+            ver()
+          }else if(element.classList.contains("verde")=== true){
+            verd()
+          }else if(element.classList.contains("amarelo")=== true){
+            amarelo()
+          }
+        }
+      }})
+  }
+  colorido()
   
 }
+
 function selecCores(){
-const vermelho = document.querySelector(".vermelho")
-const verde = document.querySelector(".verde")
-const preto = document.querySelector(".preto")
-const amarelo = document.querySelector(".amarelo")
-function clicar(event) {
-  const elementoCor = document.querySelector(".selected")
-  elementoCor.classList.remove("selected")
-  event.target.classList.add("selected")
-}
-verde.addEventListener("click",clicar)
-vermelho.addEventListener("click",clicar)
-preto.addEventListener("click",clicar)
-amarelo.addEventListener("click", clicar)
+  let corMae = document.querySelector("#color-palette")
+  corMae.addEventListener("click",(event) =>{
+    const apaga = document.querySelector(".selected")
+    apaga.classList.remove("selected")
+    event.target.classList.add("selected")
+    // console.log(event)
+    // console.log(event.target)
+    // console.log(event.target.classList)
+  })
 }
 selecCores()
+
+function clear(){
+  let buton = document.querySelector("#clear-board")
+  buton.innerHTML = "Limpar"
+  buton.addEventListener("click",(event) => {
+    const quadro = document.querySelectorAll("div")
+    for (let i = 0; i < quadro.length; i += 1 ) {
+      quadro[i].classList.remove("c4","c3","c1","c2");
+    }
+    // quadro.classList.remove("c1")
+    console.log(quadro);
+  })
+}
+clear()
+
+function pret(){
+  let cor = document.querySelector("#pixel-board")
+  // let ver = document.querySelector(".vermelho")
+  // let compStyles = getComputedStyle(ver).getPropertyValue("background")
+  cor.addEventListener("click",(event)=>{
+    event.target.classList.remove("c4","c3","c2")
+    event.target.classList.add("c1")
+  })
+}
+
+
+function ver(){
+  let cor = document.querySelector("#pixel-board")
+  cor.addEventListener("click",(event)=>{
+    event.target.classList.remove("c4","c3","c1")
+    event.target.classList.add("c2")
+  })
+}
+
+
+function verd(){
+  let cor = document.querySelector("#pixel-board")
+  cor.addEventListener("click",(event)=>{
+    event.target.classList.remove("c4","c3","c1","c2")
+    event.target.classList.add("c3")
+  })
+}
+
+function amarelo(){
+  let cor = document.querySelector("#pixel-board")
+  cor.addEventListener("click",(event)=>{
+    event.target.classList.remove("c3","c2","c1")
+    event.target.classList.add("c4")
+  })
+}
