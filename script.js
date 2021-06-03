3d4b44cf95055bea8c7fb6d1265e8542be5e53fe
Yuri Garrido <y.garrido.yuri@gmail.com>
2021-06-03 14:26:12 -0300

    envia requisto 8, e deixa o código mais limpo

--- script.js
+++ script.js
@@ -1,86 +1,38 @@
-//h1
-function cabeçalho () {
-
-  let header = document.createElement('header');
-  document.querySelector('body').appendChild(header);
-
-  let h1 = document.createElement('h1');
-  document.querySelector('header').appendChild(h1);
-  h1.id = 'title';
-  h1.className = 'centraliza';
-  h1.innerHTML = 'Paleta de Cores';
-}
-cabeçalho();
-
-//paleta de cores 1 section
-function PaletCor () {
-  // 1 section
-  let palet = document.createElement('section');
-  document.querySelector('body').appendChild(palet);
-  palet.id = 'color-palette';
-  palet.className = 'centraliza';
-  
-    //aplicar cor preta no primeiro quadrado da paleta
-    let div1 = document.createElement('div');
-    palet.appendChild(div1);
-    div1.className = 'selected';
-    div1.classList.add('color');
-    div1.id ='click';
-    div1.style.backgroundColor = 'black';
-    
-
-    let div2 = document.createElement('div');
-    palet.appendChild(div2);
-    div2.className = 'color';
-    div2.style.backgroundColor = 'yellow';
-    
-
-    let div3 = document.createElement('div');
-    palet.appendChild(div3);
-    div3.className = 'color';
-    div3.style.backgroundColor = 'green'
-
-    let div4 = document.createElement('div');
-    palet.appendChild(div4);
-    div4.className = 'color style';
-    div4.style.backgroundColor = 'red';
-  
-    //Cria 3 quadrados 
-    /* let nCor = 3;
-    for (let index =0; index < nCor; index += 1){
-      let div = document.createElement('div');
-      palet.appendChild(div);
-      div.className = 'color style';
-      div.style.backgroundColor = CorRandom ();
-    }
-  
-    //gera cor aleatoria das 3 ultimos quadros 
-    //by: stackOverFlow
-    function CorRandom() {
-      let caracteres = '0123456789ABCDEF';
-      let cor = '#';
-      
-      for ( let index = 0; index < 6; index += 1 ) {
-        cor += caracteres[Math.floor(Math.random() * 16)];
-        if (cor == '#ffffff') {
-          cor = '#ff5fff';
-        }
-      }
-      return cor; */
-      //by: stackOverFlow
-    }
-
-  PaletCor ();
-
-let coresDaPaleta = document.querySelectorAll('.color');
-
-function recebeCliq(event) {
-  for (let index = 0; index < coresDaPaleta.length; index += 1) {
-    coresDaPaleta[index].className = 'color';
+// CRIA PIXELS
+function criaPixels() {
+  for (let index =0; index < 25; index += 1){
+    let divPixel = document.createElement('div');
+    document.querySelector('#pixel-board').appendChild(divPixel);
+    divPixel.className = 'pixel'
   }
-  event.target.className = 'color selected';
-}
-
-for (let index = 0; index < coresDaPaleta.length; index += 1) {
-  coresDaPaleta[index].addEventListener('click', recebeCliq);
-}
+  }
+  criaPixels();
+  
+  // ---------------------------------------------------------------------------
+  
+  // Seleciona Cor da Paleta
+  let coresPaleta =  document.querySelectorAll('.color');
+  
+  function selected (event) {
+    for (let index = 0; index < coresPaleta.length; index += 1 ) {
+      coresPaleta[index].className = 'color';
+    }
+    event.target.className = 'selected color';
+  }
+  
+  for (let index = 0; index < coresPaleta.length; index += 1 ) {
+    coresPaleta[index].addEventListener('click', selected);
+  }
+  
+  
+  // Atribui a class selected ao pixel (pinta pixel)
+  let pixelSelecionado = document.querySelectorAll('.pixel');
+  
+  function pintaPixel (event) {
+    let corSelecionada = document.querySelector('.selected');
+    event.target.style.backgroundColor = window.getComputedStyle(corSelecionada).getPropertyValue('background-color');
+  }
+  
+  for (let index = 0; index < pixelSelecionado.length; index += 1 ) {
+    pixelSelecionado[index].addEventListener('click', pintaPixel);
+  }
