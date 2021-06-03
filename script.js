3c5b49b5d917f0c530cd563d61aab279d39823da
DouglasDrozda <lowdouglas9712@gmail.com>
2021-06-02 21:11:55 -0300

    feito requisito 7 e 8

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
-function creatPx(){
-  let quadro = 5;
-  for(let i = 0; i < quadro; i += 1){
-    const quadrado = document.createElement('div');
-    const pixelBoard = document.querySelector('#pixel-board');
-    pixelBoard.appendChild(quadrado);
-    quadrado.className = 'linha';
-    for (let index2 = 0; index2 < quadro; index2 += 1) {
-      const quadrado2 = document.createElement('div');
-      quadrado2.className = 'pixel';
-      quadrado.appendChild(quadrado2);
-  }
-  }
+// function creatPx(){
+//   let quadro = 5;
+//   for(let i = 0; i < quadro; i += 1){
+//     const quadrado = document.createElement('div');
+//     const pixelBoard = document.querySelector('#pixel-board');
+//     pixelBoard.appendChild(quadrado);
+//     quadrado.className = 'linha';
+//     for (let index2 = 0; index2 < quadro; index2 += 1) {
+//       const quadrado2 = document.createElement('div');
+//       quadrado2.className = 'pixel';
+//       quadrado.appendChild(quadrado2);
+//   }
+//   }
+// };
+// creatPx();
+function addClassSelect(event){
+  let corSelecionada = document.querySelector('.selected');
+  corSelecionada.classList.remove('selected');
+  event.target.classList.add('selected');
 };
-creatPx();
+const corPreta = document.querySelector('.black');
+const corVerm = document.querySelector('.red');
+const corAmar = document.querySelector('.yellow');
+const corVerd = document.querySelector('.green');
+corPreta.addEventListener('click', addClassSelect);
+corVerm.addEventListener('click', addClassSelect);
+corAmar.addEventListener('click', addClassSelect);
+corVerd.addEventListener('click', addClassSelect);
+
+
+const pixels = document.getElementsByClassName('pixel');
+for(let i = 0; i < pixels.length; i += 1){
+  pixels[i].addEventListener('click', paint);
+}
+function paint(event){
+  const e = event;
+  const selected = document.querySelector('.selected');
+  const color = getComputedStyle(selected).getPropertyValue('background-color');
+  e.target.style.backgroundColor = color;
+}
