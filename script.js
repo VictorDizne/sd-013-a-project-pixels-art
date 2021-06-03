1161a3df494637c22eecf75b0e6f4651305390c8
pedroalles <pedroalles@gmail.com>
2021-06-03 18:21:43 -0300

    Realizados desafios 8.

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-// let currentColor;
+let currentColor = '#000';
 function setCurrentColor(event) {
   const selectedColor = document.getElementsByClassName('selected')[0];
   event.target.classList.toggle('selected');
   selectedColor.classList.toggle('selected');
+  currentColor = event.target.innerText;
 }
 const colors = document.getElementsByClassName('color');
 function setColors() {
@@ -11,12 +12,16 @@ function setColors() {
     colors[i].style.backgroundColor = colors[i].innerText;
   }
 }
+function applyColor(event) {
+  event.target.style.backgroundColor = currentColor;
+}
 function createPixels(width) {
   const pixelLines = document.getElementsByClassName('pixel-line');
   for (let line = 0; line < pixelLines.length; line += 1) {
     for (let i = 0; i < width; i += 1) {
       const pixel = document.createElement('div');
       pixel.classList.add('pixel');
+      pixel.addEventListener('click', applyColor)
       pixelLines[line].appendChild(pixel);
     }
   }
