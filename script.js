4c9c12e94a1dc16b46f36cc661fb51237863a2e3
icaroMelo <exemployt@gmail.com>
2021-06-03 17:24:23 -0300

    reliazo requisito 3,4,5

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
-function req2() {
+function req41() {
+  let box1 = document.querySelector('#pixel-board');
+ 
+  for (let i = 0; i < 5; i += 1) {
+    let row = document.createElement('div');
+    row.className = 'row';
+    box1.appendChild(row);
+    for (let i = 0; i < 5; i += 1) {
+      let pixels = document.createElement('div');
+      pixels.className = 'pixel';
+      row.appendChild(pixels);
   
+   }
+  }
 }
+req41();
+
+function req4() {
+
+  let makebox = document.querySelector('#generate-board');
+
+  
+  makebox.addEventListener('click', function () {
+
+  let box1 = document.querySelector('#pixel-board');
+  let areaofbox = document.querySelector('#board-size').value;
+  let number = areaofbox;
+  
+  for (let i = 0; i < number; i += 1) {
+  let row = document.createElement('div');
+  row.className = 'row';
+  box1.appendChild(row);
+    for (let i = 0; i < number; i += 1) {
+    let pixels = document.createElement('div');
+    pixels.className = 'pixel';
+    row.appendChild(pixels);
+  
+   }
+  }
+ })
+}
+req4();
