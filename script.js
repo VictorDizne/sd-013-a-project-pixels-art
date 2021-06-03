d972faa565466e7a172608ec827b57c73ef611a4
Micael Santos <micaelmaicon@hotmail.com>
2021-06-03 16:47:54 -0300

    Finalize requirement 8 - Fix script.js

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ const colorBlue = document.getElementById('colorBlue');
 colorBlue.style.backgroundColor = 'Blue';
 const colorGreen = document.getElementById('colorGreen');
 colorGreen.style.backgroundColor = 'Green';
+let appliedColor = 'black';
+const getPixelOnBoard = document.querySelectorAll('.pixel');
 
-const getPixelOnBoard  = document.querySelectorAll('.pixel');
-var appliedColor = 'black';
 
 //  Requirement 7.
 // adicionar a classe `classe selected` a cor selecionada e retira a mesma classe da outra cor
@@ -17,14 +17,14 @@ colorBlack.addEventListener('click', colorSelectBlack);
 colorRed.addEventListener('click', colorSelectRed);
 colorBlue.addEventListener('click', colorSelectBlue);
 colorGreen.addEventListener('click', colorSelectGreen);
-pixelColor();
+
 // Requirement 8
 // adicionar um evento click que seleciona aquele elemento clicado e muda uma propriedade
 function pixelColor() {
-  for(let index in getPixelOnBoard) {
+  for (let index = 0; index < getPixelOnBoard.length; index += 1) {
     getPixelOnBoard[index].addEventListener('click', function () {
-    getPixelOnBoard[index].style.backgroundColor = appliedColor;
-    })
+      getPixelOnBoard[index].style.backgroundColor = appliedColor;
+    });
   }
 }
 
@@ -35,9 +35,8 @@ function colorSelectBlack(event) {
   colorBlack.classList.remove('selected');
   colorBlack.classList.add('selected');
   // Captura com 'event.target' a cor do background.
-  let colorToApply = event.target.style.backgroundColor;
+  const colorToApply = event.target.style.backgroundColor;
   appliedColor = colorToApply;
-  console.log(appliedColor);
 }
 
 function colorSelectRed(event) {
@@ -46,9 +45,8 @@ function colorSelectRed(event) {
   colorGreen.classList.remove('selected');
   colorRed.classList.remove('selected');
   colorRed.classList.add('selected');
-  let colorToApply = event.target.style.backgroundColor;
+  const colorToApply = event.target.style.backgroundColor;
   appliedColor = colorToApply;
-  console.log(appliedColor);
 }
 
 function colorSelectBlue(event) {
@@ -57,9 +55,8 @@ function colorSelectBlue(event) {
   colorGreen.classList.remove('selected');
   colorBlue.classList.remove('selected');
   colorBlue.classList.add('selected');
-  let colorToApply = event.target.style.backgroundColor;
+  const colorToApply = event.target.style.backgroundColor;
   appliedColor = colorToApply;
-  console.log(appliedColor);
 }
 
 function colorSelectGreen(event) {
@@ -67,7 +64,9 @@ function colorSelectGreen(event) {
   colorRed.classList.remove('selected');
   colorBlue.classList.remove('selected');
   colorGreen.classList.add('selected');
-  let colorToApply = event.target.style.backgroundColor;
+  const colorToApply = event.target.style.backgroundColor;
   appliedColor = colorToApply;
-  console.log(appliedColor);
 }
+
+
+pixelColor();
