3c9b6f3c78328dde8b9c01d4e0783161b5553ca4
Gean Gonçalves <gean.a.l.c@gmail.com>
2021-06-04 16:45:46 -0400

    Requisito 4 e 5

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ document.getElementsByClassName('color')[1].style.backgroundColor = 'green';
 document.getElementsByClassName('color')[2].style.backgroundColor = 'darkblue';
 document.getElementsByClassName('color')[3].style.backgroundColor = 'yellow';
 
+/*window.onload = function () {
+  
+}*/
