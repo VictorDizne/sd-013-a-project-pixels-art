4a0223981207999b9acbdd6bdfc57cef4118ac37
Micael Santos <micaelmaicon@hotmail.com>
2021-06-03 22:10:16 -0300

    Finalize requirement 9

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const colorGreen = document.getElementById('colorGreen');
 colorGreen.style.backgroundColor = 'Green';
 let appliedColor = 'black';
 const getPixelOnBoard = document.querySelectorAll('.pixel');
-
+const clearBoard = document.querySelector('#clear-board');
 
 //  Requirement 7.
 // adicionar a classe `classe selected` a cor selecionada e retira a mesma classe da outra cor
@@ -17,14 +17,12 @@ colorBlack.addEventListener('click', colorSelectBlack);
 colorRed.addEventListener('click', colorSelectRed);
 colorBlue.addEventListener('click', colorSelectBlue);
 colorGreen.addEventListener('click', colorSelectGreen);
+clearBoard.addEventListener('click', changeBoardColor);
 
-// Requirement 8
-// adicionar um evento click que seleciona aquele elemento clicado e muda uma propriedade
-function pixelColor() {
-  for (let index = 0; index < getPixelOnBoard.length; index += 1) {
-    getPixelOnBoard[index].addEventListener('click', function () {
-      getPixelOnBoard[index].style.backgroundColor = appliedColor;
-    });
+function changeBoardColor() {
+  console.log('apagou');
+  for(let index = 0; index < getPixelOnBoard.length; index += 1){
+    getPixelOnBoard[index].style.backgroundColor = 'white';
   }
 }
 
@@ -68,5 +66,14 @@ function colorSelectGreen(event) {
   appliedColor = colorToApply;
 }
 
+// Requirement 8
+// adicionar um evento click que seleciona aquele elemento clicado e muda uma propriedade
+function pixelColor() {
+  for (let index = 0; index < getPixelOnBoard.length; index += 1) {
+    getPixelOnBoard[index].addEventListener('click', function () {
+      getPixelOnBoard[index].style.backgroundColor = appliedColor;
+    });
+  }
+}
 
 pixelColor();
