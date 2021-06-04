1d95cbfc0d7150a4c9f5d3dc2cae55529f5b5d64
Walace Nascimento <wlacens@hotmail.com>
2021-06-04 19:58:03 -0300

    Resolve o requisito 7

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 function blackColor() {
-  const black = document.querySelector('.selected');
-  black.classList.add('selected')
+  const black = document.querySelector('#color-black');
+  black.classList.add('selected');
 }
 
 window.onload = blackColor;
+
+function selectColor() {
+  const colors = document.getElementsByClassName('color');
+  for (let i = 0; i < colors.length; i += 1) {
+    const color = colors[i];
+
+    color.addEventListener('click', ((event) => {
+      for (let j = 0; j < colors.length; j += 1) {
+        const colorDiv = colors[j]
+        colorDiv.classList.remove('selected');
+      }
+      event.target.classList.add('selected');
+
+    }));
+  }
+}
+selectColor();
