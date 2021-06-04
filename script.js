384afdc56578f02737dcfa4448e9b3c5fc61d888
Gustavo Mauricio <gmauricio2072gmail.com>
2021-06-04 16:12:04 -0300

    Conclusão do exercíco 10

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-let paletaDeCores = document.querySelectorAll('.color');
-let pixelBoard = document.querySelector('#pixel-board');
-let pixels = document.querySelectorAll('.pixel');
-let botao = document.querySelector('#clear-board');
+const paletaDeCores = document.querySelectorAll('.color');
+const botaoResetBoard = document.querySelector('#clear-board');
+const section = document.getElementById('tabela');
+// CRIA TABELA
+function table(size,celula){
+	let tabela = document.createElement('table');
+	section.appendChild(tabela);
+	tabela.setAttribute("id","pixel-board");
+
+	for(let i = 0; i < size; i +=1){
+		let linha = document.createElement('tr');
+		tabela.appendChild(linha);
+
+		for(let index = 0; index < celula; index += 1){
+			let cell = document.createElement('td');
+			linha.appendChild(cell);
+			cell.setAttribute('class', 'pixel');
+		}				
+	}
+}
+	table(5,5)
+
+const pixels = document.querySelectorAll('.pixel');
 
 // Inicia com a cor Preta Selecionada
 function startBlack(){
@@ -11,8 +30,8 @@ startBlack();
 
 //Reset Class selected
 function removeSelected(){
-	for(let i = 0; i < paletaDeCores.length; i += 1){
-		if(paletaDeCores[i].classList.contains('selected')){
+	for (let i = 0; i < paletaDeCores.length; i += 1){
+		if (paletaDeCores[i].classList.contains('selected')){
 			paletaDeCores[i].classList.remove('selected');
 		}
 	}
@@ -20,7 +39,7 @@ function removeSelected(){
 
 
 // Add Class selected na Cor Escolhida
- 	for(let i = 0; i < paletaDeCores.length; i += 1){
+ 	for (let i = 0; i < paletaDeCores.length; i += 1){
  		paletaDeCores[i].addEventListener('click', selecionaCor);			
  	}
  	function selecionaCor(event){
@@ -30,8 +49,8 @@ function removeSelected(){
 
 //Grava o Background da Cor
 function gravaCor(){
-for(let index = 0; index < paletaDeCores.length; index += 1){
-	if(paletaDeCores[index].classList.contains('selected')){
+for (let index = 0; index < paletaDeCores.length; index += 1){
+	if (paletaDeCores[index].classList.contains('selected')){
 	let element = paletaDeCores[index];
 	let background = window.getComputedStyle(element, null).getPropertyValue("background-color");
 	return background;
@@ -44,7 +63,7 @@ function pintaBoard(event){
 }
 
 function board(){
-	for(let i = 0; i < pixels.length; i += 1){
+	for (let i = 0; i < pixels.length; i += 1){
 		pixels[i].addEventListener('click', pintaBoard)
 	}
 }
@@ -53,10 +72,32 @@ board();
 
 // Crie um botão
 function resetBoard(){
-	for(let i = 0; i < pixels.length; i += 1){
+	for (let i = 0; i < pixels.length; i += 1){
 		pixels[i].style.backgroundColor = "#fff"
 		console.log(pixels[i]);
 	}
 }
-botao.addEventListener('click', resetBoard);
+botaoResetBoard.addEventListener('click', resetBoard);
+const input = document.querySelector('#board-size');
+
+// Definitamanho da tabela pelo usuário.
+let buttonGerarNewTable = document.querySelector('#generate-board'); // BOTÃO VQV
+
+
+function newTable(){
+	if(input.value == ""){
+		alert('Board Inválido')
+	} else if((input.value >= 5) && (input.value <= 50)){
+	section.innerHTML = null;	
+	table(input.value, input.value)
+	}	
+	
+}	
+
+
+buttonGerarNewTable.addEventListener('click', newTable)
+
+// Feito Com a ajuda do Gabriel Gaspar.
+
+	
 
