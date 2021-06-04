4c41dadc97661748699daaa8a280fd44cbc757ac
Murilo Ribeiro <murilorsv14@gmail.com>
2021-06-04 16:53:25 -0300

    90%

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const listaDeCor = 'lista-de-cor';
 const pixelBoard = '#pixel-board';
 const input = document.querySelector('input');
+let paletaDeCor = 'color-palette';
+ListaHasDeCor = '#lista-de-cor'
 
 function criaUlDeCor(id) {
   const ul = document.createElement('ul');
@@ -9,127 +11,21 @@ function criaUlDeCor(id) {
   ul.id = listaDeCor;
 }
 
-criaUlDeCor('color-palette');
+criaUlDeCor(paletaDeCor);
 
 function criaLiCores(id, n) {
   const ul = document.getElementById(id);
   for (let index = 1; index <= n; index += 1) {
     const li = document.createElement('li');
     ul.appendChild(li);
-    if (document.getElementsByClassName('selected')[0] == undefined) {
+    if (document.getElementsByClassName('selected')[0] === undefined) {
       li.className = 'color';
-      li.classList.add("selected");
+      li.classList.add('selected');
     } else {
       li.className = 'color';
     }
   }
 }
-
-criaLiCores(listaDeCor,4)
-
-
-function AdicionaPixels() {
-  let quant = NumeroValidoInput()
-  const criarBoard = document.createElement('section');
-  document.body.appendChild(criarBoard);
-  criarBoard.setAttribute('id', 'pixel-board');
-  const lugarDaLinha = document.querySelector(pixelBoard);
-  let pixelNumeroN = 0; for (let linha = 1; linha <= quant; linha += 1) {
-    const line = document.createElement('ul');
-    lugarDaLinha.appendChild(line);
-    for (let coluna = 1; coluna <= quant; coluna += 1) {
-      const column = document.createElement('li');
-      line.appendChild(column);
-      column.className = 'pixel';
-      column.id = `pixel ${pixelNumeroN}`; column.style.backgroundColor = "white"
-      column.addEventListener('click', mudaCor)
-      pixelNumeroN += 1;
-    }
-  }
-  mudaCorDoPixel();
-}
-
-AdicionaPixels();
-
-function removePixels() {
-  const tabelaDePixel = document.querySelector(pixelBoard);
-  tabelaDePixel.parentNode.removeChild(tabelaDePixel);
-}
-
-document.getElementById("generate-board").addEventListener('click', () => {
-  removePixels();
-  AdicionaPixels(input);
-});
-
-function changeColor(event) {
-  const NovaCor = event.target;
-  const CorAntiga = document.getElementsByClassName('selected');
-  CorAntiga[0].classList.remove('selected');
-  NovaCor.classList.add('selected');
-}
-let classSelecionada = document.querySelector('#lista-de-cor');
-classSelecionada.addEventListener('click', changeColor);
-
-function mudaCorDoPixel() {
-  let pixelNumeroN = 0;
-  let quantidade = NumeroValidoInput();
-  for (let i = 0; i < quantidade; i += 1) {
-    let elementoDaTabelaPixelada = document.getElementById(`pixel ${pixelNumeroN}`);
-    elementoDaTabelaPixelada.addEventListener('click', mudaCor);
-    pixelNumeroN += 1;
-  }
-}
-function mudaCor(event) {
-  const pixelClicado = event.target;
-  const corBase = document.getElementsByClassName('selected');
-  pixelClicado.style.backgroundColor = corBase[0].style.backgroundColor;
-  pixelClicado.setAttribute('id','colorido');
-}
-
-mudaCorDoPixel();
-
-document.querySelector('#clear-board').addEventListener('click', () => {
-  let pixelsColoridos = document.querySelectorAll('.pixel');
-  for (let i = 0; i < pixelsColoridos.length; i += 1) {
-    pixelsColoridos[i].style.backgroundColor = 'white';
-    pixelsColoridos[i].setAttribute('id','');
-  }
-});
-
-function removeCores() {
-  const nav = document.getElementsByTagName('nav')[0];
-  const ul = document.getElementsByTagName('ul')[0];
-  nav.removeChild(ul);
-}
-
-function gerarCor() {
-  let r = Math.random() * 255;
-  let g = Math.random() * 255;
-  let b = Math.random() * 255;
-  let cor = `rgb(${r}, ${g}, ${b})`;
-  return cor;
-}
-
-document.querySelector('#mudarCores').addEventListener('click', CriandoCoresAleatórias);
-
-function CriandoCoresAleatórias() {
-  removeCores();
-  criaUlDeCor('color-palette');
-  criaLiCores(listaDeCor, 4);  
-  let lista = document.getElementById('lista-de-cor');
-  for (let i = 0; i < lista.children.length; i += 1) {
-    if (i == 0 ) {
-      lista.children[i].style.backgroundColor = 'black'
-    } else {
-    let cor = gerarCor();
-    lista.children[i].style.backgroundColor = cor;
-    }
-  }
-  let classSelecionada = document.querySelector('#lista-de-cor');
-  classSelecionada.addEventListener('click', changeColor);
-}
-
-CriandoCoresAleatórias();
 
 function NumeroValidoInput() {
   let Inputvalor = document.querySelector('#board-size').value;
@@ -145,6 +41,111 @@ function NumeroValidoInput() {
     return alert('Board inválido!');
   }
 };
+
+function mudaCorDoPixel() {
+  let pixelNumeroN = 0;
+  const quantidade = NumeroValidoInput();
+  for (let i = 0; i < quantidade; i += 1) {
+    const elementoDaTabelaPixelada = document.getElementById(`pixel ${pixelNumeroN}`);
+    elementoDaTabelaPixelada.addEventListener('click', mudaCor);
+    pixelNumeroN += 1;
+  }
+}
+
+criaLiCores(listaDeCor, 4);
+
+function AdicionaPixels() {
+  const quant = NumeroValidoInput();;
+  const criarBoard = document.createElement('section');
+  document.body.appendChild(criarBoard);
+  criarBoard.setAttribute('id', 'pixel-board');
+  let pixelNumeroN = 0; for (let linha = 1; linha <= quant; linha += 1) {
+    const line = document.createElement('ul');
+    criarBoard.appendChild(line);
+    for (let coluna = 1; coluna <= quant; coluna += 1) {
+      const column = document.createElement('li');
+      line.appendChild(column);
+      column.className = 'pixel';
+      column.id = `pixel ${pixelNumeroN}`; column.style.backgroundColor = 'white';
+      column.addEventListener('click', mudaCor)
+      pixelNumeroN += 1;
+    }
+  }
+  mudaCorDoPixel();
+};
+
+AdicionaPixels();
+
+function removePixels() {
+  const tabelaDePixel = document.querySelector(pixelBoard);
+  tabelaDePixel.parentNode.removeChild(tabelaDePixel);
+}
+
+document.getElementById('generate-board').addEventListener('click', () => {
+  removePixels();
+  AdicionaPixels();
+});
+
+function changeColor(event) {
+  const NovaCor = event.target;
+  const CorAntiga = document.getElementsByClassName('selected');
+  CorAntiga[0].classList.remove('selected');
+  NovaCor.classList.add('selected');
+}
+const classSelecionada = document.querySelector(ListaHasDeCor);
+classSelecionada.addEventListener('click', changeColor);
+
+function mudaCor(event) {
+  const pixelClicado = event.target;
+  const corBase = document.getElementsByClassName('selected');
+  pixelClicado.style.backgroundColor = corBase[0].style.backgroundColor;
+  pixelClicado.setAttribute('id', 'colorido');
+}
+
+mudaCorDoPixel();
+
+document.querySelector('#clear-board').addEventListener('click', () => {
+  let pixelsColoridos = document.querySelectorAll('.pixel');
+  for (let i = 0; i < pixelsColoridos.length; i += 1) {
+    pixelsColoridos[i].style.backgroundColor = 'white';
+    pixelsColoridos[i].setAttribute('id', '');
+  }
+});
+
+function removeCores() {
+  const nav = document.getElementsByTagName('nav')[0];
+  const ul = document.getElementsByTagName('ul')[0];
+  nav.removeChild(ul);
+}
+
+function gerarCor() {
+  const r = Math.random() * 255;
+  const g = Math.random() * 255;
+  const b = Math.random() * 255;
+  const cor = `rgb(${r}, ${g}, ${b})`;
+  return cor;
+}
+
+document.querySelector('#mudarCores').addEventListener('click', CriandoCoresAleatórias);
+
+function CriandoCoresAleatórias() {
+  removeCores();
+  criaUlDeCor(paletaDeCor);
+  criaLiCores(listaDeCor, 4);  
+  let lista = document.getElementById('lista-de-cor');
+  for (let i = 0; i < lista.children.length; i += 1) {
+    if (i === 0 ) {
+      lista.children[i].style.backgroundColor = 'black'
+    } else {
+    const cor = gerarCor();
+    lista.children[i].style.backgroundColor = cor;
+    }
+  }
+  let classSelecionada = document.querySelector(ListaHasDeCor);
+  classSelecionada.addEventListener('click', changeColor);
+}
+
+CriandoCoresAleatórias();
 
 NumeroValidoInput();
 
@@ -174,52 +175,52 @@ function gerarCorAzul() {
 
 function CriandoCoresAleatóriasVerde() {
   removeCores();
-  criaUlDeCor('color-palette');
+  criaUlDeCor(paletaDeCor);
   criaLiCores(listaDeCor, 4);  
   let lista = document.getElementById('lista-de-cor');
   for (let i = 0; i < lista.children.length; i += 1) {
-    if (i == 0 ) {
+    if (i === 0 ) {
       lista.children[i].style.backgroundColor = 'black'
     } else {
-    let cor = gerarCorVerde();
-    lista.children[i].style.backgroundColor = cor;
+      const cor = gerarCorVerde();
+      lista.children[i].style.backgroundColor = cor;
     }
   }
-  let classSelecionada = document.querySelector('#lista-de-cor');
+  let classSelecionada = document.querySelector(ListaHasDeCor);
   classSelecionada.addEventListener('click', changeColor);
 };
 
-function CriandoCoresAleatóriasVermelha() {
+function CriandoCoresAleatóriasRed() {
   removeCores();
-  criaUlDeCor('color-palette');
+  criaUlDeCor(paletaDeCor);
   criaLiCores(listaDeCor, 4);  
   let lista = document.getElementById('lista-de-cor');
   for (let i = 0; i < lista.children.length; i += 1) {
-    if (i == 0 ) {
+    if (i === 0 ) {
       lista.children[i].style.backgroundColor = 'black'
     } else {
-    let cor = gerarCorVermelha();
-    lista.children[i].style.backgroundColor = cor;
+      const cor = gerarCorVermelha();
+      lista.children[i].style.backgroundColor = cor;
     }
   }
-  let classSelecionada = document.querySelector('#lista-de-cor');
+  let classSelecionada = document.querySelector(ListaHasDeCor);
   classSelecionada.addEventListener('click', changeColor);
 };
 
 function CriandoCoresAleatóriasAzul() {
   removeCores();
-  criaUlDeCor('color-palette');
+  criaUlDeCor(paletaDeCor);
   criaLiCores(listaDeCor, 4);  
   let lista = document.getElementById('lista-de-cor');
   for (let i = 0; i < lista.children.length; i += 1) {
-    if (i == 0 ) {
+    if (i === 0 ) {
       lista.children[i].style.backgroundColor = 'black'
     } else {
-    let cor = gerarCorAzul();
-    lista.children[i].style.backgroundColor = cor;
+      const cor = gerarCorAzul();
+      lista.children[i].style.backgroundColor = cor;
     }
   }
-  let classSelecionada = document.querySelector('#lista-de-cor');
+  let classSelecionada = document.querySelector(ListaHasDeCor);
   classSelecionada.addEventListener('click', changeColor);
 };
 
@@ -229,4 +230,4 @@ document.querySelector('#mudarCoresVerde').addEventListener('click', CriandoCore
 document.querySelector('#mudarCoresAzul').addEventListener('click', CriandoCoresAleatóriasAzul);
 
 
-document.querySelector('#mudarCoresVermelha').addEventListener('click', CriandoCoresAleatóriasVermelha);
+document.querySelector('#mudarCoresVermelha').addEventListener('click', CriandoCoresAleatóriasRed);
