b7c506b58672e0c0d08742123b66bd3488bdc605
Gabriel Priss <gabrielpriss@hotmail.com>
2021-06-03 23:32:21 -0300

    Muda a cor mas ainda não seleciona qual cor

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
 let pixels = document.getElementsByTagName("td")
 
+let corSelecionada = "red"
+
+  function pintar(event){
+    if (corSelecionada == "blue"){
+      let elementoClicado = event.target;
+      elementoClicado.classList.add('blue')
+    }
+    else if (corSelecionada == "black"){
+      let elementoClicado = event.target;
+      elementoClicado.classList.add('black')
+    }
+    else if (corSelecionada == "yellow"){
+      let elementoClicado = event.target;
+      elementoClicado.classList.add('yellow')
+    }
+    else if (corSelecionada == "red"){
+      let elementoClicado = event.target;
+      elementoClicado.classList.add('red')
+    }
+    else if (corSelecionada == "green"){
+      let elementoClicado = event.target;
+      elementoClicado.classList.add('green')
+    }
+  else {console.log("oi")}
+  }
+      for (var i = 0 ; i < pixels.length; i+=1){
+      pixels[i].addEventListener('click',pintar)
+  }
+        
+      
+    
 
 
-function pintaAzul(event){
-    let elementoClicado = event.target;
-    elementoClicado.classList.add('blue')
-}
 
 
-for (var i = 0 ; i < pixels.length; i+=1){
-pixels[i].addEventListener('click',pintaAzul)}
+
 
 
 
