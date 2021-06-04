26c4ade8804d09de0728c76d4c063a419bff4361
ggreg1987 <gabriel.gregorio1987@gmail.com>
2021-06-04 15:07:24 -0300

    Desafio 1

---
+++ script.js
@@ -0,0 +1,7 @@
+function questionOne() {
+  let h1 = document.createElement('h1');
+  h1.innerText = 'Paleta de Cores';
+  h1.setAttribute('id','title');
+  document.body.appendChild(h1);
+}
+questionOne();
