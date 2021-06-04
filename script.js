133d2c30b136802897b110a4f542bbe1436c48ec
Felipe Dias <felipeferreiradias1@gmail.com>
2021-06-04 11:27:28 -0300

    retomando projeto pixel art

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
-window.onload = function () {
+
   document.getElementById('colorOne').classList.add('selected');
-};
+
 
 let colorOne = document.getElementById('colorOne'); 
 let colorTwo = document.getElementById('colorTwo'); 
 let colorThree = document.getElementById('colorThree'); 
 let colorFour = document.getElementById('colorFour'); 
-
+let squares = document.querySelectorAll('.pixel')
+ 
 colorOne.addEventListener('click', selected);
 colorTwo.addEventListener('click', selected);
 colorThree.addEventListener('click', selected);
 colorFour.addEventListener('click', selected);
 
+for (let i=0; i < squares.length; i += 1) {
+  squares[index].addEventListener('click', changeColor);
+}
+
 function selected(event) {
-  const colorElement = document.querySelector('.selected');
-  colorElement.classList.remove('selected');
+  let colorChoose = document.querySelector('.selected');
+  colorChoose.classList.remove('selected');
   event.target.classList.add('selected');
 }
+
+function changeColor(event) {
+  // let square = document.querySelector('.printed');
+  // squares.classList.remove('printed');
+  event.target.classList.add('printed');
+}
+
+
+
+
+
 
 
