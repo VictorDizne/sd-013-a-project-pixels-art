c44b054a1583508da94fe08ff6a0388a730bd182
Breno Lopes <brenokos4@gmail.com>
2021-06-04 08:42:55 -0300

    adiciona competencia 8!

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // função para adicionar linhas e colunas no pixel-board!
 const tbody = document.querySelector('#pixel-board tbody');
 const trElem = document.getElementsByClassName('coluna');
-const tdElem = document.querySelectorAll('.coluna td');
+// const tdElem = document.querySelectorAll('.coluna td');
 
 function addPixels(numCol = 5, numLin = 5) {
   for (let i = 0; i <= numCol - 1; i += 1) {
@@ -26,4 +26,63 @@ function addClass(Array, classe) {
 addPixels();
 addClass(document.querySelectorAll('.coluna td'), 'pixel');
 
-console.log(tdElem);
+// seleção;
+window.onload = document.querySelector('.color:nth-child(1)').className = 'color selected';
+
+document.querySelectorAll('.pixel').forEach((item) => {
+  item.addEventListener('click', (_event) => {
+    item.id = 'corPreto';
+  });
+});
+
+function desmarcar() {
+  document.querySelector('.color:nth-child(1)').className = 'color';
+  document.querySelector('.color:nth-child(2)').className = 'color';
+  document.querySelector('.color:nth-child(3)').className = 'color';
+  document.querySelector('.color:nth-child(4)').className = 'color';
+}
+
+function corPreta() {
+  desmarcar();
+  document.querySelector('.color:nth-child(1)').className = 'color selected';
+  
+  document.querySelectorAll('.pixel').forEach((item) => {
+    item.addEventListener('click', (_event) => {
+      console.log('deu certo!');
+      item.id = 'corPreto';
+    });
+  });
+}
+
+function corAzul() {
+  desmarcar();
+  document.querySelector('.color:nth-child(2)').className = 'color selected';
+  document.querySelectorAll('.pixel').forEach((item) => {
+    item.addEventListener('click', (_event) => {
+      console.log('deu certo!');
+      item.id = 'corAzul';
+    });
+  });
+}
+
+function corVermelha() {
+  desmarcar();
+  document.querySelector('.color:nth-child(3)').className = 'color selected';
+  document.querySelectorAll('.pixel').forEach((item) => {
+    item.addEventListener('click', (_event) => {
+      console.log('deu certo!');
+      item.id = 'corVermelha';
+    });
+  });
+}
+
+function corVerde() {
+  desmarcar();
+  document.querySelector('.color:nth-child(4)').className = 'color selected';
+  document.querySelectorAll('.pixel').forEach((item) => {
+    item.addEventListener('click', (_event) => {
+      console.log('deu certo!');
+      item.id = 'corVerde';
+    });
+  });
+}
