93d160cf18dff81d5cfd1efbdaa0f89a9ce23bb0
deyvid-dev <deyvidscm@hotmail.com>
2021-06-04 16:10:29 -0300

    Testando requisitos 8 e 9

--- script.js
+++ script.js
@@ -7,3 +7,24 @@ function clickSelect(event){
   }
   event.target.classList.add("selected");
 }
+// Requisito 8
+
+function paintpixel(colorpixel){
+  let selectColor = document.querySelector('.selected');
+  colorpixel.target.style.backgroundColor = selectColor;
+}
+let pixel = document.querySelectorAll('.pixel');
+for(index = 0; index < pixel.length; index += 1){
+  pixel[index].addEventListener('click', paintpixel);
+}
+
+// Requisito 9
+
+function limpaBoard(){
+  let pixels = document.querySelectorAll('.pixel');
+  for(index = 0; index < pixels.length; index += 0){
+    document.querySelectorAll(".pixel")[index].style.backgroundColor = "white";
+  }
+}
+let buttom = document.querySelector("#clear-board");
+buttom.addEventListener('click', limpaBoard);
