efd17941093401b10081466b65d9f21df68845d2
Ingo Reichert Junior <ingoreichertjr@gmail.com>
2021-06-04 15:38:25 -0300

    Adiciona requisito 8

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 
 function selectedColor() {
 
-const colorPalette = document.getElementById("color-palette");
-const color = document.querySelectorAll(".color");
+    const colorPalette = document.getElementById("color-palette");
+    const color = document.querySelectorAll(".color");
 
 colorPalette.addEventListener('click', function(event) {
 
@@ -13,9 +13,35 @@ colorPalette.addEventListener('click', function(event) {
         event.target.classList.add("selected");
     })
 }
+   selectedColor();
 
+//8//
 
+           
+    function pixelColor() {
 
-    selectedColor();
+    const pixels = document.querySelectorAll('.pixel');
+    
+    for (let index = 0; index < pixels.length; index += 1) {
+        pixels[index].addEventListener('click', (event) => {
+            const colorSelected = document.querySelector('.selected');
+            const colorToPrint = getComputedStyle(colorSelected).getPropertyValue('background-color')
+            event.target.style.backgroundColor = colorToPrint;
+        });
+        }
+  }
+        pixelColor();
 
+//9//
 
+  function cleanAll() {
+
+    const button = document.querySelector("#clear-board")
+    const pixels = document.querySelectorAll('.pixel');
+    button.addEventListener('click', function() {
+    for (let index = 0; index < pixels.length; index += 1) {
+        pixels[index].backgroundColor = "white";
+        };
+    });
+};
+        cleanAll();
