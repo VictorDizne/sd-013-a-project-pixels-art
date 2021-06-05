119ac93df8602aec0ce3972e3b3f7a57748a3dcf
Adriana Barreto <adri92barreto123@gmail.com>
2021-06-05 11:34:02 -0300

    Executa setimo requesito

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-function selectColor() {
-    let corPreta = document.querySelector("#black");
-    corPreta.classList.add("selected")  
+window.onload = function (addEvendListener) {
+  corPreta;
+  corSelecionada;
+  colorir;
+};
+
+function corPreta () {
+  let selecionaPreto = document.getElementById('black');
+  selecionaPreto.classList.add('selected');
 }
 
-window.onload = selectColor;
+let corSelecionada = document.querySelector('#color-palette');
+corSelecionada.addEventListener('click', colorir);
+
+function colorir(event) {
+  let seleciona = document.querySelector('.selected');
+  seleciona.classList.remove('selected')
+  event.target.classList.add('selected'); 
+}
+
