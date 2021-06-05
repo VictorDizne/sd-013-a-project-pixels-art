4ff0591f47f7081b9e0e08b83371aa4edcd527ab
Seu nomecd <canaldogildosantos@gmail.com>
2021-06-04 21:26:16 -0300

    cria paleta de cores, cria tabela,centraliza tabela, cria borda em tabela e paleta de cores

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
- 
- document.getElementsByClassName('colors').Color.Style.Color= "blue";
- 
+ document.getElementsByClassName('color')[0].style.background = "black" 
+ document.getElementsByClassName('color')[1].style.background = "blue"
+ document.getElementsByClassName('color')[2].style.background = "red"
+ document.getElementsByClassName('color')[3].style.background = "yellow"
+
+function botaoLimpar() {
+      document.getElementsById("clear-bord").style.color = "white";
+} 
+window.onload= iniciando
+function iniciando(){
+      alert ("Crie a sua Arte");
+}
+
+  
  
     
