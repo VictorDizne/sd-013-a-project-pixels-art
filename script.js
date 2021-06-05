84feaeff8acd10cf8a79dd6bcf4929cd9d5d4128
pedroalles <pedroalles@gmail.com>
2021-06-05 17:46:05 -0300

    Realizados desafios 10 e 11.

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function setCurrentColor(event) {
   selectedColor.classList.toggle('selected');
   currentColor = event.target.innerText;
 }
+
 const colors = document.getElementsByClassName('color');
 function setColors() {
   for (let i = 0; i < colors.length; i += 1) {
@@ -13,10 +14,12 @@ function setColors() {
     colors[i].style.backgroundColor = colors[i].innerText;
   }
 }
+
 function applyColor(event) {
   const el = event.target;
   el.style.backgroundColor = currentColor;
 }
+
 function createPixels(width) {
   const pixelLines = document.getElementsByClassName('pixel-line');
   for (let line = 0; line < pixelLines.length; line += 1) {
@@ -28,6 +31,7 @@ function createPixels(width) {
     }
   }
 }
+
 function createPixelLines(lines, pixelBoard) {
   for (let line = 0; line < lines; line += 1) {
     const pixelLine = document.createElement('div');
@@ -35,23 +39,62 @@ function createPixelLines(lines, pixelBoard) {
     pixelBoard.appendChild(pixelLine);
   }
 }
-function createPixelBord(width) {
+
+function validateInput(boardSize, boardInput) {
+  let bS = boardSize;
+  const bI = boardInput;
+  if (boardSize < 5) {
+    bS = 5;
+    bI.value = bS;
+  } else if (boardSize > 50) {
+    bS = 50;
+    bI.value = bS;
+  }
+  return bS;
+}
+
+let first = true;
+function getInputValue() {
+  const boardInput = document.getElementById('board-size');
+  let boardSize = boardInput.value;
+  if (boardSize === '' && first) {
+    boardSize = 5;
+    first = false;
+  } else if (boardSize === '' && !first) {
+    alert('Board inválido!');
+  }
+  boardSize = validateInput(boardSize, boardInput);
+  return boardSize;
+}
+
+function createPixelBord() {
+  const width = getInputValue();
   const pixelBoard = document.getElementById('pixel-board');
+  pixelBoard.innerHTML = '';
   createPixelLines(width, pixelBoard);
   createPixels(width);
 }
+
 function clearPixelBoard() {
   const pixels = document.getElementsByClassName('pixel');
   for (let pixel = 0; pixel < pixels.length; pixel += 1) {
     pixels[pixel].style.backgroundColor = '#fff';
   }
 }
-function setupButton() {
+
+function setupButtonClear() {
   const bt = document.getElementById('clear-board');
   bt.addEventListener('click', clearPixelBoard);
 }
+
+function setupButtonGenerate() {
+  const bt = document.getElementById('generate-board');
+  bt.addEventListener('click', createPixelBord);
+}
+
 window.onload = function startPage() {
   setColors();
-  createPixelBord(5);
-  setupButton();
+  createPixelBord();
+  setupButtonClear();
+  setupButtonGenerate();
 };
