b09e79d8fcd2f08b56f884f882c4714bf6fd90db
Fernando <nando_filipe@hotmail.com>
2021-06-05 03:21:01 -0300

    Requisito do 7

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 
+
+
 let pixelBd = document.querySelector('#pixel-board')                                    
-let buttonInput = document.querySelector('#generate-board');
+
 function geraBoard(linhas){
-pixelBd.innerHTML = '';
+  pixelBd.innerHTML = '';
   for (let i = 0; i < linhas; i ++) {
     let linhaPixel = document.createElement('div');
     linhaPixel.className = 'linha';
@@ -12,10 +14,57 @@ pixelBd.innerHTML = '';
       pixel.className = 'pixel';
       linhaPixel.appendChild(pixel);
     }
-   
+    setaCor();
   }
 }
-window.onload = function() {
-  geraBoard(5);
-};
 
+window.onload = function() {
+  geraBoard(10);
+}
+//Pega os elementos button
+let buttonBlack = document.querySelector('#black');
+let buttonRed = document.querySelector('#red');
+let buttonGreen = document.querySelector('#green');
+let buttonBlue = document.querySelector('#blue');
+let color = 'black';
+
+//cria eventos de click/ função anônima
+buttonBlack.addEventListener('click', function() {
+  buttonBlack.classList.add('selected');
+  buttonRed.classList.remove('selected');
+  buttonGreen.classList.remove('selected');
+  buttonBlue.classList.remove('selected');
+  color = window.getComputedStyle(buttonBlack).getPropertyValue('background-color')
+})
+
+buttonRed.addEventListener('click', function () {
+  buttonRed.classList.add('selected');
+  buttonBlack.classList.remove('selected');
+  buttonRed.classList.remove('selected');
+  buttonBlue.classList.remove('selected');
+  color = window.getComputedStyle(buttonRed).getPropertyValue('background-color');
+});
+
+buttonGreen.addEventListener('click', function () {
+  buttonGreen.classList.add('selected');
+  buttonBlack.classList.remove('selected');
+  buttonRed.classList.remove('selected');
+  buttonBlue.classList.remove('selected');
+  color = window.getComputedStyle(buttonGreen).getPropertyValue('background-color');
+});
+
+buttonBlue.addEventListener('click', function () {
+  buttonBlue.classList.add('selected');
+  buttonBlack.classList.remove('selected');
+  buttonRed.classList.remove('selected');
+  buttonGreen.classList.remove('selected');
+  color = window.getComputedStyle(buttonBlue).getPropertyValue('background-color');
+});
+
+function setaCor(){
+  document.querySelectorAll('.pixel').forEach((att) => {
+    att.addEventListener('click', function() {
+      att.style.backgroundColor = color;
+    });
+  });
+}
