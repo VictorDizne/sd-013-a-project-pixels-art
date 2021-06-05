cc4b03a768ad305a8049b702d4ad4b2d3c0ba78c
Caê <eduardokae01@hotmail.com>
2021-06-05 12:29:36 -0300

    requisito 8 - pixel art

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Feito com ajuda de vitor dos santos, luiz furtado e gabriel gaspar
+// Requisito 7
 let colors = document.querySelectorAll('.color');
 
 for (let index = 0; index < colors.length; index += 1) {
@@ -10,3 +12,16 @@ for (let index = 0; index < colors.length; index += 1) {
     }
     e.target.classList.add('selected');
   }
+
+  // Requisito 8
+  let pixel = document.querySelectorAll('.pixel');
+
+  for (index = 0; index < pixel.length; index += 1) {
+      pixel[index].addEventListener('click', clicar);
+  }
+  function clicar(event) {
+    let colorToPaint = document.getElementsByClassName('selected');
+    let colorTarget = getComputedStyle(colorToPaint[0]).getPropertyValue("background-color");
+    event.target.style.backgroundColor = colorTarget;
+  }
+  
