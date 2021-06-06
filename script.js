cafb33e55302709ee058f0a0f941b1eeca474d67
thomasravache <thomasravache31@gmail.com>
2021-06-06 01:25:30 -0300

    Ajustando script.js ao lint

--- script.js
+++ script.js
@@ -1,48 +1,42 @@
-let title = 'Projeto Pixel Art - Paleta de Cores'
-let titleCounter = 0;
+const title = 'Projeto Pixel Art - Paleta de Cores';
 const colorPalette = document.querySelector('#color-palette');
-const divsOfPalette = document.querySelector('#color-palette').children;;
+const divsOfPalette = document.querySelector('#color-palette').children;
 const pixelBoard = document.querySelector('#pixel-board');
-let tr = document.querySelector('#pixel-board').children;
+const tr = document.querySelector('#pixel-board').children;
+const btnClear = document.querySelector('#clear-board');
+const btnRandom = document.querySelector('#random');
+const btnChangeSize = document.querySelector('#generate-board');
+const txtSize = document.querySelector('#board-size');
+let numLinesAndColumn = 5;
 let colors = [];
 let colorSelected = 'black';
-let btnClear = document.querySelector('#clear-board');
-let btnRandom = document.querySelector('#random');
-let btnChangeSize = document.querySelector('#generate-board');
-let txtSize = document.querySelector('#board-size');
-let numLinesAndColumn = 5;
+let titleCounter = 0;
 
 function introTitle() {
-    if(titleCounter < title.length) {
-      document.querySelector('#title').innerHTML += title.charAt(titleCounter);
-      titleCounter += 1;
-      setTimeout(introTitle, 110);
-    }
-    for (let index = 0; index < title.length; index += 1) {
-      
-    }
+  if (titleCounter < title.length) {
+    document.querySelector('#title').innerHTML += title.charAt(titleCounter);
+    titleCounter += 1;
+    setTimeout(introTitle, 110);
+  }
 }
 
 function storeRandomColors(index, div) {
-  let red = Math.random()*255;
-  let green = Math.random()*255;
-  let blue = Math.random()*255;
-  let color;
+  const red = Math.random() * 255;
+  const green = Math.random() * 255;
+  const blue = Math.random() * 255;
+  const createdDiv = div;
 
   if (index === 0) {
-    color = div.style.backgroundColor = 'black';
-    colors.push(color);
-    div.className = 'color selected';
+    colors.push(createdDiv.style.backgroundColor = 'black');
+    createdDiv.className = 'color selected';
   } else {
-    color = div.style.backgroundColor = `rgb(${red},${green},${blue})`;
-    colors.push(color);
+    colors.push(createdDiv.style.backgroundColor = `rgb(${red},${green},${blue})`);
   }
 }
 
 function createColorPalette() {
   colors = [];
   for (let index = 0; index < 4; index += 1) {
-
     const div = document.createElement('div');
     div.className = 'color';
     div.id = index;
@@ -61,7 +55,6 @@ function createLines() {
 }
 
 function createPixels() {
-
   for (let line = 0; line < tr.length; line += 1) {
     for (let column = 0; column < numLinesAndColumn; column += 1) {
       const pixel = document.createElement('div');
@@ -78,76 +71,97 @@ function resetClassSelected() {
 }
 
 function resetColorPalette() {
-  let count = colorPalette.childElementCount;
+  const count = colorPalette.childElementCount;
   for (let index = 0; index < count; index += 1) {
     colorPalette.removeChild(colorPalette.children[0]);
   }
 }
 
 function resetPixelBoard() {
-  let count = pixelBoard.childElementCount;
+  const count = pixelBoard.childElementCount;
   for (let index = 0; index < count; index += 1) {
     pixelBoard.removeChild(pixelBoard.children[0]);
   }
 }
 
 function afterError() {
-  document.querySelector('input').style.border = '1px solid black'
+  document.querySelector('input').style.border = '1px solid black';
 }
 
-btnClear.addEventListener('click', function() {
-  console.log('botão de limpar foi clicado.');
-  let pixels = document.querySelectorAll('.pixel');
+function inputSizeVerificator() {
+  const maxValue = 50;
+  const minValue = 5;
+
+  if (txtSize.value < minValue) {
+    numLinesAndColumn = minValue;
+  } else if (txtSize.value > maxValue) {
+    numLinesAndColumn = maxValue;
+  } else {
+    numLinesAndColumn = parseInt(txtSize.value, 10);
+  }
+}
+
+function catchColor(eventOfColorSelector) {
+  const clickedEvent = eventOfColorSelector.target;
+
+  for (let index = 0; index < colors.length; index += 1) {
+    if (parseInt(eventOfColorSelector.target.id, 10) === index) {
+      clickedEvent.className = 'color selected';
+      colorSelected = colors[index];
+    }
+  }
+}
+
+btnClear.addEventListener('click', function clearPixelBoard() {
+  console.log('Botão de limpar acionado.');
+  const pixels = document.querySelectorAll('.pixel');
   for (let index = 0; index < pixels.length; index += 1) {
     pixels[index].style.backgroundColor = 'white';
   }
+
+  return this;
 });
 
-btnRandom.addEventListener('click', function() {
+btnRandom.addEventListener('click', function reloadColors() {
   resetColorPalette();
   createColorPalette();
+
+  return this;
 });
 
-btnChangeSize.addEventListener('click', function() {
+btnChangeSize.addEventListener('click', function changeSize() {
   if (txtSize.value === '') {
     alert('Board inválido!');
     document.querySelector('input').style.border = '3px solid red';
     setTimeout(afterError, 2500);
   } else {
-    let maxValue = 50;
-    let minValue = 5;
-
-    if (txtSize.value < minValue) {
-      numLinesAndColumn = minValue;
-    } else if (txtSize.value > maxValue) {
-      numLinesAndColumn = maxValue;
-    } else {
-      numLinesAndColumn = parseInt(txtSize.value, 10);
-    }
+    inputSizeVerificator();
     resetPixelBoard();
     createLines();
     createPixels();
   }
+
+  return this;
 });
 
-document.addEventListener('click', function(event) {
+document.addEventListener('click', function colorSelector(event) {
   if (event.target.classList.contains('color')) {
     console.log(`div Color ${event.target.id}`);
     resetClassSelected();
-    for(let index = 0; index < colors.length; index += 1) {
-      if (parseInt(event.target.id, 10) === index) {
-        event.target.className = 'color selected';
-        colorSelected = colors[index];
-      }
-    }
+    catchColor(event);
   }
+
+  return this;
 });
 
-document.addEventListener('click', function (event) {
+document.addEventListener('click', function paintPixel(event) {
   if (event.target.classList.contains('pixel')) {
-    console.log('pixel clicado')
-    event.target.style.backgroundColor = colorSelected;
+    const eventStyle = event.target.style;
+    console.log('pixel clicado');
+    eventStyle.backgroundColor = colorSelected;
   }
+
+  return this;
 });
 
 window.onload = function initProcedures() {
@@ -156,4 +170,3 @@ window.onload = function initProcedures() {
   createLines();
   createPixels();
 };
-
