1003ab94369738ebea58ca16523a37ced1193e0f
Seu nomecd <canaldogildosantos@gmail.com>
2021-06-06 02:26:42 -0300

    acrescenta dor inicial, modifica intes, inicia criaçao do botaão limpar

--- script.js
+++ script.js
@@ -2,15 +2,31 @@
  document.getElementsByClassName('color')[1].style.background = "blue"
  document.getElementsByClassName('color')[2].style.background = "red"
  document.getElementsByClassName('color')[3].style.background = "yellow"
-
-function LimpaTela() {
-      document.getElementsById("clear-bord").style.color = "white";
-} 
-window.onload= iniciando
+ 
+  window.onload= iniciando
 function iniciando(){
       alert ("Crie a sua Arte");
 }
 
+function Color1() { 
+     let colorInicitial = document.getElementsByClassName('color')[0].class=Selected;
+}
+
+let limpaCor = document.getElementsById("color-palette");
+for ( let i= 0; i < color-palette.length; i += 1){
+ let reInicio = color-palette [i];
+ reInicio = clear-bord;
+ clear-bord.addEventListener("click, Apagar"); 
+ 
+}
+function apagar() {
+      for (let i = 0; i < clear-bord.length; i += 1) {
+         let pixels = clear-bord [i];
+         pixels.style.backgroundColor= white;
+   }  
+}
+
   
+        
  
     
