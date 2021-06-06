a24eee3e6b8bc4ee23bfacb1350216e3be744e2c
ggreg1987 <gabriel.gregorio1987@gmail.com>
2021-06-06 14:20:01 -0300

    Corrigindo o lint

--- script.js
+++ script.js
@@ -2,24 +2,24 @@ function questionOne() {
   const h1 = document.createElement('h1');
   h1.innerText = 'Paleta de Cores';
   h1.setAttribute('id', 'title');
-  h1.style.textAlign = 'center'
+  h1.style.textAlign = 'center';
   document.body.appendChild(h1);
 }
 questionOne();
 
 function questionTwoThreeSix() {
-  let divPallet = document.createElement('div');
+  const divPallet = document.createElement('div');
   divPallet.setAttribute('id', 'color-palette');
-  let arrayCores = ['black', 'yellow', 'blue', 'red'];
+  const arrayCores = ['black', 'yellow', 'blue', 'red'];
 
-  for(let i = 0; i < arrayCores.length; i += 1) {
-    let div = document.createElement('div');
+  for (let i = 0; i < arrayCores.length; i += 1) {
+    const div = document.createElement('div');
     div.style.backgroundColor = arrayCores[i];
-    if(div.style.backgroundColor == 'black') {
+    if (div.style.backgroundColor === 'black') {
       div.classList.add('selected');
     }
     div.style.border = '1px solid black';
-    div.style.display = 'inline-block'
+    div.style.display = 'inline-block';
     div.classList.add('color');
     divPallet.appendChild(div);
     document.body.appendChild(divPallet);
@@ -28,17 +28,15 @@ function questionTwoThreeSix() {
 questionTwoThreeSix();
 
 function questionFour() {
-  let divBoard = document.createElement('div');
+  const divBoard = document.createElement('div');
   divBoard.setAttribute('id', 'pixel-board');
 
-  for(let i = 1; i <= 25; i += 1) {
-    let pixel = document.createElement('div');
+  for (let i = 1; i <= 25; i += 1) {
+    const pixel = document.createElement('div');
     pixel.classList.add('pixel');
     pixel.style.backgroundColor = 'white';
     pixel.style.border = '1px solid black';
-
-      pixel.style.display = 'inline-block';
-
+    pixel.style.display = 'inline-block';
     divBoard.appendChild(pixel);
   }
   document.body.appendChild(divBoard);
@@ -46,51 +44,49 @@ function questionFour() {
 questionFour();
 
 function questionSeven() {
+  const colors = document.getElementsByClassName('color');
+  for (let i = 0; i < colors.length; i += 1) {
+    const color = colors[i];
 
-  let colors = document.getElementsByClassName('color');
-  for(let i = 0; i < colors.length; i+= 1) {
-    color = colors[i];
-
-    color.addEventListener('click',((event) => {
-        for(let j = 0; j < colors.length; j += 1) {
-          let colorDiv = colors[j]
-          colorDiv.classList.remove('selected');
-        }
-        event.target.classList.add('selected')
-    }))
+    color.addEventListener('click', ((event) => {
+      for (let j = 0; j < colors.length; j += 1) {
+        const colorDiv = colors[j];
+        colorDiv.classList.remove('selected');
+      }
+      event.target.classList.add('selected');
+    }));
   }
 }
 questionSeven();
 
 function questionEight() {
- let pixels = document.getElementsByClassName('pixel');
- let selected = document.getElementsByClassName('selected')
-
- for(let i = 0; i < pixels.length; i += 1) {
-  let pixel = pixels[i];
-  pixel.addEventListener('click',(() => {
-    for(let j = 0; j < selected.length; j += 1) {
-      select = selected[j];
-      pixel.style.backgroundColor = select.style.backgroundColor;
-    }
-  }))
- }
+  const pixels = document.getElementsByClassName('pixel');
+  const selected = document.getElementsByClassName('selected');
+  for (let i = 0; i < pixels.length; i += 1) {
+    const pixel = pixels[i];
+    pixel.addEventListener('click', (() => {
+      for (let j = 0; j < selected.length; j += 1) {
+        const select = selected[j];
+        pixel.style.backgroundColor = select.style.backgroundColor;
+      }
+    }));
+  }
 }
 questionEight();
 
 function questionNine() {
-  let div = document.createElement('div');
-  let pixels = document.getElementsByClassName('pixel');
-  let pixelsBoard = document.getElementById('pixel-board');
-  let button = document.getElementById('clear-board')
+  const div = document.createElement('div');
+  const pixels = document.getElementsByClassName('pixel');
+  const pixelsBoard = document.getElementById('pixel-board');
+  const button = document.getElementById('clear-board');
 
-  button.addEventListener('click',() => {
-    for(let i = 0; i < pixels.length; i += 1) {
-      let pixel = pixels[i];
+  button.addEventListener('click', () => {
+    for (let i = 0; i < pixels.length; i += 1) {
+      const pixel = pixels[i];
       pixel.style.backgroundColor = 'White';
     }
-  })
+  });
   div.appendChild(button);
-  document.body.insertBefore(div,pixelsBoard);
+  document.body.insertBefore(div, pixelsBoard);
 }
 questionNine();
