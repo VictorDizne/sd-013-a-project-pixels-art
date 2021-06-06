19b6f41ba95938c779e97fbf0fe274880482eb7e
Seu nomecd <canaldogildosantos@gmail.com>
2021-06-06 13:02:56 -0300

    mais uma tentativa de  acrescentar cor black selected

--- script.js
+++ script.js
@@ -3,14 +3,32 @@
  document.getElementsByClassName('color')[2].style.background = "red"
  document.getElementsByClassName('color')[3].style.background = "yellow"
  
-window.onload= iniciando
-   function iniciando(){
-      alert ("Crie a sua Arte");
-      alert ("inicia com a cor preta selecionada");
+//window.onload= iniciando
+  // function iniciando(){
+    //  alert ("Crie a sua Arte");
+      //alert ("inicia com a cor preta selecionada");
+//}
+
+window.onload= inicioCor
+function inicioCor() {
+      document.getElementsByClassName('color')[0].clasList.add('selected')
+
 }
 
+let pixels= document.getElementsByClassName("pixel");
+let reInicio = document.getElementById("clear-bord");
+reInicio.addEventListener('click', limpaCor);
+
+function limpaCor (){
+pixels .style.color=white;
+}
+
+//let color_Black = document.getElementsByClassName("color")[0]
+//if(color_Black  == 'black') {
+ //     div.classList.Add('selected')
+
 //function Color1() { 
-  //   let colorInicitial = document.getElementsByClassName('color')[0].class=Selected;
+  //   let colorInicitial = document.getElementsByClassName('color')[0].class= Selected;
 //}
 
 
