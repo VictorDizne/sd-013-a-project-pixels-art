a6c392afe39004061bf0f77a88bec6aa187f9f00
Seu nomecd <canaldogildosantos@gmail.com>
2021-06-06 10:26:39 -0300

    apaga tentativas realizadas e que zeraram a avaliação do projeto

--- script.js
+++ script.js
@@ -3,28 +3,17 @@
  document.getElementsByClassName('color')[2].style.background = "red"
  document.getElementsByClassName('color')[3].style.background = "yellow"
  
-  window.onload= iniciando
-function iniciando(){
+window.onload= iniciando
+   function iniciando(){
       alert ("Crie a sua Arte");
+      alert ("inicia com a cor preta selecionada");
 }
 
-function Color1() { 
-     let colorInicitial = document.getElementsByClassName('color')[0].class=Selected;
-}
+//function Color1() { 
+  //   let colorInicitial = document.getElementsByClassName('color')[0].class=Selected;
+//}
 
-let limpaCor = document.getElementsById("color-palette");
-for ( let i= 0; i < color-palette.length; i += 1){
- let reInicio = color-palette [i];
- reInicio = clear-bord;
- clear-bord.addEventListener("click, Apagar"); 
- 
-}
-function apagar() {
-      for (let i = 0; i < clear-bord.length; i += 1) {
-         let pixels = clear-bord [i];
-         pixels.style.backgroundColor= white;
-   }  
-}
+
 
   
         
