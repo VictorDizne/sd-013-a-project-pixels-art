2c302c479d9179b8dd15fb5f3d0b6f637ce196ce
Seu nomecd <canaldogildosantos@gmail.com>
2021-06-06 15:54:42 -0300

    acrescenta selected a paleta de cores

--- script.js
+++ script.js
@@ -3,25 +3,35 @@
  document.getElementsByClassName('color')[2].style.background = "red"
  document.getElementsByClassName('color')[3].style.background = "yellow"
  
-//window.onload= iniciando
+let preto = getElementsByClassName("color")[0];
+let azul = getElementsByClassName("color")[1];
+let vermelho = getElementsByClassName("color")[2];
+let amarelo = getElementsByClassName("color")[3];
+
+preto.addEventListener('click', addClassSelect);
+azul.addEventListener('click', addClassSelect);
+vermelho.addEventListener('click', addClassSelect);
+amarelo.addEventListener('click', addClassSelect);
+
+ //window.onload= iniciando
   // function iniciando(){
     //  alert ("Crie a sua Arte");
       //alert ("inicia com a cor preta selecionada");
 //}
 
-window.onload= inicioCor
-function inicioCor() {
-      document.getElementsByClassName('color')[0].classList.add('selected')
+window.onload = inicioCor();
+   function inicioCor() {
+   document.getElementsByClassName('color')[0].classList.add('selected')
 
 }
 
-let pixels= document.getElementsByClassName("pixel");
-let reInicio = document.getElementById("clear-bord");
-reInicio.addEventListener('click', limpaCor);
+//let pixels= document.getElementsByClassName("pixel");
+//let reInicio = document.getElementById("clear-bord");
+//reInicio.addEventListener('click', limpaCor);
 
-function limpaCor (){
-pixels .style.color=white;
-}
+//function limpaCor (){
+//pixels .style.color=white;
+//}
 
 //let color_Black = document.getElementsByClassName("color")[0]
 //if(color_Black  == 'black') {
