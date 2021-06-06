cb396cff9d129fd3290b5febba31060b45097cc1
Micael Santos <micaelmaicon@hotmail.com>
2021-06-06 17:26:39 -0300

    Finalize requirement bonus 10 - fix html

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Iniciando o projeto.
 const colorBlack = document.getElementById('colorBlack');
 colorBlack.style.backgroundColor = 'black';
 const colorRed = document.getElementById('colorRed');
@@ -7,24 +6,48 @@ const colorBlue = document.getElementById('colorBlue');
 colorBlue.style.backgroundColor = 'Blue';
 const colorGreen = document.getElementById('colorGreen');
 colorGreen.style.backgroundColor = 'Green';
+
+// Escolher tamanho do board
+const inputSize = document.getElementById('board-size');
+const btnGenerateBorder = document.getElementById('generate-board');
+const accessTable = document.getElementById('pixel-board');
+
 let appliedColor = 'black';
+
+const btnClearBoard = document.getElementById('clear-board');
+
+// Funções que criam a tabela com tamanho 5.
+// Função que cria células.
+function cellCreate(size, row){
+  for(let index = 0; index < size; index  += 1){
+    const cell = document.createElement('td');
+    cell.classList = 'pixel';
+    row.appendChild(cell);
+  }
+}
+
+// função criar linhas (row)
+function rowCreate (size){
+  for(let index = 0; index < size; index += 1){
+    const row = document.createElement('tr');
+    accessTable.appendChild(row);
+    row.classList.add('pixel-row');
+    cellCreate(size, row);
+  }
+}rowCreate(5);
+
 const getPixelOnBoard = document.querySelectorAll('.pixel');
-const clearBoard = document.querySelector('#clear-board');
 
-//  Requirement 7.
-// adicionar a classe `classe selected` a cor selecionada e retira a mesma classe da outra cor
-colorBlack.addEventListener('click', colorSelectBlack);
-colorRed.addEventListener('click', colorSelectRed);
-colorBlue.addEventListener('click', colorSelectBlue);
-colorGreen.addEventListener('click', colorSelectGreen);
-clearBoard.addEventListener('click', changeBoardColor);
-
-function changeBoardColor() {
+// função para pintar o tabuleiro de branco
+function clearBoard() {
   for (let index = 0; index < getPixelOnBoard.length; index += 1){
     getPixelOnBoard[index].style.backgroundColor = 'white';
   }
 }
 
+btnClearBoard.addEventListener('click', clearBoard);
+
+// Funções que recebem a cor selecionada
 function colorSelectBlack(event) {
   colorRed.classList.remove('selected');
   colorBlue.classList.remove('selected');
@@ -34,7 +57,9 @@ function colorSelectBlack(event) {
   // Captura com 'event.target' a cor do background.
   const colorToApply = event.target.style.backgroundColor;
   appliedColor = colorToApply;
+  console.log(colorToApply);
 }
+colorBlack.addEventListener('click', colorSelectBlack);
 
 function colorSelectRed(event) {
   colorBlack.classList.remove('selected');
@@ -44,7 +69,9 @@ function colorSelectRed(event) {
   colorRed.classList.add('selected');
   const colorToApply = event.target.style.backgroundColor;
   appliedColor = colorToApply;
+  console.log(colorToApply);
 }
+colorRed.addEventListener('click', colorSelectRed);
 
 function colorSelectBlue(event) {
   colorBlack.classList.remove('selected');
@@ -54,7 +81,9 @@ function colorSelectBlue(event) {
   colorBlue.classList.add('selected');
   const colorToApply = event.target.style.backgroundColor;
   appliedColor = colorToApply;
+  console.log(colorToApply);
 }
+colorBlue.addEventListener('click', colorSelectBlue);
 
 function colorSelectGreen(event) {
   colorBlack.classList.remove('selected');
@@ -63,16 +92,43 @@ function colorSelectGreen(event) {
   colorGreen.classList.add('selected');
   const colorToApply = event.target.style.backgroundColor;
   appliedColor = colorToApply;
+  console.log(colorToApply);
+}
+colorGreen.addEventListener('click', colorSelectGreen);
+
+// Obtêm novo tamanho do boarde n*n.
+function getPixelColor(){
+  const sizeClassPixel = document.querySelectorAll('.pixel');
+  pixelColorListener(sizeClassPixel);
 }
 
-// Requirement 8
-// adicionar um evento click que seleciona aquele elemento clicado e muda uma propriedade
-function pixelColor() {
-  for (let index = 0; index < getPixelOnBoard.length; index += 1) {
-    getPixelOnBoard[index].addEventListener('click', function () {
-      getPixelOnBoard[index].style.backgroundColor = appliedColor;
+function pixelColorListener(who) {
+  for (let index = 0; index < who.length; index += 1) {
+    who[index].addEventListener('click', function () {
+    who[index].style.backgroundColor = appliedColor;
     });
+  }
+} pixelColorListener(getPixelOnBoard);
+
+function checkBoardLimits (){
+  if(inputSize.value < 5){
+    inputSize.value = 5;
+  }
+  if(inputSize.value > 50){
+    inputSize.value = 50;
   }
 }
 
-pixelColor();
+// Cria um novo board a partir do input do usuário.
+function boardSizeChange() {
+  if (inputSize.value) {
+    checkBoardLimits();
+    accessTable.innerHTML = '';
+    rowCreate(inputSize.value);
+    getPixelColor();
+  }else {
+    alert('Board Inválido!');
+  }
+}
+
+btnGenerateBorder.addEventListener('click', boardSizeChange);
