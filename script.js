d1b8d74c2224c2525b59d21a4f1bcaf4422fa210
Leme <andresleme@gmail.com>
2021-06-07 02:59:02 -0300

    [MUST REFACTOR] Requirement 8

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 const pixelBoard = document.getElementById('pixel-board');
+const colorPalette = document.getElementById('color-palette');
+const colorArray = ['black', 'red', 'green', 'blue'];
+const h1 = document.querySelector('h1');
+
+for (let color in colorArray){
+  const div = document.createElement('div');
+  div.classList.add('color');
+  div.style.backgroundColor = colorArray[color];
+  if (div.style.backgroundColor == 'black') {
+    div.classList.add('selected')
+  }
+  colorPalette[color];
+  colorPalette.appendChild(div);  
+}
+
 
 function createLine() {
   let div = document.createElement('div');
@@ -18,10 +33,51 @@ const collorPalette = document.getElementById('color-palette');
 const colors = document.getElementsByClassName('color');
 
 // Referencia: https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
-collorPalette.addEventListener("click", function (event) {
+
+collorPalette.addEventListener("click", function(event) {
+  // for (let div in colors) {
+  //   colors[div].classList.remove('selected');
+  // }
   for (let div of colors) {
     div.classList.remove('selected');
   }
   event.target.classList.add('selected');
 });
+
+// let pixel = document.getElementsByClassName('pixel');
+// let color = document.getElementsByClassName('selected');
+
+function questionEight() {
+  const pixels = document.getElementsByClassName('pixel');
+  const selected = document.getElementsByClassName('selected');
+    for (let i = 0; i < pixels.length; i += 1) {
+    const pixel = pixels[i];
+    pixel.addEventListener('click', (() => {
+    for (let j = 0; j < selected.length; j += 1) {
+    const select = selected[j];
+    pixel.style.backgroundColor = select.style.backgroundColor;
+    }
+  }));
+  }
+}
+  questionEight(); 
+
+
+//   // for (let pixelClick of pixel) {
+// for (let i = 0; i < pixel.length; i += 1) {
+//   let pixelClick = pixel[i];
+//   pixelClick.addEventListener('click', () => {
+
+//   //for (let colorSelected of color) {
+//     for (let j = 0; j < color.length; j += 1) {
+//     let colorSelected = color[j];
+//     pixelClick.style.backgroundColor = colorSelected.style.backgroundColor;
+//   }
+// })
+// }
+
+// .addEventListener("click", function(event) {
+//   //for (let i of color) {
+    
+//   }
 
