fc5f52ee83ce6d8de853d3bd0cd26ae3f2574e14
LuanRodriguesep <luanrodriguesep@yahoo.com.br>
2021-06-06 21:09:42 -0300

    requisito 7 e 8

--- script.js
+++ script.js
@@ -1,67 +1,100 @@
 
 window.onload = selectedDefault
 
-function selectedDefault(){
-  let selected = document.querySelector('.black')
-  selected.classList.add("selected")
+let forColors = document.getElementsByClassName('color');
+
+function selectedDefault() {
+  for (let i = 0; i < forColors.length; i++) {
+    if (i === 0) {
+      forColors[i].classList.add("selected")
+    }
+  }
 }
+
+function paletteColor() {
+  for (let i = 0; i < forColors.length; i++) {
+    if (i === 0) {
+      forColors[i].style.backgroundColor = "black";
+    } else if (i === 1) {
+      forColors[i].style.backgroundColor = "red";
+    } else if (i === 2) {
+      forColors[i].style.backgroundColor = "blue";
+    } else if (i === 3) {
+      forColors[i].style.backgroundColor = "Green";
+    }
+  }
+}
+paletteColor()
+
 
 function montarQuadro() {
   let pixelBoard = document.querySelector("#pixel-board")
-  let quadroPixel = []
 
   for (let index = 0; index < 25; index++) {
 
     let quadrado = document.createElement('div')
     quadrado.setAttribute("class", "pixel")
     pixelBoard.appendChild(quadrado)
-
-
   }
 }
+
 montarQuadro()
 
-let clickBlack = document.querySelector('.black');
-clickBlack.addEventListener("click", receberClickBlack);
-
-function receberClickBlack (evento) {
-  console.log(evento.target);
-  clickBlack.classList.add("selected");
-  clickRed.classList.remove("selected")
-  clickBlue.classList.remove("selected")
-  clickGreen.classList.remove("selected")
+function classRemove() {
+  for (let i = 0; i < forColors.length; i++) {
+    if (forColors[i].classList.contains("selected")) {
+      forColors[0].classList.remove("selected")
+      forColors[1].classList.remove("selected")
+      forColors[2].classList.remove("selected")
+      forColors[3].classList.remove("selected")
+    } else if (forColors[0].classList.contains("selected")) {
+      forColors[1].classList.remove("selected")
+      forColors[2].classList.remove("selected")
+      forColors[3].classList.remove("selected")
+    } else if (forColors[1].classList.contains("selected")) {
+      forColors[0].classList.remove("selected")
+      forColors[2].classList.remove("selected")
+      forColors[3].classList.remove("selected")
+    } else if (forColors[2].classList.contains("selected")) {
+      forColors[0].classList.remove("selected")
+      forColors[1].classList.remove("selected")
+      forColors[3].classList.remove("selected")
+    } else if (forColors[3].classList.contains("selected")) {
+      forColors[0].classList.remove("selected")
+      forColors[1].classList.remove("selected")
+      forColors[2].classList.remove("selected")
+    } 
+  }
 }
 
-let clickRed = document.querySelector('.red');
-clickRed.addEventListener("click", receberClickRed);
-
-function receberClickRed (evento) {
-  console.log(evento.target);
-  clickRed.classList.add("selected");
-  clickBlack.classList.remove("selected")
-  clickBlue.classList.remove("selected")
-  clickGreen.classList.remove("selected")
+for (let i = 0; i < forColors.length; i++) {
+  forColors[i].addEventListener("click", classSelected);
 }
 
-let clickBlue = document.querySelector('.blue');
-clickBlue.addEventListener("click", receberClickBlue);
-
-function receberClickBlue (evento) {
-  console.log(evento.target);
-  clickBlue.classList.add("selected");
-  clickRed.classList.remove("selected")
-  clickBlack.classList.remove("selected")
-  clickGreen.classList.remove("selected")
+function classSelected(param) {
+  classRemove()
+  param.target.classList.add("selected");
 }
 
-let clickGreen = document.querySelector('.green');
-clickGreen.addEventListener("click", receberClickGreen);
 
-function receberClickGreen (evento) {
-  console.log(evento.target);
-  clickGreen.classList.add("selected");
-  clickRed.classList.remove("selected")
-  clickBlue.classList.remove("selected")
-  clickBlack.classList.remove("selected")
+function selected() {
+  for (let i = 0; i < forColors.length; i++) {
+    if (forColors[i].classList.contains('selected'))
+      return forColors[i].style.backgroundColor;
+  }
 }
 
+function recebeEvento(evento) {
+  let clicado = evento;
+  clicado.target.style.backgroundColor = selected();
+}
+
+let pixels = document.getElementsByClassName('pixel');
+
+function backgroundPixel() {
+  for (let index = 0; index < pixels.length; index++) {
+    pixels[index].addEventListener("click", recebeEvento)
+    }
+}
+
+backgroundPixel()
