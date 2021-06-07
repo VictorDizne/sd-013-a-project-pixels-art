5659e4b765639f74d77e14c500d7fd135aa0fda2
Adriana Barreto <adri92barreto123@gmail.com>
2021-06-06 21:46:27 -0300

    Executa questao oito

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
-window.onload = function (addEvendListener) {
-  corPreta;
-  corSelecionada;
-  colorir;
+function createPixelsBoard() {
+  for (let index = 0; index < 5; index += 1) {
+    const pixelBoard = document.querySelector('#pixel-board');
+    const line = document.createElement('div');
+    pixelBoard.appendChild(line);
+
+    for (let index = 0; index < 5; index += 1) {
+      let div = document.createElement('div');
+      div.className = 'pixel';
+      line.appendChild(div);
+    }
+  }
+}
+createPixelsBoard();
+
+
+function changeFocus(event) {
+  let actualFocus = document.querySelector('.selected');
+  actualFocus.className = 'color';
+  event.target.className = 'color selected';
+};
+let paletteArray = document.querySelectorAll('#color-palette');
+for (let i = 0; i < paletteArray.length; i += 1) {
+  paletteArray[i].addEventListener('click', changeFocus);
 };
 
-function corPreta () {
-  let selecionaPreto = document.getElementById('black');
-  selecionaPreto.classList.add('selected');
+function changePixels(event) {
+  let actualColor = document.querySelector('.selected').style.backgroundColor;
+  event.target.style.backgroundColor = actualColor;
 }
+let pixelsArray = document.querySelectorAll('.pixel');
+for (let i = 0; i < pixelsArray.length; i += 1) {
+  pixelsArray[i].addEventListener('click', changePixels);
+};
 
-let corSelecionada = document.querySelector('#color-palette');
-corSelecionada.addEventListener('click', colorir);
-
-function colorir(event) {
-  let seleciona = document.querySelector('.selected');
-  seleciona.classList.remove('selected')
-  event.target.classList.add('selected'); 
-}
-
+//Feito com auxilio de Josué Lobo
