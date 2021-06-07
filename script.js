1decebd09fff96e9fa51e460aa8c587b89b8d78e
Isabella Antunes <bella.antunes2@gmail.com>
2021-06-07 17:14:25 -0300

    Resolução do exercício 8

--- script.js
+++ script.js
@@ -6,9 +6,29 @@ getfirstColorDiv.classList.add('selected');
 const getDivColorPalette = document.querySelector('#color-palette');
 const getDivsColorized = document.querySelectorAll('.color');
 
-getDivColorPalette.addEventListener('click', (event) => {
-  for (let index = 0; index < getDivsColorized.length; index += 1) {
-    getDivsColorized[index].classList.remove('selected');
-  }
-  event.target.classList.add('selected');
-});
+function selectingColor() {
+  getDivColorPalette.addEventListener('click', (event) => {
+    for (let index = 0; index < getDivsColorized.length; index += 1) {
+      getDivsColorized[index].classList.remove('selected');
+    }
+    event.target.classList.add('selected');
+  });
+}
+
+selectingColor();
+
+function coloringTable() {
+  const getDivTable = document.querySelector('#pixel-board');
+  const getPixels = document.querySelectorAll('.pixel');
+
+  getDivTable.addEventListener('click', (event) => {
+    for (let index = 0; index < getPixels.length; index += 1) {
+      const getDivSelected = document.querySelector('.selected');
+      const colorSelected = window.getComputedStyle(getDivSelected).backgroundColor;
+      const evento = event;
+      evento.target.style.backgroundColor = colorSelected;
+    }
+  });
+}
+
+coloringTable();
