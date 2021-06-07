2f24d49f1694d883448008f0d655fec800a38ba8
Pedro Delicoli <pedrodelicoli@hotmail.com>
2021-06-07 17:06:11 -0300

    Formatando

--- script.js
+++ script.js
@@ -2,62 +2,49 @@ function setPaletteColor(event) {
   let x = event.target;
   let myPalettes = document.getElementsByClassName('color');
   for (let index = 0; index < myPalettes.length; index += 1){
-    myPalettes[index].classList.remove("selected");
+    myPalettes[index].classList.remove('selected');
   }
-  x.classList.add("selected");    
-};
+  x.classList.add('selected');
+}
 
 function setPixelColor() {
-  let selectedColor = document.getElementsByClassName('color selected');
-  let pixels = document.querySelector('#pixel-board');
- 
-  pixels.addEventListener('click', function(event){
+  const selectedColor = document.getElementsByClassName('color selected');
+  const pixels = document.querySelector('#pixel-board');
+
+  pixels.addEventListener ('click', function(event) {
     if (selectedColor.length > 0) {
-      let color = selectedColor[0].style.backgroundColor;
+      const color = selectedColor[0].style.backgroundColor;
       event.target.style.backgroundColor = color;
     }
   });
-};
+}
 
 function buttomClear() {
-  let pixels = document.getElementsByClassName('pixel');
-  let buttom = document.querySelector('#clear-board');
-  let color = 'white';
+  const pixels = document.getElementsByClassName('pixel');
+  const buttom = document.querySelector('#clear-board');
+  const color = 'white';
 
-  buttom.addEventListener('click', function(){
-    for (let index = 0; index < pixels.length; index += 1){
+  buttom.addEventListener ('click', function() {
+    for (let index = 0; index < pixels.length; index += 1) {
       if (pixels[index].style.backgroundColor !== color) {
         pixels[index].style.backgroundColor = color;
       }
     }
   });
-};
+}
 
-function colorPallete(){
+function colorPallete() {
   const colorPixel1 = document.getElementsByClassName('color')[0];
   colorPixel1.style.backgroundColor = 'black';
   const colorPixel = document.getElementsByClassName('color');
-  for (index = 1; index < colorPixel.length; index += 1){
-    colorPixel[index].style.backgroundColor = Math.floor(Math.random() * 16777215).toString(16);
-    if (colorPixel[1].backgroundColor === colorPixel[2].backgroundColor || colorPixel[1].backgroundColor === colorPixel[3].backgroundColor){
-      colorPixel[index].style.backgroundColor = Math.floor(Math.random() * 16777215).toString(16);
+  for (let index = 1; index < colorPixel.length; index += 1) {
+    colorPixel[index].style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    if (colorPixel[index].backgroundColor === undefined) {
+      colorPixel[index].style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
     }
-    else if (colorPixel[index].backgroundColor === '#ffffff'){
-      colorPixel[index].style.backgroundColor = Math.floor(Math.random() * 16777215).toString(16);
-    } 
   }
-};
+}
 
-/* function getRandomColor() {
-  let color;
-  do {
-    
-    color = Math.floor(Math.random() * 16777215).toString(16);
-  } while (color.length !== 6 || color === 'ffffff');
-
-  return `#${color}`;
-}  */
- 
 window.onload = function () {
   colorPallete()
   setPixelColor();
