3230142cdd2aaf07e79b5b2a3eb57abf1c50a669
PC <sthecso>
2021-06-07 15:22:28 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ function changeClass() {
     colorBlack.classList.add('selected');
 }
     window.onload= changeClass;
+    
+function colors() {
+    const palette = document.querySelector('#color-palette');
+    const classColors = document.getElementsByClassName('color');
+    palette.addEventListener('click', (event) => {
+        for (let index = 0; index < classColors.length; index += 1) {
+            classColors[index].classList.remove('selected');
+        } 
+        event.target.classList.add('selected');
+    });
+} 
+colors();
