bfe6c1587fa318dbd80a02ff31bd6c1938f233a0
Edgar Melo <edgarmeloam@gmail.com>
2021-06-07 17:44:29 -0300

    Adiciona a pagina uma paleta de 4 cores distintas

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-<script>    
+const colorBlack = document.getElementById('colorBlack');
+colorBlack.style.backgroundColor = 'black';
+const colorRed = document.getElementById('colorRed');
+colorRed.style.backgroundColor = 'Red';
+const colorBlue = document.getElementById('colorBlue');
+colorBlue.style.backgroundColor = 'Blue';
+const colorGreen = document.getElementById('colorGreen');
+colorGreen.style.backgroundColor = 'Green';
