3a5046d203edeb3ea6d412c9efd4cf88d15b730c
Jaqueline Silva <jaqueline.natario@gmail.com>
2021-06-07 19:35:54 -0300

    Resolve o requisito 7

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
+// Requisito 6
 function colorBlack() {
-  const getBlack = document.querySelector('.selected');
-  getBlack.classList.add('selected');
+  const getBlack = document.querySelector('#first-color');
+  getBlack.classList.add('selected'); // pega o elemento da const "getBlack" e adiciona a classe .selected.
 }
 
 window.onload = colorBlack;
+
+// Requisito 7
+function paletaCores() {
+  let colors = document.getElementsByClassName('color');
+  let colorPalette = document.getElementById('color-palette');
+
+  colorPalette.addEventListener('click', ((event) => {
+    for (let index = 0; index < colors.length; index += 1) {
+      let colorDiv = colors[index];
+      colorDiv.classList.remove('selected');
+    }
+    event.target.classList.add('selected');
+  }));
+}
+paletaCores();
