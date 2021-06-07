7251ea0850c826d0ae8d78511dbaab007d00abd0
Walace Nascimento <wlacens@hotmail.com>
2021-06-07 20:25:18 -0300

    reorganizando tudo

--- script.js
+++ script.js
@@ -12,12 +12,31 @@ function selectColor() {
 
     color.addEventListener('click', ((event) => {
       for (let j = 0; j < colors.length; j += 1) {
-        const colorDiv = colors[j]
+        const colorDiv = colors[j];
         colorDiv.classList.remove('selected');
       }
       event.target.classList.add('selected');
-
     }));
   }
 }
 selectColor();
+
+function questionNine() {
+  const div = document.createElement('div');
+  const pixels = document.getElementsByClassName('pixel');
+  const pixelsBoard = document.getElementById('pixel-board');
+  const button = document.createElement('input');
+  button.setAttribute('type', 'button');
+  button.setAttribute('value', 'Limpar');
+  button.setAttribute('id', 'clear-board');
+
+  button.addEventListener('click', () => {
+    for (let i = 0; i < pixels.length; i += 1) {
+      const pixel = pixels[i];
+      pixel.style.backgroundColor = ('White');
+    }
+  });
+  div.appendChild(button);
+  document.body.insertBefore(div, pixelsBoard);
+}
+questionNine();
