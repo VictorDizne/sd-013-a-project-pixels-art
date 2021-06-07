00c2ee4905a337786b4f14d1abbba0ced0d1abea
Leme <andresleme@gmail.com>
2021-06-06 23:45:40 -0300

    Requirement 7

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
-const frame = document.getElementById('pixel-board');
+const pixelBoard = document.getElementById('pixel-board');
 
 function createLine() {
   let div = document.createElement('div');
   div.className = 'pixel';
-  frame.appendChild(div);
+  pixelBoard.appendChild(div);
 };
-
-// function createLine() {
-//   let div = document.createElement('div');
-//   div.className = 'pixel';
-//   frame.appendChild(div);
-  
-// };
 
 for (let line = 1; line <= 5; line += 1) {
   for (let column = 1; column <= 5; column += 1) {
     createLine();
   };
   let nextLine = document.createElement('br');
-  frame.appendChild(nextLine)
+  pixelBoard.appendChild(nextLine)
 };
+
+const collorPalette = document.getElementById('color-palette');
+const colors = document.getElementsByClassName('color');
+
+// Referencia: https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
+collorPalette.addEventListener("click", function (event) {
+  for (let div of colors) {
+    div.classList.remove('selected');
+  }
+  event.target.classList.add('selected');
+});
+
