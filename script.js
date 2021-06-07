6fdc55a4adb12f01f560ff9b47ad1802deb867df
Edgar Melo <edgarmeloam@gmail.com>
2021-06-07 17:58:03 -0300

    Adiciona tamanho da borda

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
+//adiciona paleta de cores a página
 const colorBlack = document.getElementById('colorBlack');
 colorBlack.style.backgroundColor = 'black';
 const colorRed = document.getElementById('colorRed');
 colorRed.style.backgroundColor = 'Red';
-const colorBlue = document.getElementById('colorBlue');
-colorBlue.style.backgroundColor = 'Blue';
-const colorGreen = document.getElementById('colorGreen');
-colorGreen.style.backgroundColor = 'Green';
+const colorBlue = document.getElementById('colorGreen');
+colorBlue.style.backgroundColor = 'Green';
+const colorGreen = document.getElementById('colorBlue');
+colorGreen.style.backgroundColor = 'Blue';
+
+//escolhe o tamanho da borda
+const inputSize = document.getElementById('board-size');
+const btnGenerateBorder = document.getElementById('generate-board');
+const accessTable = document.getElementById('pixel-board');
+
+let appliedColor = 'black';
+
+const btnClearBoard = document.getElementById('clear-board');
+
