b953aaece264fc303faf5949dd21ccbf24f8ae06
Caio-Borin <caioborin@gmail.com>
2021-06-07 16:33:38 -0300

    Exercicio 7

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-function criaQuadro(tamanho) {
-  const pixelBoard = document.getElementById(pixelBoard);
-  for (let coluna = 0; coluna < tamanho; coluna += 1) {
-    const novaColuna = document.createElement('div');
-    novaColuna.classname = 'linhaDePixels';
-    for (let linha = 0; linha < tamanho; linha += 1) {
-      const novoPixel = document.createElement('div');
-      novoPixel.className = 'pixel';
-      novoPixel.style.backgroundColor = 'white';
-      novaColuna.appendChild(novoPixel);
+function colorSelection() {
+  const colorBlack = document.querySelector('.black');
+  colorBlack.classList.add('selected');
+  const selected = document.getElementsByClassName('selected');
+
+  document.addEventListener('click', function (event) {
+    if (event.target.classList.contains('color')) {
+      for (let index = 0; index < selected.length; index += 1) {
+        selected[index].classList.remove('selected');
+      }
+      event.target.classList.add('selected');
     }
-    pixelBoard.appendChild(novaColuna);
-  }
-  const dimensaoCorreta = 42 * tamanho;
-  const tamanhoDoQuadro = document.getElementById('pixel-board');
-  tamanhoDoQuadro.style.width = `${dimensaoCorreta}px`;
-  tamanhoDoQuadro.style.heigth = `${dimensaoCorreta}px`;
+  });
 }
+colorSelection();
