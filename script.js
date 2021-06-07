0b89081f6d2332e728d6cee2bc13d7f11045f64e
Pablo Almeida <pabloalmeidac@hotmail.com>
2021-06-07 17:13:11 -0300

    8 questao tentando

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+
+
+window.onload = function() {
 let cores = document.getElementsByClassName('color');
+let pixels = document.getElementsByClassName('pixel');
 
 cores[0].style.backgroundColor="black" 
 cores[1].style.backgroundColor="#2c3e50";
@@ -6,11 +10,15 @@ cores[2].style.backgroundColor="#7f8c8d";
 cores[3].style.backgroundColor="#1abc9c";
 
 cores[0].classList.add('selected');
+console.log(cores[0]);
+
 
 for (let index = 0; index <cores.length; index +=1) {
 	cores[index].addEventListener('click', () => {
+		pintar();
 		removeAllSelectors();
 		cores[index].classList.add('selected');
+		
 	});
 };
 
@@ -20,3 +28,15 @@ function removeAllSelectors() {
 	};
 };
 
+function pintar() {
+	for(let i =0; i<cores.length; i+=1) {
+		if(cores[i].classList.contains('selected')){
+			for(let j=0; j<pixels.length; j+=1) {
+				pixels[j].addEventListener('click', () => {
+					pixels[j].style.backgroundColor = cores[i].style.backgroundColor;
+				});
+			}
+		}	
+	}
+}
+};
