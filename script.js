4658e43bf4d4e7846e7e87aa20c04d9d1d3a76ae
Bruno <brunoalencarlima@outlook.com.br>
2021-06-07 20:28:14 -0300

    questao 6

--- script.js
+++ script.js
@@ -9,4 +9,19 @@ window.onload = function () {
         }
     }
     divCreator();
+     function selectCollor() {
+     let pallete = document.getElementsByClassName('color');
+     let otherColors = ['blue', 'red', 'yellow'];
+     console.log(pallete);
+     for (let i = 0; i < pallete.length; i+=1) {
+       pallete[0].style.background = 'black';
+       pallete[0].id = 'black';
+       pallete[0].classList.add('selected');
+       if (i>0) {
+         pallete[i].style.background = otherColors[i - 1];
+         pallete[i].id = otherColors[i - 1];
+       }
+    } 
+  }
+  selectCollor(); 
 }
