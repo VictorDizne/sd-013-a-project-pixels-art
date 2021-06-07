91f0e414875e495f1a0ddaa9a6465875b0753e1a
Isabella Antunes <bella.antunes2@gmail.com>
2021-06-07 15:35:41 -0300

    Resolução do exercício 7

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-const firstColorDiv = document.getElementById('color1');
-firstColorDiv.style.backgroundColor = 'black';
+const getfirstColorDiv = document.getElementById('color1');
+getfirstColorDiv.style.backgroundColor = 'black';
 
-firstColorDiv.classList.add('selected');
+getfirstColorDiv.classList.add('selected');
+
+const getDivColorPalette = document.querySelector('#color-palette');
+const getDivsColorized = document.querySelectorAll('.color');
+
+getDivColorPalette.addEventListener('click', (event) => {
+  for (let index = 0; index < getDivsColorized.length; index += 1) {
+    getDivsColorized[index].classList.remove('selected');
+  }
+  event.target.classList.add('selected');
+});
