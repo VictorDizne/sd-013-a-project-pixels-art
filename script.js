16dff401632091fae3db5d0f4022754ca5ffe321
Fabíola Rodrigues Moutinho <fabiolamoutinho@hotmail.com>
2021-06-07 21:37:35 -0400

    Correção dos Requisitos 4 e 5

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function pixelBoard() {
     line.className = 'row';
     document.querySelector('#pixel-board').appendChild(line);
 
-    for (i = 0; i < 5; i += 1) {
+    for (let i = 0; i < 5; i += 1) {
       const col = document.createElement('div');
       col.className = 'pixel';
       line.appendChild(col);
