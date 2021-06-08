98e40c3f34aeb67d8b27be8f0236a6b52b5d2a91
anastacioneto <anastacio.a.c.n@gmail.com>
2021-06-07 21:24:04 -0300

    [anastacioneto] verifica requisito 4

--- script.js
+++ script.js
@@ -1,33 +1,48 @@
-// pode ser usado tambem: document.addEventListener('DOMContentLoaded',()) =>
-window.onload = function () {
-  let colum = 5;
-  let numberOfLines = colum;
-  let controlLeft = numberOfLines;
-  let controlRight = numberOfLines;
-  let lines = document.querySelectorAll(".line");
+// Pode ser usado tambem: document.addEventListener('DOMContentLoaded',()) =>
+// função que acrescenta colunas a área de pintura.
+window.onload = function tabela (linha, coluna) {
+  let linhas = 5;
+  let lines = document.querySelectorAll(".pixel");
 
   fillPixels(lines);
-
   function fillPixels(lines) {
     for(let index = 0; index < lines.length; index += 1) {
       fillLine(lines[index]);
-      controlRight += 1;
-      controlLeft -= 1;
     }
   }
 
-  function createBox(classN) {
-    let box = document.createElement("div");
-    box.className = classN;
-    return box;
+  function createPixels(pixelsDiv) {
+    let divPixel = document.createElement("div");
+    divPixel.className = pixelsDiv;
+    return divPixel;
   }
 
   function fillLine(divLine) {
-    for (let lineColumn = 1; lineColumn <= colum; lineColumn += 1) {
-        let box = createBox("box");
-        divLine.appendChild(box);
-
+    for (let lineColumn = 1; lineColumn <= linhas; lineColumn += 1) {
+      let pixel = createPixels("pixel");
+      divLine.appendChild(pixel);
     }
   }
-
 }
+/*
+let pixelBoard = document.querySelector(".containerDePixels");
+
+pixelBoard.addEventListener('click', function (event){
+  event.target.classList.toggle('black');
+});
+*/
+
+function pintaPixelDePreto (event) {
+  let divSelecionada = event.target;
+  divSelecionada.classList.toggle('black');
+}
+let divs = document.querySelectorAll("*");
+
+for (let indice = 0; indice < divs.length; indice += 1) {
+  divs[indice].addEventListener('click', pintaPixelDePreto);
+};
+
+function pintaDePreto() {
+    document.querySelector("#pixel-board").style.backgroundColor = "pink";
+  }
+
