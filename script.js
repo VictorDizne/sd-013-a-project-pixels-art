22956216143fefa580f558eedf4a5ee23d3bcec4
anastacioneto <anastacio.a.c.n@gmail.com>
2021-06-08 12:04:05 -0300

    [anastacioneto] Ateração do código com função apenas para pintar pixels

--- script.js
+++ script.js
@@ -1,48 +1,14 @@
-// Pode ser usado tambem: document.addEventListener('DOMContentLoaded',()) =>
-// função que acrescenta colunas a área de pintura.
-window.onload = function tabela (linha, coluna) {
-  let linhas = 5;
-  let lines = document.querySelectorAll(".pixel");
 
-  fillPixels(lines);
-  function fillPixels(lines) {
-    for(let index = 0; index < lines.length; index += 1) {
-      fillLine(lines[index]);
-    }
-  }
+// Função que define a cor da caneta, iniciando com preto
+let penColor = 'black';
 
-  function createPixels(pixelsDiv) {
-    let divPixel = document.createElement("div");
-    divPixel.className = pixelsDiv;
-    return divPixel;
-  }
-
-  function fillLine(divLine) {
-    for (let lineColumn = 1; lineColumn <= linhas; lineColumn += 1) {
-      let pixel = createPixels("pixel");
-      divLine.appendChild(pixel);
-    }
-  }
+function setPenColour(color) {
+	penColor = color;
 }
-/*
-let pixelBoard = document.querySelector(".containerDePixels");
-
-pixelBoard.addEventListener('click', function (event){
-  event.target.classList.toggle('black');
-});
-*/
-
-function pintaPixelDePreto (event) {
-  let divSelecionada = event.target;
-  divSelecionada.classList.toggle('black');
+// Função qe pinta o pixel ao click conforme a cor selecionada
+function setPixelColour(pixel) {
+	pixel.style.backgroundColor = penColor;
 }
-let divs = document.querySelectorAll("*");
 
-for (let indice = 0; indice < divs.length; indice += 1) {
-  divs[indice].addEventListener('click', pintaPixelDePreto);
-};
 
-function pintaDePreto() {
-    document.querySelector("#pixel-board").style.backgroundColor = "pink";
-  }
 
