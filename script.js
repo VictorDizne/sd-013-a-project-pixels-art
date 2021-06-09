fe79dece036dd34975ece784aa1a7a96a568bb94
Fabíola Rodrigues Moutinho <fabiolamoutinho@hotmail.com>
2021-06-09 10:48:31 -0400

    Requisito 8 e correções no estilo de codificação

--- script.js
+++ script.js
@@ -21,18 +21,34 @@ function initialBlackColor() {
 window.onload = initialBlackColor;
 
 function select() {
-  const color = document.getElementsByClassName('color');
-  const palette = document.getElementById('color-palette');
+  const color = document.querySelectorAll('.color');
+  const palette = document.querySelector('#color-palette');
 
-  function colorSelect(event) {
+  function colorSelect(addSelectedClass) {
     for (let index1 = 0; index1 < color.length; index1 += 1) {
       if (color[index1].classList.contains('selected')) {
         color[index1].classList.remove('selected');
       }
     }
-    event.target.classList.add('selected');
+    addSelectedClass.target.classList.add('selected');
   }
 
   palette.addEventListener('click', colorSelect);
 }
 select();
+
+function colorPixel() {
+  const clickedPixel = document.querySelectorAll('.pixel');
+
+  function coloredPixel(appColor) {
+    const selectedClass = document.querySelector('.selected');
+    const selectedColor = window.getComputedStyle(selectedClass).backgroundColor;
+    const appliedColor = appColor.target;
+    appliedColor.style.backgroundColor = selectedColor;
+  }
+
+  for (let index2 = 0; index2 < clickedPixel.length; index2 += 1) {
+    clickedPixel[index2].addEventListener('click', coloredPixel);
+  }
+}
+colorPixel();
