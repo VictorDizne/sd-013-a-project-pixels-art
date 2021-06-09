abc224b00ae9375d35e987355bbcbc12a11f08f3
joaovitor-B <joao.vitorjv2000@gmail.com>
2021-06-09 13:47:02 -0300

    mais ajustes

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+  const paleta = document.querySelector('#color-palette');
+  paleta.addEventListener('click', selecionar);
 
+  function selecionar(event) {
+    const cores = document.querySelectorAll('.color');
+    console.log(cores)
+    for(let index = 0; index < cores.length; index+=1){
+      cores[index].classList.remove('selected');
+    }
+    event.target.classList.add('selected');
+  }
