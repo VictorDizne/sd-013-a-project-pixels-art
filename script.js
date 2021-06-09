892e7d879f9b8489f8e25f5f60367f06e8afb6d0
Pablo Almeida <pabloalmeidac@hotmail.com>
2021-06-08 21:30:48 -0300

    resolvendo bug 1 e 2 com ajuda do Luiz Furtado

--- script.js
+++ script.js
@@ -1,42 +1,49 @@
-
-
-window.onload = function() {
+// recupero a paleta de cores e boto no array chamado cores (as 4)
 let cores = document.getElementsByClassName('color');
+
+// recupero os pixels
 let pixels = document.getElementsByClassName('pixel');
 
+// seto as cores em cada indice do array
 cores[0].style.backgroundColor="black" 
 cores[1].style.backgroundColor="#2c3e50";
 cores[2].style.backgroundColor="#7f8c8d";
 cores[3].style.backgroundColor="#1abc9c";
 
+// adiciono a classe selected na cor preta
 cores[0].classList.add('selected');
-console.log(cores[0]);
 
-
+// Percorro o array cores para adicionar a classe selected quando for clicado.
+// Utilizo as funções pintar e remover a classe selected
 for (let index = 0; index <cores.length; index +=1) {
-	cores[index].addEventListener('click', () => {
-		pintar();
+	cores[index].addEventListener('click', (event) => {
+		console.log("teste");
 		removeAllSelectors();
-		cores[index].classList.add('selected');
-		
+		event.target.classList.add('selected');
+		paint();
 	});
 };
 
+// função pra remover a classe selected por todo o array cores
 function removeAllSelectors() {
 	for (let index = 0; index <cores.length; index +=1) {
 			cores[index].classList.remove('selected');
 	};
 };
 
-function pintar() {
+// Aqui faço uma verificação para saber se tem alguma cor selecionada e depois percorro o array pixels para pintar
+function paint() {
 	for(let i =0; i<cores.length; i+=1) {
 		if(cores[i].classList.contains('selected')){
 			for(let j=0; j<pixels.length; j+=1) {
 				pixels[j].addEventListener('click', () => {
 					pixels[j].style.backgroundColor = cores[i].style.backgroundColor;
 				});
-			}
-		}	
-	}
-}
+			};
+		};
+	};
 };
+
+paint();
+
+//agradecimento ao Luiz Furtado por ter resolvido comigo 2 bugs do projeto.
