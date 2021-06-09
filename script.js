48614e2c4b2d22343d2decddaedeba550189f9f7
anastacioneto <anastacio.a.c.n@gmail.com>
2021-06-09 19:13:28 -0300

    [anastacioneto] Testando requisito 8

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 
+
+// Função para deixar pixels brancos
+let colorWhite = 'white';
+
+function whiteColor() {
+let divs = document.querySelectorAll(".pixel");
+for (let indice = 0; indice < divs.length; indice += 1) {
+  if (divs[indice] !== '#fff'){
+    divs[indice].style.backgroundColor = colorWhite;
+    }
+  }
+}
+
+
+
+
+// Referẽncia: https://projects.raspberrypi.org/pt-PT/projects/pixel-art/complete
+
 // Função que define a cor da caneta, iniciando com preto
 let penColor = 'black';
 
 function setPenColour(color) {
 	penColor = color;
 }
-// Função qe pinta o pixel ao click conforme a cor selecionada
+// Função que pinta o pixel conforme a cor selecionada
 function setPixelColour(pixel) {
 	pixel.style.backgroundColor = penColor;
 }
