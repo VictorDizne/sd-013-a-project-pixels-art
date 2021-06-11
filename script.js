1153b77022233bad0f417107c691c2bb1126e2ab
joaovitor-B <joao.vitorjv2000@gmail.com>
2021-06-11 18:58:51 -0300

    melhorando

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-  const paleta = document.querySelector('#color-palette');
-  paleta.addEventListener('click', selecionar);
+const paleta = document.querySelector('#color-palette');
+paleta.addEventListener('click', selecionar);
+const clear = document.querySelectorAll('#clear-board')
+const pixel = document.querySelector('.pixel')
 
-  function selecionar(event) {
-    const cores = document.querySelectorAll('.color');
-    console.log(cores)
-    for(let index = 0; index < cores.length; index+=1){
-      cores[index].classList.remove('selected');
-    }
-    event.target.classList.add('selected');
+function selecionar(event) {
+  const cores = document.querySelectorAll('.color');
+  for (let index = 0; index < cores.length; index += 1) {
+    cores[index].classList.remove('selected')
   }
+  event.target.classList.add('selected')
+}
