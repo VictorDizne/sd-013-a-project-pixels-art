840d2cd6c3d7f4f6aab684f23d14e8cdf6f5690c
Jaqueline Silva <jaqueline.natario@gmail.com>
2021-06-11 08:42:50 -0300

    Resolve o requisito 8

--- script.js
+++ script.js
@@ -8,15 +8,51 @@ window.onload = colorBlack;
 
 // Requisito 7
 function paletaCores() {
-  let colors = document.getElementsByClassName('color');
-  let colorPalette = document.getElementById('color-palette');
+  const colors = document.getElementsByClassName('color');
+  const colorPalette = document.getElementById('color-palette');
 
   colorPalette.addEventListener('click', ((event) => {
     for (let index = 0; index < colors.length; index += 1) {
-      let colorDiv = colors[index];
+      const colorDiv = colors[index];
       colorDiv.classList.remove('selected');
     }
     event.target.classList.add('selected');
   }));
 }
 paletaCores();
+
+// Requisito 8
+// Mudar a classe selecionada entre os elementos com a classe 'color'. Requisito 8.
+function changeSelected() {
+  const colors = document.getElementsByClassName('color');
+
+  function changeColorSelect(event) {
+  // eslint-disable-next-line no-restricted-syntax
+    for (const index of colors) {
+      if (index.classList.contains('selected')) {
+        index.classList.remove('selected');
+      }
+    }
+    event.target.classList.add('selected');
+  }
+  for (let i = 0; i < colors.length; i += 1) {
+    colors[i].addEventListener('click', changeColorSelect);
+  }
+}
+changeSelected();
+
+// Muda a cor de fundo do pixel clickado. Requisito 8.
+function colorClick() {
+  const pixels = document.getElementsByClassName('pixel');
+
+  function changeColor(event) {
+    const selectElement = document.querySelector('.selected');
+    const colorSelect = window.getComputedStyle(selectElement).backgroundColor;
+    event.target.style.backgroundColor = colorSelect;
+  }
+
+  for (let index = 0; index < pixels.length; index += 1) {
+    pixels[index].addEventListener('click', changeColor);
+  }
+}
+colorClick();
