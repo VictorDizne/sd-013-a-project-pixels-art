4d97b068016053a02e6cc9e1ef3bcfee2836b09e
junglejf <jfcchagas@id.uff.br>
2021-06-12 00:50:01 -0300

    quesito 7 aprovado

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const board = document.getElementById('pixel-board');
+const numPallets = document.getElementById('color-palette').getElementsByClassName('color');
 
 function fillBoard(linha, coluna) {
   for (let i = 0; i < linha; i += 1) {
@@ -6,11 +7,23 @@ function fillBoard(linha, coluna) {
     line.className = 'line';
 
     for (let j = 0; j < coluna; j += 1) {
-      const colum = document.createElement('div');
-      colum.className = 'colum';
-      line.appendChild(colum);
+      const pixel = document.createElement('div');
+      pixel.className = 'pixel';
+      line.appendChild(pixel);
     }
     board.appendChild(line);
   }
 }
 fillBoard(5, 5);
+
+function selectColor(e) {
+  console.log(e.target,"antes");
+  const lastSelect = document.querySelector('.selected');
+  lastSelect.classList.remove('selected');
+  e.target.classList.add('selected');
+  console.log(e.target,"depois e lastselected = ",lastSelect);
+}
+
+for (let i = 0; i < numPallets.length; i += 1) {
+  numPallets[i].addEventListener('click', selectColor);
+}
