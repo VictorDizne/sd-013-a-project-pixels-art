ed971be47e87c928dfb15f03062d85fdacecb063
Heleny Flavia <flaviaanahmias@gmail.com>
2021-06-13 17:56:26 -0400

    requisito 8

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ window.onload = colorBlack();
 function select() {
     const colorSelect = document.querySelectorAll('.color');
     const palette = document.querySelector('#color-palette');
-  
+
     function selectColor(addSelectedClass) {
       for (let index1 = 0; index1 < colorSelect.length; index1 += 1) {
         if (colorSelect[index1].classList.contains('selected')) {
@@ -18,8 +18,25 @@ function select() {
       }
       addSelectedClass.target.classList.add('selected');
     }
-  
+
     palette.addEventListener('click', selectColor);
 }
 select();
+
+//desafio 8
+function colorPixel() {
+    const clickedPixel = document.querySelectorAll('.pixel');
+
+    function coloredPixel(appColor) {
+      const selectedClass = document.querySelector('.selected');
+      const selectedColor = window.getComputedStyle(selectedClass).backgroundColor;
+      const appliedColor = appColor.target;
+      appliedColor.style.backgroundColor = selectedColor;
+    }
+  
+    for (let index2 = 0; index2 < clickedPixel.length; index2 += 1) {
+      clickedPixel[index2].addEventListener('click', coloredPixel);
+    }
+  }
+colorPixel();
 
