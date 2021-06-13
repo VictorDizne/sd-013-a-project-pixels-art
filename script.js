140f541db3764ea352d1d437d2dffcc164f6122d
cristianovitorino <57003922+cristianovitorino@users.noreply.github.com>
2021-06-13 15:54:44 -0300

    Adiciona requisito 7

---
+++ script.js
@@ -0,0 +1,17 @@
+function setSelected() {
+  const color = document.querySelectorAll('.color');
+
+  for (let i = 0; i < color.length; i += 1) {
+    const colorIndex = color[i];
+    if (colorIndex === color[i]) {
+      colorIndex.addEventListener('click', () => {
+        color.forEach((element) => {
+          element.classList.remove("selected")
+        });
+        colorIndex.classList.add('selected');
+      });
+    }
+  }
+}
+
+setSelected();
