c872dc8291805a5c3f05a123fc13f200277ed06c
cristianovitorino <57003922+cristianovitorino@users.noreply.github.com>
2021-06-13 19:26:45 -0300

    Adiciona requisito 8

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-function setSelected() {
+let colorId = document.querySelector('.selected').id;
+
+function selectColor() {
   const color = document.querySelectorAll('.color');
 
   for (let i = 0; i < color.length; i += 1) {
@@ -9,9 +11,49 @@ function setSelected() {
           element.classList.remove('selected');
         });
         colorIndex.classList.add('selected');
+        colorId = color[i];
+        colorId = document.querySelector('.selected').id;
       });
     }
   }
 }
 
-setSelected();
+selectColor();
+
+function selectPixel() {
+  const color = document.querySelectorAll('.pixel');
+
+  for (let i = 0; i < color.length; i += 1) {
+    const colorIndex = color[i];
+    if (colorIndex === color[i]) {
+      colorIndex.addEventListener('click', () => {
+        color.forEach((element) => {
+          element.classList.remove('selected-pixel');
+        });
+        colorIndex.classList.add('selected-pixel');
+      });
+    }
+  }
+}
+
+selectPixel();
+
+function paintPixel() {
+  const allCells = document.querySelectorAll('.pixel');
+
+  if (colorId === colorId) {
+    for (let i = 0; i < allCells.length; i += 1) {
+      const colorIndex = allCells[i];
+      if (colorIndex === allCells[i]) {
+        colorIndex.addEventListener('click', () => {
+          allCells.forEach(() => {
+            const pixelId = document.querySelector('.selected-pixel').id;
+            document.getElementById(pixelId).style.backgroundColor = colorId;
+          });
+        });
+      }
+    }
+  }
+}
+
+paintPixel();
