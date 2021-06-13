b19d82009bc259f6c1911af83d3178639f60c8cf
junglejf <jfcchagas@id.uff.br>
2021-06-13 03:47:34 -0300

    projeto finalizado

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const numPallets = document.getElementById('color-palette').getElementsByClassNa
 const linhas = 5;
 const colunas = 5;
 const cleanButton = document.getElementById('clear-board');
+const generateButton = document.getElementById('generate-board');
 
 function fillBoard(linha, coluna) {
   for (let i = 0; i < linha; i += 1) {
@@ -17,14 +18,15 @@ function fillBoard(linha, coluna) {
     board.appendChild(line);
   }
 }
-fillBoard(linhas, colunas);
+
+function startBoard() {
+  fillBoard(linhas, colunas);
+}
 
 function selectColor(e) {
-  console.log(e.target,"antes");
   const lastSelect = document.querySelector('.selected');
   lastSelect.classList.remove('selected');
   e.target.classList.add('selected');
-  console.log(e.target,"depois e lastselected = ",lastSelect);
 }
 
 function paintSquare(e) {
@@ -39,8 +41,36 @@ function clearBoard() {
   fillBoard(linhas, colunas);
 }
 
+function generateBoard() {
+  let n = parseInt(document.getElementById('board-size').value, 10);
+  if (Number.isInteger(n)) {
+    if (n < 5) {
+      n = 5;
+    } else if (n > 50) {
+      n = 50;
+    }
+    board.innerHTML = '';
+    fillBoard(n, n);
+  } else {
+    alert('Board inválido!');
+  }
+}
+
+function randomPalletCollor() {
+  const palletColor = ['red', 'blue', 'green', 'yellow', 'cyan', 'orange', 'pink'];
+  for (let i = 0; i < 3; i += 1) {
+    const index = Math.floor(Math.random() * palletColor.length);
+    const color = `background-color: ${palletColor.splice(index, 1)};`;
+    console.log(index, palletColor);
+    numPallets[i + 1].setAttribute('style', color);
+  }
+}
+
 for (let i = 0; i < numPallets.length; i += 1) {
   numPallets[i].addEventListener('click', selectColor);
 }
 board.addEventListener('click', paintSquare);
 cleanButton.addEventListener('click', clearBoard);
+generateButton.addEventListener('click', generateBoard);
+document.addEventListener('DOMContentLoaded', startBoard);
+document.addEventListener('DOMContentLoaded', randomPalletCollor);
