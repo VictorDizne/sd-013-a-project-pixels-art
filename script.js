84ef44d3b73ca5c5707d190601d51e2b4a63972a
Rafael Araujo <rafacarlosaraujo@gmail.com>
2021-06-13 22:14:37 -0300

    Requisitos 8 e 9

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-const black = document.getElementById('black');
-const yellow = document.getElementById('yellow');
-const blue = document.getElementById('blue');
-const green = document.getElementById('green');
+function blackColorSelected() {
+  let colorBlack = document.getElementById('blackColor');
+  colorBlack.classList.add('selected');
+}
+blackColorSelected();
+
+let black = document.getElementById('blackColor');
+let yellow = document.getElementById('yellowColor');
+let blue = document.getElementById('blueColor');
+let green = document.getElementById('greenColor');
 
 black.addEventListener('click', addClassSelected);
 yellow.addEventListener('click', addClassSelected);
@@ -13,3 +19,32 @@ function addClassSelected(event) {
   colorSelected.classList.remove('selected');
   event.target.classList.add('selected');
 }
+
+function createPixel() {
+  let pixel = document.getElementsByClassName('pixel');
+
+  for (let i of pixel) {
+    i.addEventListener('click', changeColor);
+
+    function changeColor(event) {
+      let selected = document.getElementsByClassName('selected');
+      event.target.style.backgroundColor = selected[0].id;
+    }
+  }
+}
+createPixel();
+
+function clearBoard() {
+  let clearButton = document.getElementById('clear-board');
+  clearButton.addEventListener('click', clear);
+
+  function clear() {
+    let pixel = document.getElementsByClassName('pixel');
+    for (let index of pixel) {
+      if (index.style.backgroundColor != 'white'){
+        index.style.backgroundColor = 'white';
+      }
+    }
+  }
+}
+clearBoard();
