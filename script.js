f43078f7e8b4272fb077f1eebb1b40a1299f719e
Rafael Araujo <rafacarlosaraujo@gmail.com>
2021-06-13 22:34:46 -0300

    Requisitos 8 e 9 corrigidos

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 function blackColorSelected() {
-  let colorBlack = document.getElementById('blackColor');
+  let colorBlack = document.getElementById('black');
   colorBlack.classList.add('selected');
 }
+
 blackColorSelected();
 
-let black = document.getElementById('blackColor');
-let yellow = document.getElementById('yellowColor');
-let blue = document.getElementById('blueColor');
-let green = document.getElementById('greenColor');
+let black = document.getElementById('black');
+let yellow = document.getElementById('yellow');
+let green = document.getElementById('green');
+let blue = document.getElementById('blue');
 
 black.addEventListener('click', addClassSelected);
 yellow.addEventListener('click', addClassSelected);
-blue.addEventListener('click', addClassSelected);
 green.addEventListener('click', addClassSelected);
+blue.addEventListener('click', addClassSelected);
 
 function addClassSelected(event) {
   let colorSelected = document.querySelector('.selected');
@@ -20,29 +21,27 @@ function addClassSelected(event) {
   event.target.classList.add('selected');
 }
 
-function createPixel() {
-  let pixel = document.getElementsByClassName('pixel');
+function createArt() {
+  let pixels = document.getElementsByClassName('pixel');
 
-  for (let i of pixel) {
-    i.addEventListener('click', changeColor);
-
+  for (let pixel of pixels) {
+    pixel.addEventListener('click', changeColor);
     function changeColor(event) {
       let selected = document.getElementsByClassName('selected');
       event.target.style.backgroundColor = selected[0].id;
     }
   }
 }
-createPixel();
+createArt();
 
 function clearBoard() {
   let clearButton = document.getElementById('clear-board');
   clearButton.addEventListener('click', clear);
-
   function clear() {
-    let pixel = document.getElementsByClassName('pixel');
-    for (let index of pixel) {
-      if (index.style.backgroundColor != 'white'){
-        index.style.backgroundColor = 'white';
+    let pixels = document.getElementsByClassName('pixel');
+    for (let pixel of pixels) {
+      if (pixel.style.backgroundColor != 'white'){
+        pixel.style.backgroundColor = 'white';
       }
     }
   }
