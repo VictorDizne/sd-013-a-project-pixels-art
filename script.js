739f0f825996970afeed8504f262fb78ede91f85
Yuri <yurifranco18@live.com>
2021-06-14 12:24:45 -0300

    foi feito req. 8 e formatado o arquivo .js

--- script.js
+++ script.js
@@ -6,10 +6,22 @@ function addRemove(event) {
   event.target.classList.add('selected');
 }
 
-function selecionar () {
-  for (index = 0; index < colors.length; index +=1) {
-      colors[index].addEventListener('click', addRemove);
+function selecionar() {
+  for (index = 0; index < colors.length; index += 1) {
+    colors[index].addEventListener('click', addRemove);
   }
 }
 
 selecionar();
+
+const pixels = document.querySelectorAll('.pixel');
+
+for (index = 0; index < pixels.length; index += 1) {
+  pixels[index].addEventListener('click', addColor);
+}
+
+function addColor(event) {
+  const selecionaCor = document.getElementsByClassName('selected')[0];
+  const cor = window.getComputedStyle(selecionaCor);
+  event.target.style.backgroundColor = cor.backgroundColor;
+}
