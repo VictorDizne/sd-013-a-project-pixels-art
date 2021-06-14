ff8ae9d12fcf96a4fefe97eb6a1d00ee0b270816
cristianovitorino <57003922+cristianovitorino@users.noreply.github.com>
2021-06-13 22:30:35 -0300

    Adiciona requisito 9

--- script.js
+++ script.js
@@ -41,19 +41,29 @@ selectPixel();
 function paintPixel() {
   const allCells = document.querySelectorAll('.pixel');
 
-  if (colorId === colorId) {
-    for (let i = 0; i < allCells.length; i += 1) {
-      const colorIndex = allCells[i];
-      if (colorIndex === allCells[i]) {
-        colorIndex.addEventListener('click', () => {
-          allCells.forEach(() => {
-            const pixelId = document.querySelector('.selected-pixel').id;
-            document.getElementById(pixelId).style.backgroundColor = colorId;
-          });
+  for (let i = 0; i < allCells.length; i += 1) {
+    const colorIndex = allCells[i];
+    if (colorIndex === allCells[i]) {
+      colorIndex.addEventListener('click', () => {
+        allCells.forEach(() => {
+          const pixelId = document.querySelector('.selected-pixel').id;
+          document.getElementById(pixelId).style.backgroundColor = colorId;
         });
-      }
+      });
     }
   }
 }
 
 paintPixel();
+
+function clearBoard() {
+  const button = document.getElementById('clear-board');
+  const row = document.getElementsByClassName('pixel');
+  button.addEventListener('click', () => {
+    for (let i = 0; i < row.length; i += 1) {
+      row[i].style.backgroundColor = 'white';
+    }
+  });
+}
+
+clearBoard();
