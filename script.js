079fe2ce598d445ab1771cfc8d68d5f83885e68a
Ygor <ygormaiaa1@gmail.com>
2021-06-20 18:55:30 -0300

    Adicionando requisitos de 3 a 9

--- script.js
+++ script.js
@@ -1,11 +1,58 @@
+let btnBlack = document.querySelector(".black");
+let btnBlue = document.querySelector(".blue");
+let btnRed = document.querySelector(".red");
+let btnYellow = document.querySelector(".yellow");
+let qdrPixels = document.querySelectorAll(".pixel"); 
+let btnClear = document.querySelector("#clear-board");
 
-let buttonClear = document.getElementById("clear-board");
+function limparSelecao() {
+  document.querySelector(".selected").classList.remove("selected");
+};
 
-buttonClear.addEventListener("click", umClick);
+function selecionar(elemento) {
+  elemento.classList.add("selected");
+};
 
-function umClick() {
-alert("O botão foi clicado!");
+btnBlack.addEventListener('click', function () {
+  limparSelecao();
+
+  selecionar(this);
+});
+
+btnBlue.addEventListener('click', function () {
+  limparSelecao();
+
+  selecionar(this);
+});
+
+btnRed.addEventListener('click', function () {
+  limparSelecao();
+
+  selecionar(this);
+});
+
+btnYellow.addEventListener('click', function () {
+  limparSelecao();
+
+  selecionar(this);
+});
+
+for (pixel of qdrPixels) {
+  pixel.addEventListener('click', function() {
+    let selected = document.querySelector(".selected");
+    let selectedColor = selected.classList.item(1);
+    this.classList.remove("black", "red", "blue", "yellow");
+    this.classList.add(selectedColor);
+  })
 }
+
+btnClear.addEventListener("click", function () {
+  for (pixel of qdrPixels) {
+    pixel.classList.remove("black", "red", "yellow", "blue");
+  }
+})
+
+
 
 
 
