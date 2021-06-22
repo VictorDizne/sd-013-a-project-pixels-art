7772d4a1e7097208d41af3fc01bd9ffad7b4560f
CarlosLima <carlos.augustolimaf@gmail.com>
2021-06-22 03:32:26 -0300

    Adiciona os requisitos obrigatórios

---
+++ script.js
@@ -0,0 +1,47 @@
+
+
+// function pixelBoard() {
+//   const pixelPalette = document.querySelector('#pixel-board');
+
+//   for (let index = 0; index < 25; index += 1) {
+  
+//     const board = document.createElement('div');
+//     board.classList.add('pixel');
+//     pixelPalette.appendChild(board);
+//     console.log('oi');
+//   }
+// }
+
+// pixelBoard();
+
+
+const selectedCor = document.querySelector('#color-palette');
+
+function newSelected(event) {
+  const elemento = document.querySelector('.selected');
+  elemento.classList.remove('selected');
+  event.target.classList.add('selected');
+}
+
+selectedCor.addEventListener('click', newSelected);
+
+const pixelBoard = document.querySelector('#pixel-board');
+
+function changeColor(event) {
+  const elemento = document.querySelector('.selected').id;
+  event.target.setAttribute('id', elemento);
+}
+
+pixelBoard.addEventListener('click', changeColor);
+
+
+const limpar = document.querySelector('#clear-board');
+
+function clearPixels() {
+  const pixelFilhos = document.querySelectorAll('.pixel');
+  for (const iterator of pixelFilhos) {
+    iterator.removeAttribute('id');
+  }
+}
+
+limpar.addEventListener('click', clearPixels);
