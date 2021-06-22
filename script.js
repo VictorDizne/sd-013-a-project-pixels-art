df31c85a7a0265d230461693c6212f7758d498d6
Ygor <ygormaiaa1@gmail.com>
2021-06-22 11:40:27 -0300

    Terminando requisitos 09 e 12

--- script.js
+++ script.js
@@ -4,6 +4,23 @@ let btnRed = document.querySelector(".red");
 let btnYellow = document.querySelector(".yellow");
 let qdrPixels = document.querySelectorAll(".pixel"); 
 let btnClear = document.querySelector("#clear-board");
+let input = document.querySelector("#board-size");
+let btnVqv = document.querySelector("#generate-board");
+let paletaCores = document.querySelector("color-palette");
+
+
+
+let coresAleatorias = document.querySelectorAll(".color");
+
+for (cor of coresAleatorias) {
+  let r = Math.floor(Math.random() * 256);
+  let g = Math.floor(Math.random() * 256);
+  let b = Math.floor(Math.random() * 256);
+
+   console.log(cor) 
+   cor.style.backgroundColor = `rgb(${r},${g},${b})`;
+
+}
 
 function limparSelecao() {
   document.querySelector(".selected").classList.remove("selected");
@@ -40,17 +57,24 @@ btnYellow.addEventListener('click', function () {
 for (pixel of qdrPixels) {
   pixel.addEventListener('click', function() {
     let selected = document.querySelector(".selected");
-    let selectedColor = selected.classList.item(1);
-    this.classList.remove("black", "red", "blue", "yellow");
-    this.classList.add(selectedColor);
+    let selectedColor = window.getComputedStyle(selected).backgroundColor;
+    this.style.backgroundColor = selectedColor;
   });
 };
 
 btnClear.addEventListener("click", function () {
   for (pixel of qdrPixels) {
-    pixel.classList.remove("black", "red", "yellow", "blue");
+    pixel.style.backgroundColor = "white";
   }
 });
+
+
+btnVqv.addEventListener('click', function () {
+  if (input.innerText === "") {
+    alert("Board inválido!")
+  }
+})
+
 
 
 
