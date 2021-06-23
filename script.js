50a32526008e61179983b566ae55b6673cb551a6
Max Salewski <maxsalewski@gmail.com>
2021-06-23 16:33:07 -0300

    Fixado alguns erros lint

--- script.js
+++ script.js
@@ -2,73 +2,44 @@ const pixel1 = document.getElementsByClassName('pixel');
 const btnGenerate = document.getElementById('generate-board');
 const inputNumber = document.getElementById('board-size');
 
-
 function generateBoard(linesN) {
-let lines = document.getElementById('pixel-board');
-
-lines.innerHTML = '';
-
-  for (let count1 = 0; count1 < linesN; count1++) {
-    let lines1 = lines.appendChild(document.createElement("div"))
-    lines1.className = "lines";
-      for(let count2 = 0; count2 < linesN; count2++) {
-        let lines2 = lines1.appendChild(document.createElement("div"));
-        lines2.className = "pixel";
-      }
+  const lines = document.getElementById('pixel-board');
+  lines.innerHTML = '';
+  for (let count1 = 0; count1 < linesN; count1 += 1) {
+    const lines1 = lines.appendChild(document.createElement('div'));
+    lines1.className = 'lines';
+    for (let count2 = 0; count2 < linesN; count2 += 1) {
+      const lines2 = lines1.appendChild(document.createElement('div'));
+      lines2.className = 'pixel';
+    }
   }
-  for (let i = 0, len = pixel1.length; i < len; i++)
-  {
-    (function(index){
-        pixel1[i].onclick = function(){
-              pixel1[i].style.backgroundColor = 'black';
-        }    
-    })(i);
-
+  for (let i = 0, len = pixel1.length; i < len; i += 1) {
+    pixel1[i].onclick = function () {
+      pixel1[i].style.backgroundColor = 'black';
+    };
   }
 }
-
-function printColor() {
-  const selected = document.querySelector('.selected');
-  const color1 = getColor(selected);
-  const pixel1 = document.getElementsByClassName('pixel');
-  console.log(pixel1);
-  for (let i = 0, len = pixel1.length; i < len; i++)
-  {
-    (function(index){
-        pixel1[i].onclick = function(){
-              pixel1[i].style.backgroundColor = color1;
-        }    
-    })(i);
-
-  }
-  }
 
 function getColor(color) {
   const colorG = window.getComputedStyle(color).getPropertyValue('background-color');
-  let qr = document.querySelectorAll('.selected');
+  const qr = document.querySelectorAll('.selected');
   qr.forEach(e => e.classList.remove('selected'));
   color.classList.add('selected');
   return colorG;
-  }
+}
 
 function printColor() {
   const selected = document.querySelector('.selected');
   const color1 = getColor(selected);
-  console.log(pixel1);
-    for (let i = 0, len = pixel1.length; i < len; i++)
-    {
-    (function(index){
-        pixel1[i].onclick = function(){
-              pixel1[i].style.backgroundColor = color1;
-        }    
-    })(i);
-
-    }
+  for (let i = 0, len = pixel1.length; i < len; i += 1) {
+    pixel1[i].onclick = function () {
+      pixel1[i].style.backgroundColor = color1;
+    };
+  }
 }
 
 function cleanBtn() {
-  for (let i = 0, len = pixel1.length; i < len; i++)
-  {
+  for (let i = 0, len = pixel1.length; i < len; i += 1) {
     pixel1[i].style.backgroundColor = 'white';
   }
 }
