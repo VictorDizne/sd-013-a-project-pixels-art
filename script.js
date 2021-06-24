bd2c5e81addb2322771736f8ea42bd4a7c287178
Eduardomak <eduardomak92@gmail.com>
2021-06-24 18:37:40 -0300

    corrigindo lint

--- script.js
+++ script.js
@@ -23,77 +23,73 @@ function removeChild(number) {
 const sizeInput = document.querySelector('#board-size');
 const btnSize = document.querySelector('#generate-board');
 function newBoard() {
-  btnSize.addEventListener('click', function () {
+  btnSize.addEventListener('click', () => {
     let pixelSize = 5;
     removeChild(pixelSize);
-    pixelSize = parseInt(sizeInput.value);
+    pixelSize = parseInt(sizeInput.value, 10);
     if (sizeInput.value === '') {
       alert('Board inválido!');
-    }
-    else if (pixelSize < 5) {
+    } else if (pixelSize < 5) {
       pixelSize = 5;
       initialBoard(pixelSize);
-    }
-    else if (pixelSize > 50) {
+    } else if (pixelSize > 50) {
       pixelSize = 50;
       initialBoard(pixelSize);
     } else {
       initialBoard(pixelSize);
     }
-  })
+  });
 }
-newBoard()
+newBoard();
 function whatIsTheColor() {
   const element = document.querySelector('.selected');
   const style = getComputedStyle(element);
   return style.backgroundColor;
 }
 function selectColor() {
-  let clickC = document.querySelectorAll('.color');
+  const clickC = document.querySelectorAll('.color');
   for (let indexColor = 0; indexColor < clickC.length; indexColor += 1) {
-    clickC[indexColor].addEventListener('click',function() {
-      let unselectColor = document.getElementsByClassName('selected');
+    clickC[indexColor].addEventListener('click', () => {
+      const unselectColor = document.getElementsByClassName('selected');
       unselectColor[0].className = 'color';
-      let colorSelected = document.getElementsByClassName('color')[indexColor];
+      const colorSelected = document.getElementsByClassName('color')[indexColor];
       colorSelected.className = 'color selected';
-    })
+    });
   }
 }
-selectColor()
+selectColor();
 
-function paint(){
-let pixel = document.querySelectorAll('.pixel')
-for (let indexPaint = 0; indexPaint < pixel.length; indexPaint += 1) {
-  pixel[indexPaint].addEventListener('click',function() {
-    let string = whatIsTheColor();
-    pixel[indexPaint].style.backgroundColor=string;
-  })
+function paint() {
+  const pixel = document.querySelectorAll('.pixel');
+  for (let indexPaint = 0; indexPaint < pixel.length; indexPaint += 1) {
+    pixel[indexPaint].addEventListener('click', () => {
+      const string = whatIsTheColor();
+      pixel[indexPaint].style.backgroundColor = string;
+    });
+  }
 }
-}
-paint()
+paint();
 
-function resetColor(){
-
-  let pixel = document.querySelectorAll('.pixel')
-  let reset = document.querySelector('#clear-board')
-  reset.addEventListener('click',function() {
+function resetColor() {
+  const pixel = document.querySelectorAll('.pixel');
+  const reset = document.querySelector('#clear-board');
+  reset.addEventListener('click', () => {
     for (let indexReset = 0; indexReset < pixel.length; indexReset += 1) {
-      pixel[indexReset].style.backgroundColor = 'white'
-  }
-})
+      pixel[indexReset].style.backgroundColor = 'white';
+    }
+  });
 }
-resetColor()
+resetColor();
 
 function randomColor() {
-  let number1 = parseInt(Math.random()*256);
-  let number2 = parseInt(Math.random()*256);
-  let number3 = parseInt(Math.random()*256);
-  let change2 = document.getElementById('color2')
-  let change3 = document.getElementById('color3')
-  let change4 = document.getElementById('color4')
+  const number1 = parseInt(Math.random() * 256, 10);
+  const number2 = parseInt(Math.random() * 256, 10);
+  const number3 = parseInt(Math.random() * 256, 10);
+  const change2 = document.getElementById('color2');
+  const change3 = document.getElementById('color3');
+  const change4 = document.getElementById('color4');
   change2.style.backgroundColor = `rgb(${number1}, ${number2}, ${number3})`;
   change3.style.backgroundColor = `rgb(${number2}, ${number3}, ${number1})`;
   change4.style.backgroundColor = `rgb(${number3}, ${number2}, ${number1})`;
-  console.log(number1)
 }
-randomColor()
+randomColor();
