9fc43f949157ed1c489b296928ec457a579d9d7d
Eduardomak <eduardomak92@gmail.com>
2021-06-24 17:47:29 -0300

    Fazendo os 100 por cento (sem lint)

--- script.js
+++ script.js
@@ -1,46 +1,40 @@
 function initialBoard(number) {
-  let board = document.getElementById('pixel-board');
-  for(let index = 0; index < number; index += 1){
-    let newUl = document.createElement('ul');
-    let lines = board.appendChild(newUl)
-    lines.id = "pixel-table";
-    let pixel = document.querySelectorAll('#pixel-table');
-    for(let indexColumn = 0; indexColumn < number; indexColumn += 1) {
-      let newLi = document.createElement('li');
-      let pixels = pixel[index].appendChild(newLi);
-      pixels.className = 'pixel'
+  const board = document.getElementById('pixel-board');
+  for (let index = 0; index < number; index += 1) {
+    const newUl = document.createElement('ul');
+    const lines = board.appendChild(newUl);
+    lines.id = 'pixel-table';
+    const pixel = document.querySelectorAll('#pixel-table');
+    for (let indexColumn = 0; indexColumn < number; indexColumn += 1) {
+      const newLi = document.createElement('li');
+      const pixels = pixel[index].appendChild(newLi);
+      pixels.className = 'pixel';
     }
   }
 }
 initialBoard(5);
 function removeChild(number) {
-  let erase = document.querySelectorAll('#pixel-table')
-  let parent = document.querySelector('#pixel-board')
-  for(let index = 0; index < number; index += 1) {
+  const erase = document.querySelectorAll('#pixel-table');
+  const parent = document.querySelector('#pixel-board');
+  for (let index = 0; index < number; index += 1) {
     parent.removeChild(erase[index]);
   }
 }
-let sizeInput = document.querySelector('#board-size')
-let btnSize = document.querySelector('#generate-board')
+const sizeInput = document.querySelector('#board-size');
+const btnSize = document.querySelector('#generate-board');
 function newBoard() {
-  btnSize.addEventListener('click', function() {
-    /* let erase = document.querySelectorAll('#pixel-table')
-    let parent = document.querySelector('#pixel-board')
-    let number = 5;
-    for(let index = 0; index < number; index += 1){
-      parent.removeChild(erase[index]);
-    } */
+  btnSize.addEventListener('click', function () {
     let pixelSize = 5;
     removeChild(pixelSize);
     pixelSize = parseInt(sizeInput.value);
-    if(sizeInput.value === '') {
+    if (sizeInput.value === '') {
       alert('Board inválido!');
     }
-    else if(pixelSize < 5){
+    else if (pixelSize < 5) {
       pixelSize = 5;
       initialBoard(pixelSize);
     }
-    else if(pixelSize > 50){
+    else if (pixelSize > 50) {
       pixelSize = 50;
       initialBoard(pixelSize);
     } else {
@@ -89,3 +83,17 @@ function resetColor(){
 })
 }
 resetColor()
+
+function randomColor() {
+  let number1 = parseInt(Math.random()*256);
+  let number2 = parseInt(Math.random()*256);
+  let number3 = parseInt(Math.random()*256);
+  let change2 = document.getElementById('color2')
+  let change3 = document.getElementById('color3')
+  let change4 = document.getElementById('color4')
+  change2.style.backgroundColor = `rgb(${number1}, ${number2}, ${number3})`;
+  change3.style.backgroundColor = `rgb(${number2}, ${number3}, ${number1})`;
+  change4.style.backgroundColor = `rgb(${number3}, ${number2}, ${number1})`;
+  console.log(number1)
+}
+randomColor()
