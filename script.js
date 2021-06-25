2dde9737799075804e7e6e0c03c3e11a348f7304
Eduardomak <eduardomak92@gmail.com>
2021-06-24 21:54:41 -0300

    versão final

--- script.js
+++ script.js
@@ -13,6 +13,24 @@ function initialBoard(number) {
   }
 }
 initialBoard(5);
+
+function whatIsTheColor() {
+  const element = document.querySelector('.selected');
+  const style = getComputedStyle(element);
+  return style.backgroundColor;
+}
+
+function paint() {
+  const pixel = document.querySelectorAll('.pixel');
+  for (let indexPaint = 0; indexPaint < pixel.length; indexPaint += 1) {
+    pixel[indexPaint].addEventListener('click', () => {
+      const string = whatIsTheColor();
+      pixel[indexPaint].style.backgroundColor = string;
+    });
+  }
+}
+paint();
+
 function removeChild(number) {
   const erase = document.querySelectorAll('#pixel-table');
   const parent = document.querySelector('#pixel-board');
@@ -23,8 +41,8 @@ function removeChild(number) {
 const sizeInput = document.querySelector('#board-size');
 const btnSize = document.querySelector('#generate-board');
 function newBoard() {
+  let pixelSize = 5;
   btnSize.addEventListener('click', () => {
-    let pixelSize = 5;
     removeChild(pixelSize);
     pixelSize = parseInt(sizeInput.value, 10);
     if (sizeInput.value === '') {
@@ -38,14 +56,11 @@ function newBoard() {
     } else {
       initialBoard(pixelSize);
     }
+    paint();
   });
 }
 newBoard();
-function whatIsTheColor() {
-  const element = document.querySelector('.selected');
-  const style = getComputedStyle(element);
-  return style.backgroundColor;
-}
+
 function selectColor() {
   const clickC = document.querySelectorAll('.color');
   for (let indexColor = 0; indexColor < clickC.length; indexColor += 1) {
@@ -59,17 +74,6 @@ function selectColor() {
 }
 selectColor();
 
-function paint() {
-  const pixel = document.querySelectorAll('.pixel');
-  for (let indexPaint = 0; indexPaint < pixel.length; indexPaint += 1) {
-    pixel[indexPaint].addEventListener('click', () => {
-      const string = whatIsTheColor();
-      pixel[indexPaint].style.backgroundColor = string;
-    });
-  }
-}
-paint();
-
 function resetColor() {
   const pixel = document.querySelectorAll('.pixel');
   const reset = document.querySelector('#clear-board');
@@ -82,14 +86,14 @@ function resetColor() {
 resetColor();
 
 function randomColor() {
-  const number1 = parseInt(Math.random() * 256, 10);
-  const number2 = parseInt(Math.random() * 256, 10);
-  const number3 = parseInt(Math.random() * 256, 10);
+  const number1 = parseInt(Math.random() * 255, 10);
+  const number2 = parseInt(Math.random() * 255, 10);
+  const number3 = parseInt(Math.random() * 255, 10);
   const change2 = document.getElementById('color2');
   const change3 = document.getElementById('color3');
   const change4 = document.getElementById('color4');
   change2.style.backgroundColor = `rgb(${number1}, ${number2}, ${number3})`;
   change3.style.backgroundColor = `rgb(${number2}, ${number3}, ${number1})`;
-  change4.style.backgroundColor = `rgb(${number3}, ${number2}, ${number1})`;
+  change4.style.backgroundColor = `rgb(${number3}, ${number1}, ${number2})`;
 }
 randomColor();
