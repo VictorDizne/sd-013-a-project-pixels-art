8983e335cbe449e3605143de1ee28d3758e50769
João Victor Paduam <joao.paduam@hotmail.com>
2021-06-27 00:36:34 -0300

    Finalmente acabei

--- script.js
+++ script.js
@@ -17,7 +17,8 @@ const desafio2e3e6 = () => {
   const ul = document.createElement('ul');
   ul.setAttribute('id', 'color-palette');
   section.setAttribute('id', 'palette');
-  const colors = ['black', 'red', 'blue', 'green'];
+  const rgbAleatorio = () => `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+  const colors = ['black', rgbAleatorio(), rgbAleatorio(), rgbAleatorio()];
   for (let i = 0; i < colors.length; i += 1) {
     const color = colors[i];
     const li = document.createElement('li');
@@ -33,27 +34,25 @@ const desafio2e3e6 = () => {
 };
 desafio2e3e6();
 
-const desafio4e5 = () => {
+const desafio4e5 = (linhas) => {
   const section = document.createElement('section');
   const ul = document.createElement('ul');
+  section.appendChild(ul);
+  document.body.appendChild(section);
   section.setAttribute('id', 'pixels');
   ul.setAttribute('id', 'pixel-board');
-  for (let i = 0; i < 5; i += 1) {
+  for (let i = 0; i < linhas; i += 1) {
     const div = document.createElement('div');
     div.setAttribute('class', 'flex');
-    for (let j = 0; j < 25; j += 1) {
+    ul.appendChild(div);
+    for (let j = 0; j < linhas; j += 1) {
       const li = document.createElement('li');
       li.classList.add('pixel');
-      if (j % 5 === 0) {
-        div.appendChild(li);
-        ul.appendChild(div);
-        section.appendChild(ul);
-        document.body.appendChild(section);
-      }
+      div.appendChild(li);
     }
   }
 };
-desafio4e5();
+desafio4e5(5);
 
 const desafio7 = () => {
   const colors = document.getElementsByClassName('color');
@@ -88,6 +87,7 @@ desafio8();
 
 const desafio9 = () => {
   const div = document.createElement('div');
+  div.setAttribute('id', 'div-botoes');
   const button = document.createElement('button');
   const pixels1 = document.getElementById('pixels');
   button.setAttribute('id', 'clear-board');
@@ -106,3 +106,41 @@ const desafio9 = () => {
   });
 };
 desafio9();
+
+// eslint-disable-next-line max-lines-per-function
+const desafio10 = () => {
+  const divBotoes = document.querySelector('#div-botoes');
+  const input = document.createElement('input');
+  input.setAttribute('type', 'number');
+  input.setAttribute('min', '1');
+  input.setAttribute('id', 'board-size');
+  const botaoAumentar = document.createElement('button');
+  botaoAumentar.setAttribute('id', 'generate-board');
+  botaoAumentar.innerHTML = 'VQV';
+  divBotoes.appendChild(input);
+  divBotoes.appendChild(botaoAumentar);
+  botaoAumentar.addEventListener('click', () => {
+    if (input.value === '') {
+      return alert('Board inválido!');
+    } if (input.value < 5) {
+      input.value = 5;
+    } if (input.value > 50) {
+      input.value = 50;
+    }
+    const ulPixel = document.querySelector('#pixel-board');
+    ulPixel.remove();
+    desafio4e5(input.value);
+    desafio8();
+  });
+};
+desafio10();
+
+// let r = Math.floor(Math.random()*256);
+// let g = Math.floor(Math.random()*256);
+// let b = Math.floor(Math.random()*256);
+
+// let rgb = `rgb(${r}, ${g}, ${b})`;
+// let bgr = `rgb(${b}, ${g}, ${r})`;
+// let grb = `rgb(${g}, ${r}, ${b})`;
+
+// const cores = ['black',rgb, bgr, grb];
