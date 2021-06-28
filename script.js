290a6c5cb3bad5affb03bab1e16712b867763c78
Reinaldo Takashima Ouriques <reinaldotouriques@gmail.com>
2021-06-28 20:25:02 -0300

    Requisitos 6 e 7

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // Pega a section pixel-board.
 const pixelBoard = document.getElementById('pixel-board');
 
+// Pega a id color-palette da section da paleta.
+const sectionColorPalette = document.getElementById('color-palette');
+
+// Pega a classe color das divs da paleta.
+const divColorPalette = document.getElementsByClassName('color');
+
 // Função responsável por criar as linhas e colunas.
 function createBoard(boardSize) {
   // Cria as linas.
@@ -19,3 +25,23 @@ function createBoard(boardSize) {
 }
 // Executa a função.
 createBoard(5);
+
+// Função responsável por percorrer toda a extenção do array da paleta, a partir da posição 1 e verifica se existe algum elemento com a classe selected. Se houver, o laço remove a classe.
+function selectedDiv() {
+  divColorPalette[0].style.backgroundColor = 'rgb(0, 0, 0)';
+  divColorPalette[0].classList.add('selected');
+  sectionColorPalette.addEventListener('click', (event) => {
+    console.log(event.target);
+    const allDivColor = document.getElementsByClassName('color');
+    for (let index = 0; index < allDivColor.length; index += 1) {
+      if (allDivColor[index].classList.contains('selected')) {
+        allDivColor[index].classList.remove('selected');
+      }
+    }
+    // Adiciona a classe selected no elemento que sofrer a ação de click declarada anteriormente.
+    event.target.classList.add('selected');
+   //console.log();
+  })
+}
+// Executa a função.
+selectedDiv();
